be876c8596cdae5d7e14821eddb8b5ae25890511
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2009-10-26 15:09:31 +0100

    permisions grant to alerts tables.
    added:
      data_model/32_grant_alerts.sql

---
+++ data_model/32_grant_alerts.sql
@@ -0,0 +1,49 @@
+--
+-- grant_alerts.sql
+--
+
+-- ui/daemon may read everything
+GRANT SELECT
+  ON TABLE alerts,
+           hosts,
+           procs,
+           analizers,
+           reference_urls,
+           services,
+           severities,
+           reported_hosts,
+           reported_procs,
+           reported_services
+  TO "acarm-ng-ui",
+     "acarm-ng-daemon";
+
+-- daemon may add new data to DB
+GRANT INSERT
+  ON TABLE alerts,
+           hosts,
+           procs,
+           analizers,
+           reference_urls,
+           services,
+           reported_hosts,
+           reported_procs,
+           reported_services
+  TO "acarm-ng-daemon";
+
+-- daemon may update pieces of information gathered by sensors
+GRANT UPDATE
+  ON TABLE hosts
+  TO "acarm-ng-daemon";
+
+-- daemon may cleanup old data from DB
+GRANT DELETE
+  ON TABLE alerts,
+           hosts,
+           procs,
+           analizers,
+           reference_urls,
+           services,
+           reported_hosts,
+           reported_procs,
+           reported_services
+  TO "acarm-ng-daemon";
