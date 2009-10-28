38e4eb4daee4a56663035ecdcb96dc7ade324c2c
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2009-10-28 08:50:11 +0100

    upbraded build_process to pre-relese, development version.
    added:
      src/build_process/makefiles/common/toolchains/avr-flags.mk
      src/build_process/makefiles/common/toolchains/avr.mk
      src/build_process/makefiles/common/toolchains/local-flags.mk
      src/build_process/templates/features/deps/README.txt
      src/build_process/templates/features/deps/common
      src/build_process/templates/features/deps/common_dynamic_compile
      src/build_process/templates/features/deps/common_dynamic_link
    modified:
      src/build_process/makefiles/common/targets.mk
      src/build_process/scripts/getdeps
      src/build_process/templates/components.lst
      src/build_process/templates/features/modes/default

---
+++ src/build_process/makefiles/common/toolchains/avr.mk
@@ -0,0 +1,7 @@
+CC=avr-gcc
+CXX=avr-g++
+AR=avr-ar
+RANLIB=avr-ranlib
+LD=avr-ld
+STRIP=avr-strip
+DOXYGEN=doxygen
---
+++ src/build_process/makefiles/common/toolchains/local-flags.mk
@@ -0,0 +1,9 @@
+# compiler flags
+OPT_FLAGS:=-O3 -DNDEBUG -Werror
+DBG_FLAGS:=-g3 -rdynamic
+PRF_FLAGS:=$(DBG_FLAGS) $(OPT_FLAGS) -pg
+
+# linker flags
+PRF_LDFLAGS:=-pg
+DBG_LDFLAGS:=
+OPT_LDFLAGS:=
