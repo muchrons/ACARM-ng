e438aaef11c013b71e7f041c8bfc1f09fcbb69f3
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2009-11-02 10:47:47 +0100

    code has been documented.
    modified:
      src/logger/Logger/Synchro.hpp

--- src/logger/Logger/Synchro.hpp
+++ src/logger/Logger/Synchro.hpp
@@ -8,13 +8,15 @@
 #include <boost/thread/locks.hpp>
 #include <boost/thread/mutex.hpp>
 
-// TODO: comment
-
 namespace Logger
 {
 
+/** \brief class to be used as a mutex type.
+ */
 typedef boost::mutex Mutex;
 
+/** \brief mutex-locking class.
+ */
 typedef boost::lock_guard<Mutex> Lock;
 
 } // namespace Logger
