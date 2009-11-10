6b6a9f3ce50ddc9646b19d2c11d9ba2cb6b78d0c
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2009-11-10 10:37:48 +0100

    basic tests added.
    added:
      src/inputprelude/Input/Prelude/ReaderBuilder.t.cpp

---
+++ src/inputprelude/Input/Prelude/ReaderBuilder.t.cpp
@@ -0,0 +1,53 @@
+/*
+ * ReaderBuilder.t.cpp
+ *
+ */
+#include <tut.h>
+#include <cstring>
+#include <memory>
+
+#include "Input/Prelude/ReaderBuilder.hpp"
+
+using namespace std;
+using namespace Input::Prelude;
+
+namespace
+{
+
+struct ReaderBuilderTestClass
+{
+  ReaderBuilder          rb_;
+  ReaderBuilder::Options opts_;
+};
+
+typedef ReaderBuilderTestClass TestClass;
+typedef tut::test_group<TestClass> factory;
+typedef factory::object testObj;
+
+factory tf("Input/Prelude/ReaderBuilder");
+} // unnamed namespace
+
+
+namespace tut
+{
+
+// tets getting name
+template<>
+template<>
+void testObj::test<1>(void)
+{
+  ensure_equals("invalid name", rb_.getTypeName(), "prelude");
+}
+
+// test creating some reader
+template<>
+template<>
+void testObj::test<2>(void)
+{
+  ReaderBuilder::FactoryPtr ptr=rb_.build(opts_);
+  ensure("NULL pointere returned", ptr!=NULL);
+}
+
+// TODO: test different options
+
+} // namespace tut
