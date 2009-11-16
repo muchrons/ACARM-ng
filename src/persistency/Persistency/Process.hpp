848f343c7058b8c5a829638ae438553fa85ae172
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2009-11-16 17:25:23 +0100

    code commented.
    modified:
      src/persistency/Persistency/Process.hpp

--- src/persistency/Persistency/Process.hpp
+++ src/persistency/Persistency/Process.hpp
@@ -18,19 +18,39 @@
 namespace Persistency
 {
 
+/** \brief process data repreesntation.
+ */
 class Process: private boost::noncopyable
 {
 public:
+  /** \brief path to file type.
+   */
   typedef detail::LimitedNULLString<256> Path;
+  /** \brief process name representation.
+   */
   typedef detail::LimitedString<256>     Name;
-
+  /** \brief ensure proper destruction when inherited.
+   */
   virtual ~Process(void);
-
+  /** \brief gets path to executable.
+   *  \return path to executable process has been run from.
+   */
   const Path &getPath(void) const;
+  /** \brief gets process name.
+   *  \return process name.
+   */
   const Name &getName(void) const;
+  /** \brief MD5 hash of given file.
+   *  \return hash of executable or NULL if not present.
+   */
   const MD5Sum *getMD5(void) const;
 
 protected:
+  /** \brief creates object from given data.
+   *  \param path path to executable
+   *  \param name process name.
+   *  \param md5  hash for given binary, if available.
+   */
   Process(const Path   &path,
           const Name   &name,
           const MD5Sum *md5=NULL);
