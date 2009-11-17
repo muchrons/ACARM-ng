2c642ac421aecfac419d51325f5dd2853fc971b4
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2009-11-17 09:07:59 +0100

    code has been commented.
    modified:
      src/persistency/Persistency/ReferenceURL.hpp

--- src/persistency/Persistency/ReferenceURL.hpp
+++ src/persistency/Persistency/ReferenceURL.hpp
@@ -13,20 +13,36 @@
 
 namespace Persistency
 {
-
+/** \brief represents reference URL entry.
+ */
 class ReferenceURL: private boost::noncopyable
 {
 public:
+  /** \brief type of name of reference URL.
+   */
   typedef detail::LimitedString<128> Name;
+  /** \brief URL itself.
+   */
   typedef detail::LimitedString<256> URL;
+  // TODO: consider making URL a separate clas with syntax validation
 
+  /** \brief ensure proper destruction when inherited.
+   */
   virtual ~ReferenceURL(void);
-
+  /** \brief gets reference url's name.
+   *  \return name of reference URL.
+   */
   const Name &getName(void) const;
-
+  /** \brief gets URL itself.
+   *  \return URL address.
+   */
   const URL &getURL(void) const;
 
 protected:
+  /** \brief creates an object.
+   *  \param name name of url (~description).
+   *  \param url  url address itself.
+   */
   ReferenceURL(const Name &name, const URL url);
 
 private:
