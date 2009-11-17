a437eba4c281cab6b2f108821156e59b3ecf634e
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2009-11-17 13:32:50 +0100

    initial Alert implementation.
    added:
      src/persistency/Persistency/Alert.cpp
    modified:
      src/persistency/Persistency/Alert.hpp

---
+++ src/persistency/Persistency/Alert.cpp
@@ -0,0 +1,79 @@
+/*
+ * Alert.cpp
+ *
+ */
+#include <cassert>
+
+#include "Persistency/Alert.hpp"
+
+using namespace std;
+
+namespace Persistency
+{
+
+Alert::~Alert(void)
+{
+}
+
+const Alert::Name &Alert::getName(void) const
+{
+  return name_;
+}
+
+const Analyzer &Alert::getAnalyzer(void) const
+{
+  assert( analyzer_.get()!=NULL );
+  return *analyzer_;
+}
+
+const Alert::Timestamp *Alert::getDetectionTime(void) const
+{
+  return detected_.get();
+}
+
+const Alert::Timestamp &Alert::getCreationTime(void) const
+{
+  return created_;
+}
+
+const Severity &Alert::getSeverity(void) const
+{
+  assert( severity_.get()!=NULL );
+  return *severity_;
+}
+
+Certanity Alert::getCertanity(void) const
+{
+  return certanity_;
+}
+
+const std::string *Alert::getDescription(void) const
+{
+  return description_.get();
+}
+
+Alert::Alert(const Name        &name,
+             AnalyzerPtr       &analyzer,
+             const Timestamp   *detected,
+             const Timestamp   &created,
+             SeverityPtr        severity,
+             Certanity          certanity,
+             const std::string *description):
+  name_(name),
+  analyzer_(analyzer),
+  detected_( (detected!=NULL)?( new Timestamp(*detected) ):NULL ),
+  created_(created),
+  severity_(severity),
+  certanity_(certanity),
+  description_( (description!=NULL)?( new string(*description) ):NULL )
+{
+  // check if required pointer paramters are NOT NULLs, as they should
+
+  if(analyzer_.get()==NULL)
+    throw ExceptionNULLParameter(CALLNAME, "analyzer");
+
+  if(severity_.get()==NULL)
+    throw ExceptionNULLParameter(CALLNAME, "severity");
+}
+
+} // namespace Persistency
--- src/persistency/Persistency/Alert.hpp
+++ src/persistency/Persistency/Alert.hpp
@@ -7,8 +7,17 @@
 
 /* public header */
 
+#include <string>
 #include <boost/shared_ptr.hpp>
+#include <boost/scoped_ptr.hpp>
 #include <boost/noncopyable.hpp>
+#include <boost/date_time/gregorian/gregorian.hpp>
+
+#include "Persistency/Analyzer.hpp"
+#include "Persistency/Severity.hpp"
+#include "Persistency/Certanity.hpp"
+#include "Persistency/detail/LimitedString.hpp"
+#include "Persistency/ExceptionNULLParameter.hpp"
 
 // TODO: implement
 // TODO: test
@@ -22,11 +31,44 @@ namespace Persistency
 class Alert: private boost::noncopyable
 {
 public:
+  typedef detail::LimitedString<256> Name;
+  typedef boost::gregorian::date     Timestamp;
+
   /** \brief starts virtual d-tors from the begining of the hierarchy.
    */
-  virtual ~Alert(void)
-  {
-  }
+  virtual ~Alert(void);
+
+  const Name &getName(void) const;
+
+  const Analyzer &getAnalyzer(void) const;
+
+  const Timestamp *getDetectionTime(void) const;
+
+  const Timestamp &getCreationTime(void) const;
+
+  const Severity &getSeverity(void) const;
+
+  Certanity getCertanity(void) const;
+
+  const std::string *getDescription(void) const;
+
+protected:
+  Alert(const Name        &name,
+        AnalyzerPtr       &analyzer,
+        const Timestamp   *detected,
+        const Timestamp   &created,
+        SeverityPtr        severity,
+        Certanity          certanity,
+        const std::string *description);
+
+private:
+  Name                           name_;
+  AnalyzerPtr                    analyzer_;
+  boost::scoped_ptr<Timestamp>   detected_;
+  Timestamp                      created_;
+  SeverityPtr                    severity_;
+  Certanity                      certanity_;
+  boost::scoped_ptr<std::string> description_;
 }; // class Alert
 
 
