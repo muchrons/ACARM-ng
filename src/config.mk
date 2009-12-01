93c69f72b8fe86beb55021e5379e25261eab024c
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2009-12-01 15:37:51 +0100

    enabled new feature - ccache.
    modified:
      src/config.mk

--- src/config.mk
+++ src/config.mk
@@ -12,3 +12,10 @@ export PROFILE=test
 # set default toolchain
 export TC=local
 
+# enable ccache
+export WITH_CCACHE=1
+
+# project-specific flags
+export USER_OPT_FLAGS:=
+export USER_DBG_FLAGS:=
+export USER_PRF_FLAGS:=
