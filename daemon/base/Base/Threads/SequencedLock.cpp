1c3885c69544e05b216f9a71cdaae4864eb83d5f
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2009-12-09 15:04:30 +0100

    added test for passing the same mutex twiece.
    modified:
      daemon/base/Base/Threads/SequencedLock.cpp

--- daemon/base/Base/Threads/SequencedLock.cpp
+++ daemon/base/Base/Threads/SequencedLock.cpp
@@ -13,9 +13,18 @@ namespace Base
 namespace Threads
 {
 
+namespace
+{
+Mutex *testedMin(Mutex *m1, Mutex *m2)
+{
+  assert(m1!=m2 && "the same mutexe passed twice");
+  return min(m1, m2);
+}
+} // unnamed namespace
+
 SequencedLock::SequencedLock(Mutex &m1, Mutex &m2):
-  l1_( *min(&m1, &m2) ),
-  l2_( *max(&m1, &m2) )
+  l1_( *testedMin(&m1, &m2) ),
+  l2_( *max      (&m1, &m2) )
 {
 }
 
