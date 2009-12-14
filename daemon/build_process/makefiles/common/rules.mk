13cc24be73aeea4f59c0f154614c5bdcada88bb0
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2009-12-14 13:29:54 +0100

    build_process v1.1.2 introduced.
    modified:
      daemon/build_process/makefiles/common/basic_flags.mk
      daemon/build_process/makefiles/common/rules.mk
      daemon/build_process/makefiles/common/toolchains/avr-flags.mk
      daemon/build_process/makefiles/common/toolchains/local-flags.mk

--- daemon/build_process/makefiles/common/rules.mk
+++ daemon/build_process/makefiles/common/rules.mk
@@ -10,3 +10,11 @@
 %.o:: %.c
 	@echo "CC    $*.c"
 	$(CC) $(CFLAGS)    -MMD -c "$(THIS_SRC_BASE_DIR)/$*.c"   -o "$@"
+
+# missing headers on dependency problem - C++
+%.hpp:
+	@true
+
+# missing headers on dependency problem - C
+%.h:
+	@true
