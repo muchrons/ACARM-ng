98e18aa012bdd6698b838f73db38056981010572
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2009-12-15 11:51:50 +0100

    added stubbed testst fcor meta-alert.
    added:
      daemon/persistency/Persistency/MetaAlert.t.cpp

---
+++ daemon/persistency/Persistency/MetaAlert.t.cpp
@@ -0,0 +1,40 @@
+/*
+ * MetaAlert.t.cpp
+ *
+ */
+#include <tut.h>
+#include <cstring>
+#include <memory>
+
+#include "Persistency/MetaAlert.hpp"
+#include "Persistency/TestHelpers.t.hpp"
+
+using namespace std;
+using namespace Persistency;
+
+namespace
+{
+
+struct TestClass
+{
+};
+
+typedef TestClass TestClass;
+typedef tut::test_group<TestClass> factory;
+typedef factory::object testObj;
+
+factory tf("Persistency/MetaAlert");
+} // unnamed namespace
+
+
+namespace tut
+{
+
+// 
+template<>
+template<>
+void testObj::test<1>(void)
+{
+}
+
+} // namespace tut
