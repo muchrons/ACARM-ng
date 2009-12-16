349612b914d87e49a274b8c47d7b57c066ce0da9
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2009-12-16 09:58:13 +0100

    added stream interface and its tests.
    modified:
      daemon/logger/Logger/Logger.hpp
      daemon/logger/Logger/Logger.t.cpp
      daemon/logger/Logger/LoggerImpl.hpp

--- daemon/logger/Logger/Logger.hpp
+++ daemon/logger/Logger/Logger.hpp
@@ -8,6 +8,7 @@
  * Logger::Node myLogger("parent.child.subchild");
  * // ...
  * LOGMSG_INFO(myLogger, "hello logs!");
+ * LOGMSG_INFO_S(myLogger)<<"the answer is: "<<42;
  * </code>
  *
  */
@@ -28,30 +29,54 @@ namespace Logger
  *  \param msg message to log.
  */
 #define LOGMSG_DEBUG(id, msg) LOGMSG_PRI_INTERNAL_IMPLEMENTATION( (id).debug, (msg) )
+/** \brief log debug message to a given node.
+ *  \param id  logger node.
+ */
+#define LOGMSG_DEBUG_S(id) LOGMSG_PRI_INTERNAL_STREAM_IMPLEMENTATION( (id), debug )
+
 
 /** \brief log info message to a given node.
  *  \param id  logger node.
  *  \param msg message to log.
  */
 #define LOGMSG_INFO(id, msg)  LOGMSG_PRI_INTERNAL_IMPLEMENTATION( (id).info,  (msg) )
+/** \brief log info message to a given node.
+ *  \param id logger node.
+ */
+#define LOGMSG_INFO_S(id) LOGMSG_PRI_INTERNAL_STREAM_IMPLEMENTATION( (id), info )
+
 
 /** \brief log warn message to a given node.
- *  \param id  logger node.
+ *  \param id logger node.
  *  \param msg message to log.
  */
 #define LOGMSG_WARN(id, msg)  LOGMSG_PRI_INTERNAL_IMPLEMENTATION( (id).warn,  (msg) )
+/** \brief log warn message to a given node.
+ *  \param id logger node.
+ */
+#define LOGMSG_WARN_S(id) LOGMSG_PRI_INTERNAL_STREAM_IMPLEMENTATION( (id), warn )
+
 
 /** \brief log error message to a given node.
  *  \param id  logger node.
  *  \param msg message to log.
  */
 #define LOGMSG_ERROR(id, msg) LOGMSG_PRI_INTERNAL_IMPLEMENTATION( (id).error, (msg) )
+/** \brief log error message to a given node.
+ *  \param id logger node.
+ */
+#define LOGMSG_ERROR_S(id) LOGMSG_PRI_INTERNAL_STREAM_IMPLEMENTATION( (id), error )
+
 
 /** \brief log fatal message to a given node.
  *  \param id  logger node.
  *  \param msg message to log.
  */
 #define LOGMSG_FATAL(id, msg) LOGMSG_PRI_INTERNAL_IMPLEMENTATION( (id).fatal, (msg) )
+/** \brief log fatal message to a given node.
+ *  \param id logger node.
+ */
+#define LOGMSG_FATAL_S(id) LOGMSG_PRI_INTERNAL_STREAM_IMPLEMENTATION( (id), fatal )
 
 } // namespace Logger
 
--- daemon/logger/Logger/Logger.t.cpp
+++ daemon/logger/Logger/Logger.t.cpp
@@ -7,6 +7,7 @@
 #include <cstring>
 
 #include "Logger/Logger.hpp"
+#include "Logger/TestHelpers.t.hpp"
 
 using namespace tut;
 using namespace std;
@@ -153,6 +154,64 @@ void testObj::test<5>(void)
 {
   LOGMSG_FATAL(*this, "test");
   ensureCalls(4);
+}
+
+// test stream logging - empty message.
+template<>
+template<>
+void testObj::test<6>(void)
+{
+  LOGMSG_DEBUG_S(n_);
+}
+
+// test stream logging debug message
+template<>
+template<>
+void testObj::test<7>(void)
+{
+  LOGMSG_DEBUG_S(n_)<<"hello "<<"debug"<<" stream!";
+  ensureLoggedPart("hello debug stream!");
+  ensureLoggedPart("DEBUG@");
+}
+
+// test stream logging info message
+template<>
+template<>
+void testObj::test<8>(void)
+{
+  LOGMSG_INFO_S(n_)<<"hello "<<"info"<<" stream!";
+  ensureLoggedPart("hello info stream!");
+  ensureLoggedPart("INFO @");
+}
+
+// test stream logging warning message
+template<>
+template<>
+void testObj::test<9>(void)
+{
+  LOGMSG_WARN_S(n_)<<"hello "<<"warn"<<" stream!";
+  ensureLoggedPart("hello warn stream!");
+  ensureLoggedPart("WARN @");
+}
+
+// test stream logging error message
+template<>
+template<>
+void testObj::test<10>(void)
+{
+  LOGMSG_ERROR_S(n_)<<"hello "<<"error"<<" stream!";
+  ensureLoggedPart("hello error stream!");
+  ensureLoggedPart("ERROR@");
+}
+
+// test stream logging fatal message
+template<>
+template<>
+void testObj::test<11>(void)
+{
+  LOGMSG_FATAL_S(n_)<<"hello "<<"fatal"<<" stream!";
+  ensureLoggedPart("hello fatal stream!");
+  ensureLoggedPart("FATAL@");
 }
 
 } // namespace tut
