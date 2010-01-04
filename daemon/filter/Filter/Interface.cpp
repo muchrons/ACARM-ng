7fb57a13b92713ffe12141c88f2a397a26474655
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-01-04 12:54:39 +0100

    asseration added.
    modified:
      daemon/filter/Filter/Interface.cpp

--- daemon/filter/Filter/Interface.cpp
+++ daemon/filter/Filter/Interface.cpp
@@ -2,6 +2,8 @@
  * Interface.cpp
  *
  */
+#include <cassert>
+
 #include "Persistency/IO/BackendFactory.hpp"
 #include "Filter/Interface.hpp"
 
@@ -11,6 +13,8 @@ namespace Filter
 void Interface::process(Node n, NodesDeque &changed)
 {
   BackendProxy bp( conn_, getFilterName() );
+
+  assert( changed.size()==0 && "non-empty output collection received");
   processImpl(n, changed, bp);
 }
 
