7006c12172b7bb2f92f98a8c1807458a75b54b42
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-01-04 10:39:37 +0100

    code commented.
    modified:
      daemon/filter/Filter/BackendProxy.hpp

--- daemon/filter/Filter/BackendProxy.hpp
+++ daemon/filter/Filter/BackendProxy.hpp
@@ -13,7 +13,6 @@
 
 #include "Persistency/GraphNode.hpp"
 
-// TODO: comments
 
 namespace Persistency
 {
@@ -43,7 +42,7 @@ public:
 
   /** \brief create object's instance.
    *  \param conn       connection object to use.
-   *  \param filterName name of filter this ibject is created for.
+   *  \param filterName name of filter this object is created for.
    */
   BackendProxy(Persistency::IO::ConnectionPtrNN  conn,
                const std::string                &filterName);
@@ -51,18 +50,44 @@ public:
    */
   ~BackendProxy(void);
 
+  /** \brief set name of a given host.
+   *  \param host host naem ot set name to.
+   *  \param name DNS name to be set for a ginve host.
+   */
   void setHostName(Persistency::HostPtrNN host, const std::string &name);
+  /** \brief updates severity of a given meta-alert.
+   *  \param ma    meta-alert to update severity of.
+   *  \param delta severity change to be applied.
+   */
   void updateSeverityDelta(Persistency::MetaAlertPtrNN ma, double delta);
+  /** \brief updates certanity of a given meta-alert.
+   *  \param ma    meta alert to update certanity on.
+   *  \param delta certanity change to be applied.
+   */
   void updateCertanityDelta(Persistency::MetaAlertPtrNN ma, double delta);
+  /** \brief adds given node as a child for other node.
+   *  \param parent parent node (this node will have child added).
+   *  \param child  child node (will be added to parent node).
+   */
   void addChild(Persistency::GraphNodePtrNN parent,
                 Persistency::GraphNodePtrNN child);
+  /** \brief correlate set of children creating new meta-alert's node as a parent.
+   *  \param ma            meta alert data of which to create node from.
+   *  \param child1        first child to correlate.
+   *  \param child2        second child to correlate.
+   *  \param otherChildren more child nodes, if needed.
+   */
   Persistency::GraphNodePtrNN correlate(
             Persistency::MetaAlertPtrNN  ma,
             Persistency::GraphNodePtrNN  child1,
             Persistency::GraphNodePtrNN  child2,
             const ChildrenVector        &otherChildren=ChildrenVector() );
-
-
+  /** \brief commit current transaction.
+   *
+   * if not transaction is started, call does nothing. if transaction is opened,
+   * it is commited. calling another call after commitChanges will open new
+   * transaction.
+   */
   void commitChanges(void);
 
 private:
