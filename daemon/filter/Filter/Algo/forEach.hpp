d3cf9d657940ec3463eaf08707917f4417c03f2b
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-01-05 10:05:25 +0100

    code commented.
    modified:
      daemon/filter/Filter/Algo/forEach.hpp

--- daemon/filter/Filter/Algo/forEach.hpp
+++ daemon/filter/Filter/Algo/forEach.hpp
@@ -1,5 +1,5 @@
 /*
- * ForEach.hpp
+ * forEach.hpp
  *
  */
 #ifndef INCLUDE_FILTER_ALGO_FOREACH_HPP_FILE
@@ -13,7 +13,12 @@ namespace Filter
 {
 namespace Algo
 {
-
+/** \brief traverses all elements in a given range.
+ *  \param begin first iterator to check.
+ *  \param end   end-indicating iterator.
+ *  \param f     function object to apply to each element.
+ *  \note algorithm does not go deeper into tree.
+ */
 template<typename Iter, typename FuncObj>
 FuncObj forEach(Iter begin, Iter end, FuncObj f)
 {
