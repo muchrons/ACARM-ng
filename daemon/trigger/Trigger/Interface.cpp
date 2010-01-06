eadd5a4a23d4d8edd090894fce30d6ad6da5c90f
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-01-06 10:39:29 +0100

    implemented and tested trigger's interface.
    added:
      daemon/trigger/Trigger/Interface.cpp
      daemon/trigger/Trigger/Interface.hpp
      daemon/trigger/Trigger/Interface.t.cpp

---
+++ daemon/trigger/Trigger/Interface.cpp
@@ -0,0 +1,66 @@
+/*
+ * Interface.cpp
+ *
+ */
+#include <boost/thread.hpp>
+#include <cassert>
+
+#include "Logger/Logger.hpp"
+#include "Trigger/Interface.hpp"
+
+using namespace std;
+
+namespace Trigger
+{
+
+Interface::~Interface(void)
+{
+  LOGMSG_INFO(log_, "deallocating trigger");
+}
+
+void Interface::process(Persistency::GraphNodePtrNN n)
+{
+  LOGMSG_DEBUG_S(log_)<<"processing node at address 0x"
+                      <<static_cast<void*>( n.get() );
+  // clean-up old entries
+  nos_.prune();
+
+  // check if node should be processed at all
+  if( !matchCriteria(*n) )
+  {
+    LOGMSG_DEBUG_S(log_)<<"node at address 0x"
+                        <<static_cast<void*>( n.get() )
+                        <<" does not match criteria";
+    return;
+  }
+
+  // process node
+  nos_.add(n);
+  LOGMSG_INFO_S(log_)<<"calling trigger for node at address 0x"
+                     <<static_cast<void*>( n.get() );
+  trigger(*n);
+}
+
+
+namespace
+{
+inline Logger::NodeName makeNodeName(const string &name)
+{
+  const string &out="trigger." + name;
+  return Logger::NodeName( out.c_str() );
+} // makeNodeName()
+} // unnamed namespace
+
+Interface::Interface(const std::string &name):
+  log_( makeNodeName(name) ),
+  name_(name)
+{
+  LOGMSG_INFO(log_, "creating trigger");
+}
+
+void Interface::interruptionPoint(void)
+{
+  boost::this_thread::interruption_point();
+}
+
+} // namespace Filter
