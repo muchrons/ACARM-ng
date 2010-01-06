0d81f31acd7319200547cb5b9931a2f6a6112bb4
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-01-06 16:06:08 +0100

    implemented template interface's implementation to be used as an argument for processor.
    added:
      daemon/coretypes/Core/Types/Proc/InterfaceImpl.hpp

---
+++ daemon/coretypes/Core/Types/Proc/InterfaceImpl.hpp
@@ -0,0 +1,56 @@
+/*
+ * InterfaceImpl.hpp
+ *
+ */
+#ifndef INCLUDE_CORE_TYPES_PROC_INTERFACEIMPL_HPP_FILE
+#define INCLUDE_CORE_TYPES_PROC_INTERFACEIMPL_HPP_FILE
+
+/* public header */
+
+#include "Core/Types/Proc/Interface.hpp"
+
+namespace Core
+{
+namespace Types
+{
+namespace Proc
+{
+
+// TODO: test
+
+
+/** \brief template-based implementation of interface for different strategies.
+ */
+template<typename TStrategy>
+class InterfaceImpl: public Interface
+{
+public:
+  /** \brief creates object with a given name.
+   *  \param name name for this processing unit.
+   */
+  InterfaceImpl(const std::string &name,
+                const TStrategy   &strategy):
+    Interface(name),
+    strategy_(strategy)
+  {
+  }
+
+  /** \brief call to process data in derived class.
+   *  \param node         node to process.
+   *  \param changedNodes output list of nodes that were chenged/created
+   *                      during this call. initially list is empty.
+   */
+  virtual void process(Node node, ChangedNodes &changedNodes)
+  {
+    strategy_.process(node, changedNodes);
+  }
+
+private:
+  TStrategy strategy_;
+}; // class Interface
+
+} // namespace Proc
+} // namespace Types
+} // namespace Core
+
+#endif
