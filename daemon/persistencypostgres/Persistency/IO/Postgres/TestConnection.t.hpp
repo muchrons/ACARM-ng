c5ddd233f173cca0cd33f2cb7a180e90b7728dfd
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-01-11 16:33:31 +0100

    implemented helper test object for connecting with data base.
    added:
      daemon/persistencypostgres/Persistency/IO/Postgres/TestConnection.t.cpp
      daemon/persistencypostgres/Persistency/IO/Postgres/TestConnection.t.hpp

---
+++ daemon/persistencypostgres/Persistency/IO/Postgres/TestConnection.t.hpp
@@ -0,0 +1,35 @@
+/*
+ * TestConnection.t.hpp
+ *
+ */
+#ifndef INCLUDE_PERSISTENCY_IO_POSTGRES_TESTCONNECTION_T_HPP_FILE
+#define INCLUDE_PERSISTENCY_IO_POSTGRES_TESTCONNECTION_T_HPP_FILE
+
+#include "Persistency/IO/Postgres/DBHandler.hpp"
+
+namespace Persistency
+{
+namespace IO
+{
+namespace Postgres
+{
+
+struct TestConnection
+{
+  TestConnection(void):
+    connParams_("localhost", "acarm_ng_test", "acarm-ng-tests", "test.password"),
+    idCache_(new IDCache),
+    dbHandler_(connParams_, idCache_)
+  {
+  }
+
+  DBConnection::Parameters connParams_;
+  IDCachePtrNN             idCache_;
+  DBHandler                dbHandler_;
+}; // struct TestConnection
+
+} // namespace Postgres
+} // namespace IO
+} // namespace Persistency
+
+#endif
