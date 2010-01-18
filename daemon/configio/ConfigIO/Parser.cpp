cc7ce076ba029fc91a2492a79eafcdc716c9b295
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-01-18 15:47:45 +0100

    doxygen fixed.
    modified:
      daemon/configio/ConfigIO/Parser.cpp

--- daemon/configio/ConfigIO/Parser.cpp
+++ daemon/configio/ConfigIO/Parser.cpp
@@ -16,6 +16,10 @@ namespace ConfigIO
 
 namespace
 {
+/** \brief gets XML tree after parsing a given file.
+ *  \param path path to file to be parsed.
+ *  \return parsed XML tree.
+ */
 inline Tree getParsedTree(const string &path)
 {
   FileReader       reader(path);
@@ -24,7 +28,7 @@ inline Tree getParsedTree(const string &path)
 } // getParsedTree()
 } // unnamed namespace
 
-Parser::Parser(const string &path):
+Parser::Parser(const std::string &path):
     tree_( getParsedTree(path) ),
     parseNodes_(       tree_.getRoot().getChild("logger").getChild("nodes") ),
     parseAppenders_(   tree_.getRoot().getChild("logger").getChild("appenders") ),
