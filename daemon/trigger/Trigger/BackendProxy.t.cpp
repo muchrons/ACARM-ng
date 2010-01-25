2d719899a28e91b59a557895bc922982965b1311
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-01-25 09:00:43 +0100

    BackendProxy implemented for triggers
    added:
      daemon/trigger/Trigger/BackendProxy.cpp
      daemon/trigger/Trigger/BackendProxy.hpp
      daemon/trigger/Trigger/BackendProxy.t.cpp

---
+++ daemon/trigger/Trigger/BackendProxy.t.cpp
@@ -0,0 +1,50 @@
+/*
+ * BackendProxy.t.cpp
+ *
+ */
+#include <tut.h>
+
+#include "Trigger/BackendProxy.hpp"
+#include "TestHelpers/Persistency/TestStubs.hpp"
+#include "TestHelpers/Persistency/TestHelpers.hpp"
+#include "Persistency/IO/BackendFactory.hpp"
+
+using namespace Trigger;
+using namespace Persistency;
+
+namespace
+{
+struct TestClass: private TestHelpers::Persistency::TestStubs
+{
+  TestClass(void):
+    conn_( IO::create() ),
+    bp_( new BackendProxy(conn_, "sometest") )
+  {
+    assert( bp_.get()!=NULL );
+    assert( conn_.get()!=NULL );
+  }
+
+  IO::ConnectionPtrNN             conn_;
+  boost::scoped_ptr<BackendProxy> bp_;
+};
+
+typedef TestClass TestClass;
+typedef tut::test_group<TestClass> factory;
+typedef factory::object testObj;
+
+factory tf("Trigger/BackendProxy");
+} // unnamed namespace
+
+
+namespace tut
+{
+
+// smoke-test for marking as triggered
+template<>
+template<>
+void testObj::test<1>(void)
+{
+  bp_->markAsTriggered( TestHelpers::Persistency::makeNewMetaAlert() );
+}
+
+} // namespace tut
