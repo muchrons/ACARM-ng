050320fa323f4ebd70fdcc91c008944438d09089
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-01-27 13:18:35 +0100

    added stream-like_logging.
    modified:
      daemon/logger/Logger/Logger.mt.cpp

--- daemon/logger/Logger/Logger.mt.cpp
+++ daemon/logger/Logger/Logger.mt.cpp
@@ -2,8 +2,6 @@
 
 #include "Logger/Logger.hpp"
 
-using namespace Logger;
-
 void w(void)
 {
   usleep(10*1000);
@@ -11,13 +9,21 @@ void w(void)
 
 int main(void)
 {
-  Node n("some.node");
+  Logger::Node n("some.node");
 
+  // basic logging
   LOGMSG_DEBUG(n, "hello debug"  ); w();
   LOGMSG_INFO (n, "hello info"   ); w();
   LOGMSG_WARN (n, "hello warning"); w();
   LOGMSG_ERROR(n, "hello error"  ); w();
   LOGMSG_FATAL(n, "hello fatal"  ); w();
+
+  // stream logging
+  LOGMSG_DEBUG_S(n)<<"hello debug";   w();
+  LOGMSG_INFO_S (n)<<"hello info";    w();
+  LOGMSG_WARN_S (n)<<"hello warning"; w();
+  LOGMSG_ERROR_S(n)<<"hello error";   w();
+  LOGMSG_FATAL_S(n)<<"hello fatal";   w();
 
   return 0;
 }
