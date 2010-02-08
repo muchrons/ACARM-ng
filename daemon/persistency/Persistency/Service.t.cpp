53d986d720c6af43828c0bedf3aa6642881537ea
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-02-08 16:00:22 +0100

    implemented == and != in Service.
    modified:
      daemon/persistency/Persistency/Service.cpp
      daemon/persistency/Persistency/Service.hpp
      daemon/persistency/Persistency/Service.t.cpp

--- daemon/persistency/Persistency/Service.t.cpp
+++ daemon/persistency/Persistency/Service.t.cpp
@@ -15,6 +15,12 @@ namespace
 
 struct TestClass: private TestBase
 {
+  TestClass(void):
+    custom_("mail daemon", 25, "smtp", makeNewReferenceURL() )
+  {
+  }
+
+  const Service custom_;
 };
 
 typedef TestClass TestClass;
@@ -33,11 +39,10 @@ template<>
 template<>
 void testObj::test<1>(void)
 {
-  const Service ti("mail daemon", 25, "smtp", makeNewReferenceURL() );
-  ensure_equals("invalid name",    ti.getName().get(),     string("mail daemon") );
-  ensure_equals("invalid port",    ti.getPort(),           25                    );
-  ensure_equals("invalid service", ti.getProtocol().get(), string("smtp")        );
-  ensure("invalid url",            ti.getReferenceURL()!=NULL);
+  ensure_equals("invalid name",    custom_.getName().get(),     string("mail daemon") );
+  ensure_equals("invalid port",    custom_.getPort(),           25                    );
+  ensure_equals("invalid service", custom_.getProtocol().get(), string("smtp")        );
+  ensure("invalid url",            custom_.getReferenceURL()!=NULL);
 }
 
 // test NULL protocol
@@ -62,6 +67,51 @@ void testObj::test<3>(void)
   ensure_equals("invalid port",    ti.getPort(),           25                    );
   ensure_equals("invalid service", ti.getProtocol().get(), string("smtp")        );
   ensure("invalid url",            ti.getReferenceURL()==NULL);
+}
+
+// test comaprison
+template<>
+template<>
+void testObj::test<4>(void)
+{
+  const Service ti("mail daemon", 25, "smtp", makeNewReferenceURL() );
+  ensure("instances differ", custom_==ti);
+}
+
+// test comaprison when name differs
+template<>
+template<>
+void testObj::test<5>(void)
+{
+  const Service ti("different mail daemon", 25, "smtp", makeNewReferenceURL() );
+  ensure("instances don't differ", custom_!=ti);
+}
+
+// test comaprison when port differs
+template<>
+template<>
+void testObj::test<6>(void)
+{
+  const Service ti("mail daemon", 42, "smtp", makeNewReferenceURL() );
+  ensure("instances don't differ", custom_!=ti);
+}
+
+// test comaprison when protocol differs
+template<>
+template<>
+void testObj::test<7>(void)
+{
+  const Service ti("mail daemon", 25, "narf", makeNewReferenceURL() );
+  ensure("instances don't differ", custom_!=ti);
+}
+
+// test comaprison when reference URL differs
+template<>
+template<>
+void testObj::test<8>(void)
+{
+  const Service ti("mail daemon", 25, "smtp", ReferenceURLPtr() );
+  ensure("instances don't differ", custom_!=ti);
 }
 
 } // namespace tut
