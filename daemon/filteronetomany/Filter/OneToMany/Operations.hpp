f4e184691474dd8ce6e0fa560b203fc7f124f2bd
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-02-23 16:26:22 +0100

    added helper object for doing necessary operations (not yet implemented).
    added:
      daemon/filteronetomany/Filter/OneToMany/Operations.hpp
      daemon/filteronetomany/Filter/OneToMany/Operations.t.cpp

---
+++ daemon/filteronetomany/Filter/OneToMany/Operations.hpp
@@ -0,0 +1,41 @@
+/*
+ * Operations.hpp
+ *
+ */
+#ifndef INCLUDE_FILTER_ONETOMANY_OPERATIONS_HPP_FILE
+#define INCLUDE_FILTER_ONETOMANY_OPERATIONS_HPP_FILE
+
+#include "Filter/HostCommon/Strategy.hpp"
+
+namespace Filter
+{
+namespace OneToMany
+{
+
+/** \brief helpers oprations implementation.
+ */
+struct Operations
+{
+private:
+  typedef Filter::HostCommon::Strategy Types;
+
+public:
+  /** \brief computes reoprted host and downs it to one.
+   *  \param node node to compute them for.
+   *  \return host pointer or NULL if not correlation has been found.
+   */
+  static Persistency::HostPtr getReportedHost(const Types::Node node)
+  {
+    // TODO
+  }
+
+  static Persistency::MetaAlert::Name getMetaAlertName(const Persistency::HostPtrNN h)
+  {
+    // TODO
+  }
+}; // class Strategy
+
+} // namespace OneToMany
+} // namespace Filter
+
+#endif
