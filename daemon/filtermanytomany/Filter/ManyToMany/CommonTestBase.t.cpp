503d7f3f6b07da69e25e16af03b8639f449094bb
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-03-02 12:10:39 +0100

    implemented common elements for test cases in single class.
    added:
      daemon/filtermanytomany/Filter/ManyToMany/CommonTestBase.t.cpp
      daemon/filtermanytomany/Filter/ManyToMany/CommonTestBase.t.hpp

---
+++ daemon/filtermanytomany/Filter/ManyToMany/CommonTestBase.t.cpp
@@ -0,0 +1,62 @@
+/*
+ * CommonTestBase.t.cpp
+ *
+ */
+#include "Filter/ManyToMany/CommonTestBase.t.hpp"
+#include <cassert>
+
+using namespace Persistency;
+using namespace TestHelpers::Persistency;
+
+namespace Filter
+{
+namespace ManyToMany
+{
+
+HostPtrNN CommonTestBase::makeHost(const char *host) const
+{
+  assert(host!=NULL);
+  const Host::Netmask_v4  mask(mask4_bytes);
+  const char             *os="Linux";
+  return makeNewHost4(host, &mask, os);
+}
+
+AlertPtrNN CommonTestBase::makeAlert(const char *hostSrc1,
+                                     const char *hostSrc2,
+                                     const char *hostDst1,
+                                     const char *hostDst2) const
+{
+  const Alert::SourceAnalyzers  sa( makeNewAnalyzer() );
+  Alert::ReportedHosts          hostsSrc;
+  Alert::ReportedHosts          hostsDst;
+
+  if(hostSrc1!=NULL)
+    hostsSrc.push_back( makeHost(hostSrc1) );
+  if(hostSrc2!=NULL)
+    hostsSrc.push_back( makeHost(hostSrc2) );
+
+  if(hostDst1!=NULL)
+    hostsDst.push_back( makeHost(hostDst1) );
+  if(hostDst2!=NULL)
+    hostsDst.push_back( makeHost(hostDst2) );
+
+  return AlertPtrNN( new Alert("alert 123",
+                               sa,
+                               NULL,
+                               Timestamp(),
+                               Severity(SeverityLevel::INFO),
+                               Certainty(0.42),
+                               "some test allert",
+                               hostsSrc,
+                               hostsDst) );
+}
+
+GraphNodePtrNN CommonTestBase::makeLeaf(AlertPtrNN alert) const
+{
+  IO::ConnectionPtrNN conn=IO::create();
+  IO::Transaction     t( conn->createNewTransaction("make_leaf_trans") );
+  return GraphNodePtrNN( new GraphNode(alert, conn, t) );
+}
+
+} // namespace ManyToMany
+} // namespace Filter
