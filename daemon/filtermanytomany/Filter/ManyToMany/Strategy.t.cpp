27038546fec5089360eeefa7314514a044376ece
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-03-02 17:54:00 +0100

    testing ManyToMany strategy in progress.
    modified:
      daemon/filtermanytomany/Filter/ManyToMany/Strategy.t.cpp

--- daemon/filtermanytomany/Filter/ManyToMany/Strategy.t.cpp
+++ daemon/filtermanytomany/Filter/ManyToMany/Strategy.t.cpp
@@ -18,64 +18,17 @@ using namespace TestHelpers::Persistency;
 namespace
 {
 
-#if 0
-struct TestStrategy: public Strategy
+struct TestClass: public CommonTestBase
 {
-  TestStrategy(void):
-    Strategy("teststrategy", 42)
+  TestClass(void):
+    sampleLeaf_( makeLeaf( makeAlert("1.2.3.4", "2.3.4.5",
+                                     "1.2.3.4", "9.8.7.6") ) )
   {
   }
 
-  virtual const Persistency::Alert::ReportedHosts &getReportedHostsArray(
-                                                          const Node node) const
-  {
-    return node->getAlert().getReportedSourceHosts();
-  }
-
-  virtual MetaAlert::Name getMetaAlertName(const HostPtrNN /*h*/) const
-  {
-    return Persistency::MetaAlert::Name("hakuna matata");
-  }
-}; // struct TestStrategy
-#endif
-
-
-struct TestClass: private CommonTestBase
-{
-  /*
-  AlertPtrNN makeAlertWithHost(const char *host, const char *os) const
-  {
-    const Alert::SourceAnalyzers sa( makeNewAnalyzer() );
-    const Host::Netmask_v4       mask(mask4_bytes);
-    Alert::ReportedHosts         hosts;
-    hosts.push_back( makeNewHost4(host, &mask, os) );
-    return AlertPtrNN( new Alert("alert 123",
-                                 sa,
-                                 NULL,
-                                 Timestamp(),
-                                 Severity(SeverityLevel::INFO),
-                                 Certainty(0.42),
-                                 "some test allert",
-                                 hosts,
-                                 Alert::ReportedHosts() ) );
-  }
-
-  TestStrategy::Node makeLeaf(const char *host, const char *os="Linux") const
-  {
-    assert(host!=NULL);
-    IO::ConnectionPtrNN conn=IO::create();
-    IO::Transaction     t( conn->createNewTransaction("make_leaf_trans") );
-    return GraphNodePtrNN( new GraphNode( makeAlertWithHost(host, os), conn, t) );
-  }
-
-  TestStrategy::Node makeNode(void) const
-  {
-    return makeNewNode( makeLeaf("1.2.3.4"), makeLeaf("1.2.3.4") );
-  }
-
-  TestStrategy::ChangedNodes changed_;
-  TestStrategy               ts_;
-  */
+  GraphNodePtrNN         sampleLeaf_;
+  Strategy::ChangedNodes changed_;
+  Strategy               s_;
 };
 
 typedef tut::test_group<TestClass> factory;
@@ -88,106 +41,67 @@ factory tf("Filter/ManyToMany/Strategy");
 namespace tut
 {
 
-#if 0
-// test runs when hosts differ
+// test if entry is not interesting when no target hosts are set
 template<>
 template<>
 void testObj::test<1>(void)
 {
-  const char *tab[]={"1.2.3.4", "2.3.4.5"};
-  for(int i=0; i<2; ++i)
-  {
-    const TestStrategy::Node tmp=makeLeaf(tab[i]);
-    ts_.process(tmp, changed_);
-    ensure_equals("some nodes have been changed", changed_.size(), 0);
-  }
+  GraphNodePtrNN sourceOnlyLeaf( makeLeaf( makeAlert("1.2.3.4", NULL,
+                                                     NULL,      NULL) ) );
+  s_.process(sourceOnlyLeaf, changed_);
+  ensure_equals("some nodes have been changed / 1", changed_.size(), 0);
+
+  s_.process(sampleLeaf_, changed_);
+  ensure_equals("some nodes have been changed / 2", changed_.size(), 0);
 }
 
-// test running when self-entry has been found
+// test if entry is not interesting when no source hosts are set
 template<>
 template<>
 void testObj::test<2>(void)
 {
-  TestStrategy::Node n=makeLeaf("6.6.6.6");
-  for(int i=0; i<2; ++i)
-  {
-    ts_.process(n, changed_);
-    ensure_equals("some nodes have been changed", changed_.size(), 0);
-  }
+  GraphNodePtrNN targetOnlyLeaf( makeLeaf( makeAlert(NULL,      NULL,
+                                                     "1.2.3.4", NULL) ) );
+  s_.process(targetOnlyLeaf, changed_);
+  ensure_equals("some nodes have been changed / 1", changed_.size(), 0);
+
+  s_.process(sampleLeaf_, changed_);
+  ensure_equals("some nodes have been changed / 2", changed_.size(), 0);
 }
 
-// test when no common host in node is found.
+// test canCorrelate method
 template<>
 template<>
 void testObj::test<3>(void)
 {
-  for(int i=0; i<2; ++i)
-  {
-    ts_.process( makeNewLeaf(), changed_);
-    ensure_equals("some nodes have been changed", changed_.size(), 0);
-  }
+  GraphNodePtrNN tmp( makeLeaf( makeAlert("6.6.3.4", "2.3.4.5",
+                                          "6.6.3.4", "9.8.7.6") ) );
+  s_.process(tmp, changed_);
+  ensure_equals("some nodes have been changed / 1", changed_.size(), 0);
+
+  s_.process(sampleLeaf_, changed_);
+  ensure_equals("some nodes have been changed / 2", changed_.size(), 1);
 }
 
-// try correlating two hosts, with different os'es
+// 
 template<>
 template<>
 void testObj::test<4>(void)
 {
-  TestStrategy::Node l1=makeLeaf("1.2.3.4", "os2");
-  ts_.process(l1, changed_);
-  ensure_equals("some nodes have been changed", changed_.size(), 0);
-
-  TestStrategy::Node l2=makeLeaf("1.2.3.4", "os2");
-  ts_.process(l2, changed_);
-  ensure_equals("no correlation has been done", changed_.size(), 1);
-  // comapre pointers - new node should have been created here
-  ensure("invalid element in changed list",
-             changed_.begin()->get()!=l1.get() &&
-             changed_.begin()->get()!=l2.get()    );
-  ensure_equals("invalid name",
-                (*changed_.begin())->getMetaAlert()->getName().get(),
-                string("hakuna matata") );
 }
 
-namespace
-{
-struct ThrowStrategy: public TestStrategy
-{
-  virtual MetaAlert::Name getMetaAlertName(const HostPtrNN /*h*/) const
-  {
-    throw Filter::ManyToMany::Exception(SYSTEM_SAVE_LOCATION, "just", "testing");
-    return "alicehasacat";  // we never reach here
-  }
-}; // struct ThrowStrategy
-} // unnamed namespace
-
-// test if exceptions inside the loop does not get outside!
+// 
 template<>
 template<>
 void testObj::test<5>(void)
 {
-  ThrowStrategy ts;
-  ts.process( makeLeaf("1.2.3.4", "os1"), changed_);
-  ensure_equals("some nodes have been changed / 1", changed_.size(), 0);
-  ts.process( makeLeaf("1.2.3.4", "os2"), changed_);    // nothing should happen here
-  ensure_equals("some nodes have been changed / 2", changed_.size(), 0);
 }
 
-// test adding elements to already exisitng node.
+// 
 template<>
 template<>
 void testObj::test<6>(void)
 {
-  TestStrategy::Node n=makeNode();
-  ts_.process(n, changed_);
-  ensure_equals("some nodes have been changed", changed_.size(), 0);
-
-  TestStrategy::Node l=makeLeaf("1.2.3.4");
-  ts_.process(l, changed_);
-  ensure_equals("nodes not changed", changed_.size(), 1);
-  // check pointers
-  ensure("invalid node has been changed", changed_.begin()->get()==n.get() );
 }
-#endif
 
 } // namespace tut
