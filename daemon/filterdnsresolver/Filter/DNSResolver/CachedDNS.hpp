b590449347c23b4b4ca79bf59f0a45bcd87bdec9
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-03-09 09:41:59 +0100

    doxygen fixed.
    modified:
      daemon/filterdnsresolver/Filter/DNSResolver/CachedDNS.hpp
      daemon/filterdnsresolver/Filter/DNSResolver/CachedEntry.hpp

--- daemon/filterdnsresolver/Filter/DNSResolver/CachedDNS.hpp
+++ daemon/filterdnsresolver/Filter/DNSResolver/CachedDNS.hpp
@@ -16,16 +16,34 @@ namespace Filter
 namespace DNSResolver
 {
 
+/** \brief DNS cache facility.
+ */
 class CachedDNS: private boost::noncopyable
 {
 public:
+  /** \brief repreesntation of IP. */
   typedef Persistency::Host::IP              IP;
+  /** \brief DNS name and information if given IP has reverse DNS. */
   typedef std::pair<bool, CachedEntry::Name> Entry;
 
+  /** \brief create cache with a given timeout.
+   *  \param timeout time for each cached entry to exist in cache.
+   */
   explicit CachedDNS(unsigned int timeout);
-
+  /** \brief get reverse-dns for given IP.
+   *  \param ip ip to query for.
+   *  \return pair with bool value telling if IP has reverse DNS and
+   *          name, if present.
+   *
+   *  this call is not const, since it caches obtained results. if given
+   *  ip appears more often in a short time window (defined by timeout value)
+   *  it is read from cache, instead of being re-queried. this make reverse-dns
+   *  mapping much faster.
+   */
   Entry operator[](const IP &ip);
-
+  /** \brief remove outdated entries from cache.
+   *  \note this is a separete call for performance reasons.
+   */
   void prune(void);
 
 private:
