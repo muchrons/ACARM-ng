848f2816cfe12d9754a45fe3cbd071ad886992be
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-03-10 11:11:28 +0100

    code has been commented.
    modified:
      daemon/filteripblacklist/Filter/IPBlackList/BlackList.hpp

--- daemon/filteripblacklist/Filter/IPBlackList/BlackList.hpp
+++ daemon/filteripblacklist/Filter/IPBlackList/BlackList.hpp
@@ -14,18 +14,28 @@ namespace Filter
 namespace IPBlackList
 {
 
+/** \brief class repreesnting collection of blacklisted IPs.
+ */
 class BlackList
 {
 public:
+  /** \brief IP type. */
   typedef Persistency::IPTypes<BlackList>::IP IP;
 
+  /** \brief create instance.
+   *  \param begin first iterator to read entry from.
+   *  \param end   end iterator.
+   */
   template<typename TForwardIterator>
   BlackList(const TForwardIterator begin, const TForwardIterator end)
   {
     for(TForwardIterator it=begin; it!=end; ++it)
       bl_.insert(*it);
   }
-
+  /** \brief checks if igven IP is balcklisted.
+   *  \param ip IP to be checked.
+   *  \return true if IP is blacklisted, false otherwise.
+   */
   bool operator[](const IP &ip) const;
 
 private:
