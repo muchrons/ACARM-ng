9710e3aeec9f71be484a570179bdbe85e3fe3dfb
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-03-16 11:59:57 +0100

    added test for throw on non-exisitng input type.
    added:
      daemon/input/features/testdata/invalid_input.xml
    modified:
      daemon/input/Input/Factory.t.cpp

--- daemon/input/Input/Factory.t.cpp
+++ daemon/input/Input/Factory.t.cpp
@@ -4,6 +4,7 @@
  */
 #include <tut.h>
 
+#include "ConfigIO/Singleton.hpp"
 #include "Input/Factory.hpp"
 
 using namespace std;
@@ -34,6 +35,24 @@ void testObj::test<1>(void)
   Core::Types::AlertsFifo q;
   InputsCollection        c=create(q);
   ensure_equals("some elements are found", c.size(), 0);
+}
+
+// test throw when non-existing input type is requested.
+template<>
+template<>
+void testObj::test<2>(void)
+{
+  ConfigIO::Singleton::get()->rereadConfig("testdata/invalid_input.xml");
+  Core::Types::AlertsFifo q;
+  try
+  {
+    InputsCollection c=create(q);
+    fail("create() didn't throw when requested non-existing input");
+  }
+  catch(const std::runtime_error &)
+  {
+    // this is expected
+  }
 }
 
 } // namespace tut
