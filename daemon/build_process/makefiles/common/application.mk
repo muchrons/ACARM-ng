0dc46c1fa5347887b4784ddb75339184cfad08e4
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-04-12 16:57:11 +0200

    development version of build_process, with more prallelism has been introduced.
    modified:
      daemon/build_process/makefiles/basic_mks/AutoToolsComponent
      daemon/build_process/makefiles/common/application.mk
      daemon/build_process/makefiles/common/lib_app_common.mk
      daemon/build_process/makefiles/common/library.mk
      daemon/build_process/makefiles/common/main_macros.mk
      daemon/build_process/makefiles/common/targets.mk
      daemon/build_process/makefiles/component_mods/no_doxygen.mk
      daemon/build_process/scripts/getdeps
      daemon/build_process/templates/build_config/config.mk

--- daemon/build_process/makefiles/common/application.mk
+++ daemon/build_process/makefiles/common/application.mk
@@ -1,6 +1,7 @@
-# note: public_headers are NOT necessary for applications
+# note: public_headers are NOT needed for applications
+
 .PHONY: all
-all:: $(APPLICATION_NAME)
+all:: all-$(COMPONENT_PART)
 
 include $(MAKEFILES_COMMON_BASE_DIR)/lib_app_common.mk
 
@@ -15,3 +16,9 @@ $(APPLICATION_NAME):: $(LIBRARY_NAME) $(MAIN_OBJS) $(LIBS_GEN_DEPS)
 	  $(STRIP) $@ || rm -f "$@" ; \
 	fi
 
+.PHONY: all-MKPUBLIC
+all-MKPUBLIC::
+.PHONY: all-OBJECTS
+all-OBJECTS:: $(LIBRARY_OBJ_DEPS) $(MAIN_OBJS)
+.PHONY: all-LINK
+all-LINK:: $(APPLICATION_NAME)
