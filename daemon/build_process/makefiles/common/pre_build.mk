c2198af52cca36fbd13f2b453a6a9a6031f7622a
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-04-19 16:45:15 +0200

    added build_process with support for 'features'.
    added:
      daemon/build_process/templates/features/modes/HOWTO.txt
      daemon/build_process/templates/features/modes/features/
    modified:
      daemon/build_process/makefiles/basic_mks/ProjectMakefile
      daemon/build_process/makefiles/common/dirs.mk
      daemon/build_process/makefiles/common/dirs_features.mk
      daemon/build_process/makefiles/common/pre_build.mk
      daemon/build_process/makefiles/common/source_finder.mk
      daemon/build_process/makefiles/common/targets.mk
      daemon/build_process/templates/build_config/config.mk

--- daemon/build_process/makefiles/common/pre_build.mk
+++ daemon/build_process/makefiles/common/pre_build.mk
@@ -3,4 +3,8 @@ include $(MAKEFILES_COMMON_BASE_DIR)/version_check.mk
 
 # force doing some extra stuff before building starts
 .PHONY: pre_build
-pre_build: $(BUILD_PROCESS_VERSION_TIMESTAMP)
+pre_build: $(BUILD_PROCESS_VERSION_TIMESTAMP) $(GEN_WORK_ROOT)/features_build_list.txt
+
+# write down list of features buing build in given directory
+$(GEN_WORK_ROOT)/features_build_list.txt:
+	@echo "$(FEATURES)" | xargs -n 1 echo | sort | uniq > "$@"
