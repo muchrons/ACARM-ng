4618bddc3d1f2d7c3004a08f976ea1cb66fbd352
Wojciech Waga <wojek@libra>
2010-04-22 13:33:56 +0200

    modified:   daemon/inputprelude/Input/Prelude/IDMEFParserAnalyzer.hpp

--- daemon/inputprelude/Input/Prelude/IDMEFParserAnalyzer.hpp
+++ daemon/inputprelude/Input/Prelude/IDMEFParserAnalyzer.hpp
@@ -5,7 +5,7 @@
 #ifndef INCLUDE_INPUT_PRELUDE_IDMEFPARSERANALYZER_HPP_FILE
 #define INCLUDE_INPUT_PRELUDE_IDMEFPARSERANALYZER_HPP_FILE
 
-#include "Prelude/prelude.h"
+#include "prelude.h"
 
 #include "Persistency/Alert.hpp"
 #include "Persistency/Timestamp.hpp"
