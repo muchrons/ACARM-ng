bf688cbe87860df4adbbc869b00a3c2acf939651
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-04-26 16:52:01 +0200

    ConfigIO::Preprocessor::Config implemented and tested.
    added:
      daemon/configio/ConfigIO/Preprocessor/Config.hpp
      daemon/configio/ConfigIO/Preprocessor/Config.t.cpp

---
+++ daemon/configio/ConfigIO/Preprocessor/Config.t.cpp
@@ -0,0 +1,54 @@
+/*
+ * Config.t.cpp
+ *
+ */
+#include <tut.h>
+
+#include "ConfigIO/Preprocessor/Config.hpp"
+
+using namespace std;
+using namespace ConfigIO::Preprocessor;
+
+namespace
+{
+struct TestClass
+{
+  Config::Sections makeSections(size_t size=0) const
+  {
+    Config::Sections s;
+    for(size_t i=0; i<size; ++i)
+      s.push_back( Section(Section::Type::ACCEPT, Expression::makeTerm( Rule::makeTrue() ) ) );
+    return s;
+  }
+
+};
+
+typedef tut::test_group<TestClass> factory;
+typedef factory::object testObj;
+
+factory tf("ConfigIO/Preprocessor/Config");
+} // unnamed namespace
+
+
+namespace tut
+{
+
+// test making empty sections.
+template<>
+template<>
+void testObj::test<1>(void)
+{
+  const Config c( makeSections() );
+  ensure_equals("invalid elemsnts count", c.getSections().size(), 0u);
+}
+
+// test non-empty section
+template<>
+template<>
+void testObj::test<2>(void)
+{
+  const Config c( makeSections(42) );
+  ensure_equals("invalid elemsnts count", c.getSections().size(), 42u);
+}
+
+} // namespace tut
