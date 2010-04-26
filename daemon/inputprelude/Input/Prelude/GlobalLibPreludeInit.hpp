b69b0795d72e8a70468857007de6578c6de3b4e5
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-04-26 12:33:48 +0200

    doxygen for GlobalLibPreludeInit fixed.
    modified:
      daemon/inputprelude/Input/Prelude/GlobalLibPreludeInit.hpp

--- daemon/inputprelude/Input/Prelude/GlobalLibPreludeInit.hpp
+++ daemon/inputprelude/Input/Prelude/GlobalLibPreludeInit.hpp
@@ -10,10 +10,23 @@ namespace Input
 namespace Prelude
 {
 
+/** \brief helper object for safe initialization of lib-prelude, that cannot
+ *         be re-initialized at runtime.
+ *
+ *  class does automatic prelude initialization uppon first usage, and ensures
+ *  proper deinitialization. it ensures that no more than one init will be
+ *  done, since prelude does not handle it correctly.
+ *
+ *  \note class is thread safe.
+ */
 class GlobalLibPreludeInit
 {
 public:
+  /** \brief initialize prelude, if not already initialized.
+   */
   GlobalLibPreludeInit(void);
+  /** \brief uninitialize prelude, if needed.
+   */
   ~GlobalLibPreludeInit(void);
 }; // class GlobalLibPreludeInit
 
