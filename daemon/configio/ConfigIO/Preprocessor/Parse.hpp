c0ee9b09d86749c0558caa3966fb44dcf07e60cb
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-04-26 18:09:36 +0200

    doxygen fixed.
    modified:
      daemon/configio/ConfigIO/Preprocessor/Parse.hpp

--- daemon/configio/ConfigIO/Preprocessor/Parse.hpp
+++ daemon/configio/ConfigIO/Preprocessor/Parse.hpp
@@ -5,8 +5,6 @@
 #ifndef INCLUDE_CONFIGIO_PREPROCESSOR_PARSE_HPP_FILE
 #define INCLUDE_CONFIGIO_PREPROCESSOR_PARSE_HPP_FILE
 
-// TODO: comment this code
-
 #include "XML/Node.hpp"
 #include "ConfigIO/ExceptionParseError.hpp"
 #include "ConfigIO/Preprocessor/Config.hpp"
@@ -21,8 +19,13 @@ namespace Preprocessor
 class Parse
 {
 public:
+  /** \brief parses configuration sub-tree.
+   *  \param root root node for subtree to parse.
+   */
   explicit Parse(const XML::Node &root);
-
+  /** \brief gets parsed configuration.
+   *  \return parsed configuration.
+   */
   const Config &getConfig(void) const
   {
     return cfg_;
