48a9865df152cf97ab2dd2e40f32703e8940aa01
Wojciech Waga <wojek@libra>
2010-04-26 14:37:33 +0200

    Test for commons, Client set to use GlobalLibPreludeInit
    modified:
      daemon/inputprelude/Input/Prelude/Client.cpp
      daemon/inputprelude/Input/Prelude/Client.hpp
      daemon/inputprelude/Input/Prelude/GlobalLibPreludeInit.cpp
    unknown:
      daemon/inputprelude/Input/Prelude/Client.t.cpp.orig
      daemon/inputprelude/Input/Prelude/IDMEFParserCommons.cpp
      daemon/inputprelude/Input/Prelude/IDMEFParserCommons.hpp
      daemon/inputprelude/Input/Prelude/IDMEFParserCommons.t.cpp

--- daemon/inputprelude/Input/Prelude/GlobalLibPreludeInit.cpp
+++ daemon/inputprelude/Input/Prelude/GlobalLibPreludeInit.cpp
@@ -4,8 +4,10 @@
  */
 #include <cassert>
 
+#include <prelude.h>
 #include "System/Threads/SafeInitLocking.hpp"
 #include "Input/Prelude/GlobalLibPreludeInit.hpp"
+#include "Input/Exception.hpp"
 
 namespace Input
 {
@@ -52,7 +54,11 @@ struct Releaser
     assert(g_wasInitialized==false);
     assert(g_end==false);
 
-    // TODO: prelude-init code goes here
+    int argc=1;
+    char a1[]="name";
+    char *argv[]={a1};
+    if ( prelude_init(&argc, argv) < 0 )
+      throw Input::Exception(SYSTEM_SAVE_LOCATION, "Unable to initialize prelude library.");
   }
 
   static void uninit(void)
@@ -63,7 +69,7 @@ struct Releaser
 
     try
     {
-      // TODO: prelude-release code goes here
+      prelude_deinit();
     }
     catch(...)
     {
