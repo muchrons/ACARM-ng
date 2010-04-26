205468f53329da00eaf52107e596a85be74b3b4b
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-04-26 12:18:54 +0200

    global init for prelude has been implemented.
    added:
      daemon/inputprelude/Input/Prelude/GlobalLibPreludeInit.cpp
      daemon/inputprelude/Input/Prelude/GlobalLibPreludeInit.hpp

---
+++ daemon/inputprelude/Input/Prelude/GlobalLibPreludeInit.hpp
@@ -0,0 +1,23 @@
+/*
+ * GlobalLibPreludeInit.hpp
+ *
+ */
+#ifndef INCLUDE_INPUT_PRELUDE_GLOBALLIBPRELUDEINIT_HPP_FILE
+#define INCLUDE_INPUT_PRELUDE_GLOBALLIBPRELUDEINIT_HPP_FILE
+
+namespace Input
+{
+namespace Prelude
+{
+
+class GlobalLibPreludeInit
+{
+public:
+  GlobalLibPreludeInit(void);
+  ~GlobalLibPreludeInit(void);
+}; // class GlobalLibPreludeInit
+
+} // namespace Prelude
+} // namespace Input
+
+#endif
