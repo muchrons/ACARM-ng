4645229b51b4d97835123bf00e85a57a0bc4ad33
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-04-27 16:13:54 +0200

    preprocessor's rules for getting values have been implemented (but not tested yet).
    modified:
      daemon/preprocessor/Preprocessor/Expressions/Rule.cpp
      daemon/preprocessor/Preprocessor/Expressions/Rule.hpp

--- daemon/preprocessor/Preprocessor/Expressions/Rule.hpp
+++ daemon/preprocessor/Preprocessor/Expressions/Rule.hpp
@@ -10,6 +10,7 @@
 #include "ConfigIO/Preprocessor/Rule.hpp"
 #include "Preprocessor/Checkers/Mode.hpp"
 #include "Preprocessor/Expressions/Base.hpp"
+#include "Preprocessor/ExceptionInvalidPath.hpp"
 
 // TODO: comment this code
 
@@ -30,25 +31,35 @@ public:
 private:
   typedef Path::const_iterator PathCit;
 
-  template<typename Tit>
-  bool matchEach(PathCit   t,     const PathCit pathEnd,
-                 const Tit begin, const Tit     end) const;
+  template<typename T>
+  bool processCollection(PathCit t, const T &col) const;
 
   template<typename Tit>
-  bool matchAny(PathCit   t,     const PathCit pathEnd,
-                const Tit begin, const Tit     end) const;
+  bool matchEach(PathCit t, const Tit begin, const Tit end) const;
+
+  template<typename Tit>
+  bool matchAny(PathCit t, const Tit begin, const Tit end) const;
+
+  template<typename T>
+  bool check(const T *e, PathCit t) const;
 
   bool check(const Persistency::Alert                   &e, PathCit t) const;
   bool check(const Persistency::Alert::SourceAnalyzers  &e, PathCit t) const;
   bool check(const Persistency::Analyzer                &e, PathCit t) const;
   bool check(const Persistency::Alert::ReportedHosts    &e, PathCit t) const;
+  bool check(const Persistency::ReferenceURL            &e, PathCit t) const;
   bool check(const Persistency::Host                    &e, PathCit t) const;
   bool check(const Persistency::Host::ReportedServices  &e, PathCit t) const;
   bool check(const Persistency::Service                 &e, PathCit t) const;
   bool check(const Persistency::Host::ReportedProcesses &e, PathCit t) const;
   bool check(const Persistency::Process                 &e, PathCit t) const;
+  bool check(const Persistency::MD5Sum                  &e, PathCit t) const;
   // final point
   bool check(const std::string                          &e, PathCit t) const;
+
+  void throwIfEnd(PathCit t) const;
+  void throwIfNotEnd(PathCit t) const;
+  void throwInvalid(PathCit t) const;
 
   const Path                        path_;
   boost::scoped_ptr<Checkers::Mode> checker_;
