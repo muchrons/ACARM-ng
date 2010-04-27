fb241ad4fa40e5d620cdec9465de6749a8a6f25b
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-04-27 13:20:56 +0200

    implemented base of expressions.
    added:
      daemon/preprocessor/Preprocessor/Expressions/Base.cpp
      daemon/preprocessor/Preprocessor/Expressions/Base.hpp

---
+++ daemon/preprocessor/Preprocessor/Expressions/Base.cpp
@@ -0,0 +1,17 @@
+/*
+ * Base.cpp
+ *
+ */
+#include "Preprocessor/Expressions/Base.hpp"
+
+namespace Preprocessor
+{
+namespace Expressions
+{
+
+Base::~Base(void)
+{
+}
+
+} // namespace Expressions
+} // namespace Preprocessor
---
+++ daemon/preprocessor/Preprocessor/Expressions/Base.hpp
@@ -0,0 +1,35 @@
+/*
+ * Base.hpp
+ *
+ */
+#ifndef INCLUDE_PREPROCESSOR_EXPRESSIONS_BASE_HPP_FILE
+#define INCLUDE_PREPROCESSOR_EXPRESSIONS_BASE_HPP_FILE
+
+#include <boost/noncopyable.hpp>
+
+#include "Commons/SharedPtrNotNULL.hpp"
+#include "Persistency/Alert.hpp"
+#include "Preprocessor/Path.hpp"
+
+// TODO: comment this code
+
+namespace Preprocessor
+{
+namespace Expressions
+{
+
+class Base: private boost::noncopyable
+{
+public:
+  virtual ~Base(void);
+
+  virtual bool compute(const Path &path, const Persistency::Alert &alert) const = 0;
+}; // class Base
+
+
+typedef Commons::SharedPtrNotNULL<Base> BasePtrNN;
+
+} // namespace Expressions
+} // namespace Preprocessor
+
+#endif
