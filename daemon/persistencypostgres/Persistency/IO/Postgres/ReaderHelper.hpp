f099aea304dc9aa8ef24d63fba6986a1e93fa1dc
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-04-28 11:15:26 +0200

    ReaderHelper has been split into 3 files - API and 2 with implementation helpers.
    added:
      daemon/persistencypostgres/Persistency/IO/Postgres/detail/Default.hpp
      daemon/persistencypostgres/Persistency/IO/Postgres/detail/Type.hpp
    modified:
      daemon/persistencypostgres/Persistency/IO/Postgres/ReaderHelper.hpp

--- daemon/persistencypostgres/Persistency/IO/Postgres/ReaderHelper.hpp
+++ daemon/persistencypostgres/Persistency/IO/Postgres/ReaderHelper.hpp
@@ -6,16 +6,10 @@
 #ifndef INCLUDE_PERSISTENCY_IO_POSTGRES_READERHELPER_HPP_FILE
 #define INCLUDE_PERSISTENCY_IO_POSTGRES_READERHELPER_HPP_FILE
 
-#include <string>
-#include <memory>
-#include <boost/asio/ip/address.hpp>
 #include <pqxx/pqxx>
 
-#include "Base/NullValue.hpp"
-#include "Commons/LimitedNULLString.hpp"
-#include "Persistency/Timestamp.hpp"
-#include "Persistency/MD5Sum.hpp"
-#include "Persistency/IO/Postgres/timestampFromString.hpp"
+#include "Persistency/IO/Postgres/detail/Default.hpp"
+#include "Persistency/IO/Postgres/detail/Type.hpp"
 
 namespace Persistency
 {
@@ -23,104 +17,8 @@ namespace IO
 {
 namespace Postgres
 {
-namespace detail
-{
 
-template<typename T>
-struct Default
-{
-  static inline T null(void)
-  {
-    return T(NULL);
-  }
-}; // struct Default
-
-template<typename T>
-struct Default< Base::NullValue<T> >
-{
-  static inline Base::NullValue<T> null(void)
-  {
-    return Base::NullValue<T>();
-  }
-}; // struct Default
-
-
-
-template<typename To>
-struct Type
-{
-  typedef To ReadProxy;
-
-  static inline To convert(const ReadProxy &f)
-  {
-    return To(f);
-  }
-}; // struct TypeConvert
-
-template<uint16_t N>
-struct Type< Commons::LimitedNULLString<N> >
-{
-  typedef std::string ReadProxy;
-
-  static inline Commons::LimitedNULLString<N> convert(const ReadProxy &f)
-  {
-    return Commons::LimitedNULLString<N>(f);
-  }
-}; // struct TypeConvert
-
-template<typename T>
-struct Type< Base::NullValue<T> >
-{
-  typedef typename Type<T>::ReadProxy ReadProxy;
-
-  static inline Base::NullValue<T> convert(const ReadProxy &f)
-  {
-    return Base::NullValue<T>( Type<T>::convert(f) );
-  }
-}; // struct TypeConvert
-
-template<>
-struct Type<boost::asio::ip::address>
-{
-  typedef std::string ReadProxy;
-
-  static inline boost::asio::ip::address convert(const ReadProxy &f)
-  {
-    return boost::asio::ip::address::from_string(f);
-  }
-}; // struct TypeConvert
-
-template<>
-struct Type<Persistency::Timestamp>
-{
-  typedef std::string ReadProxy;
-
-  static inline Persistency::Timestamp convert(const ReadProxy &f)
-  {
-    return timestampFromString(f);
-  }
-}; // struct TypeConvert
-
-template<>
-struct Type< std::auto_ptr<Persistency::MD5Sum> >
-{
-private:
-  typedef Persistency::MD5Sum MD5;
-  typedef std::auto_ptr<MD5>  MD5Ptr;
-
-public:
-  typedef std::string ReadProxy;
-
-  static inline MD5Ptr convert(const ReadProxy &f)
-  {
-    MD5Ptr ret( new MD5( MD5::createFromString( f.c_str() ) ) );
-    return ret;
-  }
-}; // struct TypeConvert
-
-} // namespace detail
-
-/** \brief helper class which gets values from SQL queries
+/** \brief helper class which gets values from SQL queries.
  */
 template<typename T>
 struct ReaderHelper
@@ -130,8 +28,9 @@ private:
   typedef typename Type::ReadProxy ReadProxy;
 
 public:
-  /** \brief check if SQL query is null, if not return proper value
+  /** \brief check if SQL query is null, if not return proper value.
    *  \param r SQL result field
+   *  \note default c-tor for T is expected to accept NULL paramter.
    */
   static T readAs(const pqxx::result::field &r)
   {
@@ -139,9 +38,11 @@ public:
       return detail::Default<T>::null();
     return readAsNotNull(r);
   }
+
   /** \brief get data from given SQL result field.
    *  \param r SQL result field.
    *  \return data get from SQL result field.
+   *  \note r is assumed not to be NULL here.
    */
   static T readAsNotNull(const pqxx::result::field &r)
   {
---
+++ daemon/persistencypostgres/Persistency/IO/Postgres/detail/Type.hpp
@@ -0,0 +1,171 @@
+/*
+ * Type.hpp
+ *
+ */
+
+#ifndef INCLUDE_PERSISTENCY_IO_POSTGRES_DETAIL_TYPE_HPP_FILE
+#define INCLUDE_PERSISTENCY_IO_POSTGRES_DETAIL_TYPE_HPP_FILE
+
+#include <string>
+#include <memory>
+#include <boost/asio/ip/address.hpp>
+
+#include "Base/NullValue.hpp"
+#include "Commons/LimitedNULLString.hpp"
+#include "Persistency/Timestamp.hpp"
+#include "Persistency/MD5Sum.hpp"
+#include "Persistency/IO/Postgres/timestampFromString.hpp"
+
+namespace Persistency
+{
+namespace IO
+{
+namespace Postgres
+{
+namespace detail
+{
+
+/** \brief type convertion mechanism.
+ */
+template<typename To>
+struct Type
+{
+  /** \brief helper proxy object to read data to. */
+  typedef To ReadProxy;
+
+  /** \brief converting function.
+   *  \param f value to convert from.
+   *  \return destination value type.
+   */
+  static inline To convert(const ReadProxy &f)
+  {
+    return To(f);
+  }
+}; // struct TypeConvert
+
+
+/** \brief specialization for LimitedNULLString<>.
+ */
+template<uint16_t N>
+struct Type< Commons::LimitedNULLString<N> >
+{
+  /** \brief helper proxy object to read data to. */
+  typedef std::string ReadProxy;
+
+  /** \brief converting function.
+   *  \param f value to convert from.
+   *  \return destination value type.
+   */
+  static inline Commons::LimitedNULLString<N> convert(const ReadProxy &f)
+  {
+    return Commons::LimitedNULLString<N>(f);
+  }
+}; // struct TypeConvert
+
+
+/** \brief specialization for LimitedString<>.
+ */
+template<uint16_t N>
+struct Type< Commons::LimitedString<N> >
+{
+  /** \brief helper proxy object to read data to. */
+  typedef std::string ReadProxy;
+
+  /** \brief converting function.
+   *  \param f value to convert from.
+   *  \return destination value type.
+   */
+  static inline Commons::LimitedString<N> convert(const ReadProxy &f)
+  {
+    return Commons::LimitedString<N>(f);
+  }
+}; // struct TypeConvert
+
+
+/** \brief specialization for NullValue<>.
+ */
+template<typename T>
+struct Type< Base::NullValue<T> >
+{
+  /** \brief helper proxy object to read data to. */
+  typedef typename Type<T>::ReadProxy ReadProxy;
+
+  /** \brief converting function.
+   *  \param f value to convert from.
+   *  \return destination value type.
+   */
+  static inline Base::NullValue<T> convert(const ReadProxy &f)
+  {
+    return Base::NullValue<T>( Type<T>::convert(f) );
+  }
+}; // struct TypeConvert
+
+
+/** \brief specialization for IP address.
+ */
+template<>
+struct Type<boost::asio::ip::address>
+{
+  /** \brief helper proxy object to read data to. */
+  typedef std::string ReadProxy;
+
+  /** \brief converting function.
+   *  \param f value to convert from.
+   *  \return destination value type.
+   */
+  static inline boost::asio::ip::address convert(const ReadProxy &f)
+  {
+    return boost::asio::ip::address::from_string(f);
+  }
+}; // struct TypeConvert
+
+
+/** \brief specialization for timestamp.
+ */
+template<>
+struct Type<Persistency::Timestamp>
+{
+  /** \brief helper proxy object to read data to. */
+  typedef std::string ReadProxy;
+
+  /** \brief converting function.
+   *  \param f value to convert from.
+   *  \return destination value type.
+   */
+  static inline Persistency::Timestamp convert(const ReadProxy &f)
+  {
+    return timestampFromString(f);
+  }
+}; // struct TypeConvert
+
+
+/** \brief specialization for auto_pointer to MD5.
+ */
+template<>
+struct Type< std::auto_ptr<Persistency::MD5Sum> >
+{
+private:
+  typedef Persistency::MD5Sum MD5;
+  typedef std::auto_ptr<MD5>  MD5Ptr;
+
+public:
+  /** \brief helper proxy object to read data to. */
+  typedef std::string ReadProxy;
+
+  /** \brief converting function.
+   *  \param f value to convert from.
+   *  \return destination value type.
+   */
+  static inline MD5Ptr convert(const ReadProxy &f)
+  {
+    MD5Ptr ret( new MD5( MD5::createFromString( f.c_str() ) ) );
+    return ret;
+  }
+}; // struct TypeConvert
+
+} // namespace detail
+} // namespace Postgres
+} // namespace IO
+} // namespace Persistency
+
+#endif
