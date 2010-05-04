ecb18f5a70795b8a85a65311284e4db9b2d9e572
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-05-04 16:18:54 +0200

    implementation file for LogicImpl added.
    added:
      daemon/preprocessor/Preprocessor/LogicImpl.cpp

---
+++ daemon/preprocessor/Preprocessor/LogicImpl.cpp
@@ -0,0 +1,21 @@
+/*
+ * LogicImpl.cpp
+ *
+ */
+#include "Preprocessor/LogicImpl.hpp"
+
+namespace Preprocessor
+{
+
+LogicImpl::LogicImpl(const ConfigIO::Preprocessor::Config &cfg)
+{
+  // TODO
+}
+
+bool LogicImpl::checkAccept(const Persistency::Alert &alert) const
+{
+  // TODO
+  return false;
+}
+
+} // namespace Preprocessor
