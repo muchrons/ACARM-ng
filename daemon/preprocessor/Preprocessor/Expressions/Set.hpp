9b44074343504ce31ba18071419b9d7829f54ce2
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-05-04 17:50:02 +0200

    moived common declaration from And/Or to separate header.
    added:
      daemon/preprocessor/Preprocessor/Expressions/Set.hpp
    modified:
      daemon/preprocessor/Preprocessor/Expressions/And.hpp
      daemon/preprocessor/Preprocessor/Expressions/Or.hpp

---
+++ daemon/preprocessor/Preprocessor/Expressions/Set.hpp
@@ -0,0 +1,23 @@
+/*
+ * Set.hpp
+ *
+ */
+#ifndef INCLUDE_PREPROCESSOR_EXPRESSIONS_SET_HPP_FILE
+#define INCLUDE_PREPROCESSOR_EXPRESSIONS_SET_HPP_FILE
+
+#include <vector>
+
+#include "Preprocessor/Expressions/Base.hpp"
+
+namespace Preprocessor
+{
+namespace Expressions
+{
+
+/** \brief set of expressions. */
+typedef std::vector<BasePtrNN> Set;
+
+} // namespace Expressions
+} // namespace Preprocessor
+
+#endif
