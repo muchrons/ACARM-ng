06e52de0e4b3f3e5af98a3336393d4ba76d96b2a
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-05-04 16:13:27 +0200

    Preprocessor's API has been defined.
    added:
      daemon/preprocessor/Preprocessor/Logic.cpp
      daemon/preprocessor/Preprocessor/Logic.hpp
      daemon/preprocessor/Preprocessor/LogicImpl.hpp

---
+++ daemon/preprocessor/Preprocessor/LogicImpl.hpp
@@ -0,0 +1,34 @@
+/*
+ * LogicImpl.hpp
+ *
+ */
+#ifndef INCLUDE_PREPROCESSOR_LOGICIMPL_HPP_FILE
+#define INCLUDE_PREPROCESSOR_LOGICIMPL_HPP_FILE
+
+#include "ConfigIO/Preprocessor/Config.hpp"
+#include "Persistency/Alert.hpp"
+
+namespace Preprocessor
+{
+
+/** \brief main preprocessor's logic - the implementation.
+ */
+class LogicImpl
+{
+public:
+  /** \brief create object instance from given configuration.
+   */
+  LogicImpl(const ConfigIO::Preprocessor::Config &cfg);
+
+  /** \brief checks if given alert is to be accepted according to defined rules.
+   *  \param alert alert to be checked.
+   *  \return true, if alert is to be accepted, false otherwise.
+   */
+  bool checkAccept(const Persistency::Alert &alert) const;
+
+private:
+}; // class LogicImpl
+
+} // namespace Preprocessor
+
+#endif
