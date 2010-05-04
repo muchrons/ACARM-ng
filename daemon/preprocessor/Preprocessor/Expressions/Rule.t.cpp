2d1f9055ef7bfd15cd2c260ac80ab9dc1d5bb9be
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-05-04 12:15:26 +0200

    extra tests added.
    modified:
      daemon/preprocessor/Preprocessor/Expressions/Rule.t.cpp

--- daemon/preprocessor/Preprocessor/Expressions/Rule.t.cpp
+++ daemon/preprocessor/Preprocessor/Expressions/Rule.t.cpp
@@ -7,13 +7,63 @@
 #include "Preprocessor/Expressions/Rule.hpp"
 #include "Preprocessor/Expressions/TestsCommon.t.hpp"
 
+using Persistency::Alert;
 using Preprocessor::Path;
+using namespace TestHelpers::Persistency;
 using namespace Preprocessor::Expressions;
 
 namespace
 {
 struct TestClass
 {
+  TestClass(void):
+    alert_("alert name",
+           Alert::SourceAnalyzers( makeAnalyzer() ),
+           ::Base::NullValue<Persistency::Timestamp>( Persistency::Timestamp(123) ).get(),
+           Persistency::Timestamp(12345),
+           Persistency::Severity(Persistency::SeverityLevel::INFO),
+           Persistency::Certainty(0.42),
+           "some test alert",
+           makeHosts("1.1.1.1", "dns.org",
+                     "2.2.2.2", "kszy.net"),
+           makeHosts("3.2.2.3", "ogre.org",
+                     "3.2.2.3", "ogre.org") )
+  {
+  }
+
+  Persistency::AnalyzerPtrNN makeAnalyzer(void) const
+  {
+    const Persistency::Analyzer::IP ip=Persistency::Host::IPv4::from_string("6.6.6.0");
+    return Persistency::AnalyzerPtrNN( new Persistency::Analyzer("analyzer",
+                                                                 "v1.2.3",
+                                                                 "Linux!",
+                                                                 &ip) );
+  }
+
+  Alert::ReportedHosts makeHosts(const char *ip1, const char *dns1,
+                                 const char *ip2, const char *dns2) const
+  {
+    Alert::ReportedHosts hosts;
+    hosts.push_back( makeHost(ip1, dns1) );
+    hosts.push_back( makeHost(ip2, dns2) );
+    return hosts;
+  }
+
+  Persistency::HostPtrNN makeHost(const char *ip, const char *dns) const
+  {
+    const Persistency::Host::Netmask_v4  mask(mask4_bytes);
+    Persistency::Host::ReportedServices  services;
+    Persistency::Host::ReportedProcesses procs;
+    return Persistency::HostPtrNN( new Persistency::Host( Persistency::Host::IPv4::from_string(ip),
+                                                          &mask,
+                                                          "penguin",
+                                                          makeNewReferenceURL(),
+                                                          services,
+                                                          procs,
+                                                          dns) );
+  }
+
+  const Alert alert_;
 };
 
 typedef tut::test_group<TestClass> factory;
@@ -31,8 +81,8 @@ template<>
 template<>
 void testObj::test<1>(void)
 {
-  const Rule r( Path("alert.name"), Rule::Mode::EQUALS, "some alert" );
-  ensure("equals failed", r.compute( *makeAlert() )==true );
+  const Rule r( Path("alert.name"), Rule::Mode::EQUALS, "alert name" );
+  ensure("equals failed", r.compute(alert_)==true );
 }
 
 // test Mode::EQUALS - negative case
@@ -41,7 +91,7 @@ template<>
 void testObj::test<2>(void)
 {
   const Rule r( Path("alert.name"), Rule::Mode::EQUALS, "some alert that differs" );
-  ensure("equals didn't failed", r.compute( *makeAlert() )==false );
+  ensure("equals didn't failed", r.compute(alert_)==false );
 }
 
 // test Mode::CONTAINS - positive case
@@ -50,7 +100,7 @@ template<>
 void testObj::test<3>(void)
 {
   const Rule r( Path("alert.name"), Rule::Mode::CONTAINS, "alert" );
-  ensure("contains failed", r.compute( *makeAlert() )==true );
+  ensure("contains failed", r.compute(alert_)==true );
 }
 
 // test Mode::CONTAINS - negative case
@@ -59,7 +109,7 @@ template<>
 void testObj::test<4>(void)
 {
   const Rule r( Path("alert.name"), Rule::Mode::CONTAINS, "ALERT" );
-  ensure("contains didn't failed", r.compute( *makeAlert() )==false );
+  ensure("contains didn't failed", r.compute(alert_)==false );
 }
 
 // test detection time
@@ -67,8 +117,8 @@ template<>
 template<>
 void testObj::test<5>(void)
 {
-  const Rule r( Path("alert.detected"), Rule::Mode::EQUALS, "<NULL>" );
-  ensure("invalid detection time", r.compute( *makeAlert() )==true );
+  const Rule r( Path("alert.detected"), Rule::Mode::EQUALS, "123" );
+  ensure("invalid detection time", r.compute(alert_)==true );
 }
 
 // test creation time
@@ -77,7 +127,7 @@ template<>
 void testObj::test<6>(void)
 {
   const Rule r( Path("alert.created"), Rule::Mode::EQUALS, "12345" );
-  ensure("invalid creation time", r.compute( *makeAlert() )==true );
+  ensure("invalid creation time", r.compute(alert_)==true );
 }
 
 // test certanity
@@ -86,7 +136,7 @@ template<>
 void testObj::test<7>(void)
 {
   const Rule r( Path("alert.certainty"), Rule::Mode::CONTAINS, "0.4" );
-  ensure("invalid certanity", r.compute( *makeAlert() )==true );
+  ensure("invalid certanity", r.compute(alert_)==true );
 }
 
 // test severity
@@ -95,7 +145,7 @@ template<>
 void testObj::test<8>(void)
 {
   const Rule r( Path("alert.severity"), Rule::Mode::EQUALS, "info" );
-  ensure("invalid severity", r.compute( *makeAlert() )==true );
+  ensure("invalid severity", r.compute(alert_)==true );
 }
 
 // test description
@@ -103,8 +153,8 @@ template<>
 template<>
 void testObj::test<9>(void)
 {
-  const Rule r( Path("alert.description"), Rule::Mode::EQUALS, "some test allert" );
-  ensure("invalid description", r.compute( *makeAlert() )==true );
+  const Rule r( Path("alert.description"), Rule::Mode::EQUALS, "some test alert" );
+  ensure("invalid description", r.compute(alert_)==true );
 }
 
 // test analyzer.name
@@ -112,57 +162,71 @@ template<>
 template<>
 void testObj::test<10>(void)
 {
-  const Rule r( Path("alert.analyzers.*.name"), Rule::Mode::EQUALS, "some analyzer" );
-  ensure("invalid analyzer's name", r.compute( *makeAlert() )==true );
+  const Rule r( Path("alert.analyzers.*.name"), Rule::Mode::EQUALS, "analyzer" );
+  ensure("invalid analyzer's name", r.compute(alert_)==true );
 }
 
-// 
+// test analyzer.version
 template<>
 template<>
 void testObj::test<11>(void)
 {
+  const Rule r( Path("alert.analyzers.*.version"), Rule::Mode::EQUALS, "v1.2.3" );
+  ensure("invalid analyzer's version", r.compute(alert_)==true );
 }
 
-// 
+// test analyzer.os
 template<>
 template<>
 void testObj::test<12>(void)
 {
+  const Rule r( Path("alert.analyzers.*.os"), Rule::Mode::EQUALS, "Linux!" );
+  ensure("invalid analyzer's OS", r.compute(alert_)==true );
 }
 
-// 
+// test analyzer.ip
 template<>
 template<>
 void testObj::test<13>(void)
 {
+  const Rule r( Path("alert.analyzers.*.ip"), Rule::Mode::EQUALS, "6.6.6.0" );
+  ensure("invalid analyzer's IP", r.compute(alert_)==true );
 }
 
-// 
+// test host.ip
 template<>
 template<>
 void testObj::test<14>(void)
 {
+  const Rule r( Path("alert.source.$.ip"), Rule::Mode::EQUALS, "2.2.2.2" );
+  ensure("invalid host's IP", r.compute(alert_)==true );
 }
 
-// 
+// test NULL value
 template<>
 template<>
 void testObj::test<15>(void)
 {
+  const Rule r( Path("alert.detected"), Rule::Mode::EQUALS, "<NULL>" );
+  ensure("invalid NULL value", r.compute( *makeAlert() )==true );
 }
 
-// 
+// test host.mask
 template<>
 template<>
 void testObj::test<16>(void)
 {
+  const Rule r( Path("alert.source.*.mask"), Rule::Mode::EQUALS, "255.255.0.0" );
+  ensure("invalid host's mask", r.compute(alert_)==true );
 }
 
-// 
+// host.os
 template<>
 template<>
 void testObj::test<17>(void)
 {
+  const Rule r( Path("alert.source.*.os"), Rule::Mode::EQUALS, "penguin" );
+  ensure("invalid mask's OS", r.compute(alert_)==true );
 }
 
 // 
