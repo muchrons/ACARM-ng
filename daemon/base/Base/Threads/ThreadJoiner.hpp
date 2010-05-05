401a80b3d064018b24a3213c6030bfc52a06e896
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-05-05 13:36:31 +0200

    doxygebn fixes.
    modified:
      daemon/base/Base/Threads/ThreadJoiner.hpp

--- daemon/base/Base/Threads/ThreadJoiner.hpp
+++ daemon/base/Base/Threads/ThreadJoiner.hpp
@@ -14,21 +14,39 @@ namespace Base
 namespace Threads
 {
 
+/** \brief wrapper for boost::thread that ensures thread is interrupted
+ *         and joined when d-tor is called.
+ *
+ *  thanks to this class threads are known to be nterrupted and joined even
+ *  when normal flow (i.e. non-exception one) would not allow this.
+ */
 class ThreadJoiner
 {
 public:
+  /** \brief create and start thread from any object that would be accepted
+   *         by boost::thread itself.
+   *  \param t function object to be run in thread.
+   */
   template<typename T>
   explicit ThreadJoiner(const T &t):
     th_(t)
   {
   }
-
+  /** \brief interrupr and join thread.
+   *
+   *  interrupt thread and join it. call block until thread
+   *  will finish processing.
+   */
   ~ThreadJoiner(void)
   {
     th_.interrupt();
     th_.join();
   }
 
+  /** \brief arrow operator to access underlying thread directly.
+   *  \return pointer to thread held inside.
+   *  \note returned pointer is guaranteed to be non-NULL.
+   */
   boost::thread *operator->(void)
   {
     return &th_;
