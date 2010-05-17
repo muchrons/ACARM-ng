f9bbb9c8b4a2fa49e80221ec296baa526f9b3e18
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-05-17 14:17:26 +0200

    Config has been tested.
    modified:
      daemon/triggergg/Trigger/GG/Config.t.cpp

--- daemon/triggergg/Trigger/GG/Config.t.cpp
+++ daemon/triggergg/Trigger/GG/Config.t.cpp
@@ -15,6 +15,12 @@ namespace
 
 struct TestClass: private TestHelpers::Persistency::TestStubs
 {
+  TestClass(void):
+    cfg_( AccountConfig(42, "abc"), 123, Trigger::Simple::ThresholdConfig("1.2", "4") )
+  {
+  }
+
+  const Config cfg_;
 };
 
 typedef tut::test_group<TestClass> factory;
@@ -27,43 +33,41 @@ factory tf("Trigger/GG/Config");
 namespace tut
 {
 
-// 
+// test getting account
 template<>
 template<>
 void testObj::test<1>(void)
 {
+  ensure_equals("invalid account's uid", cfg_.getAccountConfig().getUserID(), 42);
+  ensure_equals("invalid account's password", cfg_.getAccountConfig().getPassword(), "abc");
 }
 
-// 
+// test getting receiver's ID
 template<>
 template<>
 void testObj::test<2>(void)
 {
-  // TODO
+  ensure_equals("invalid receiver's uid", cfg_.getReceiver(), 123);
 }
 
-// 
+// get severity threshold
 template<>
 template<>
 void testObj::test<3>(void)
 {
-  // TODO
+  ensure("severity threshold is NULL", cfg_.getThresholdConfig().getSeverityThreshold().get()!=NULL );
+  ensure_equals("invalid severity threshold", *cfg_.getThresholdConfig().getSeverityThreshold().get(), 1.2);
+
+  ensure("alerts' count threshold is NULL", cfg_.getThresholdConfig().getAlertCountThreshold().get()!=NULL );
+  ensure_equals("invalid alerts' count threshold", *cfg_.getThresholdConfig().getAlertCountThreshold().get(), 4);
 }
 
-// 
+// test copyability
 template<>
 template<>
 void testObj::test<4>(void)
 {
-  // TODO
-}
-
-// 
-template<>
-template<>
-void testObj::test<5>(void)
-{
-  // TODO
+  const Config tmp=cfg_;
 }
 
 } // namespace tut
