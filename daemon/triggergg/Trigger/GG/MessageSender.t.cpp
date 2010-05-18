2eb9144b036d9730d385e63b5c8eb86dcf6defa3
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-05-18 10:39:53 +0200

    mvoed some helper functions to common, test file.
    modified:
      daemon/triggergg/Trigger/GG/MessageSender.t.cpp
      daemon/triggergg/Trigger/GG/TestAccount.t.hpp

--- daemon/triggergg/Trigger/GG/MessageSender.t.cpp
+++ daemon/triggergg/Trigger/GG/MessageSender.t.cpp
@@ -14,64 +14,19 @@ using namespace TestHelpers::Persistency;
 namespace
 {
 
-// helper class that protects event from possible exceptions
-struct EventWrapper: private boost::noncopyable
-{
-  explicit EventWrapper(gg_event *e):
-    e_(e)
-  {
-  }
-
-  ~EventWrapper(void)
-  {
-    if(e_!=NULL)
-      gg_event_free(e_);
-  }
-
-  gg_event *get(void)
-  {
-    return e_;
-  }
-
-private:
-  gg_event *e_;
-}; // struct EventWrapper
-
-
 struct TestClass: private TestHelpers::Persistency::TestStubs
 {
   TestClass(void):
-    conn1_( getTestConfig1() ),
-    conn2_( getTestConfig2() )
+    conn1_( getTestConfig1() )
   {
   }
 
-  std::string getMessageFromAccount2(void)
+  std::string getMessageFromAccount2(void) const
   {
-    for(;;)
-    {
-      EventWrapper e( gg_watch_fd( conn2_.get() ) );    // read event
-      // skip errors
-      if( e.get()==NULL )
-        continue;
-      // we wait for message
-      if( e.get()->type!=GG_EVENT_MSG )
-        continue;
-      // get reference to message
-      gg_event_msg &m=e.get()->event.msg;
-      // only messages from given UID are interesting for us
-      if( m.sender!=getTestConfig1().getUserID() )
-        continue;
-
-      // ok - this is our message!
-      tut::ensure("NULL message received", m.message!=NULL);
-      const char *msg=reinterpret_cast<const char*>(m.message);
-      return msg;
-    } // for(events)
+    return getMessageFromAccount( getTestConfig2() );
   }
 
   Connection conn1_;
-  Connection conn2_;
 };
 
 typedef tut::test_group<TestClass> factory;
@@ -84,24 +39,24 @@ factory tf("Trigger/GG/MessageSender");
 namespace tut
 {
 
-// test sending message from one account to another.
+// test c-tor/d-tor
 template<>
 template<>
 void testObj::test<1>(void)
+{
+  MessageSender ms(conn1_);
+}
+
+// test sending message from one account to another.
+template<>
+template<>
+void testObj::test<2>(void)
 {
   MessageSender     ms(conn1_);
   const std::string msg("alice has a cat");
   ms.send( getTestConfig2().getUserID(), msg );
   const std::string recv=getMessageFromAccount2();
   ensure_equals("invalid message received", recv, msg);
-}
-
-// test c-tor/d-tor
-template<>
-template<>
-void testObj::test<2>(void)
-{
-  MessageSender ms(conn1_);
 }
 
 // test sending UTF8 message
--- daemon/triggergg/Trigger/GG/TestAccount.t.hpp
+++ daemon/triggergg/Trigger/GG/TestAccount.t.hpp
@@ -5,7 +5,11 @@
 #ifndef INCLUDE_TRIGGER_GG_TESTACCOUNT_T_HPP_FILE
 #define INCLUDE_TRIGGER_GG_TESTACCOUNT_T_HPP_FILE
 
+#include <string>
+#include <libgadu.h>
+
 #include "Trigger/GG/AccountConfig.hpp"
+#include "Trigger/GG/Connection.hpp"
 
 namespace
 {
@@ -25,6 +29,56 @@ Trigger::GG::AccountConfig getTestConfig(void)
 {
   return getTestConfig1();
 }
+
+
+
+// helper class that protects event from possible exceptions
+struct EventWrapper: private boost::noncopyable
+{
+  explicit EventWrapper(gg_event *e):
+    e_(e)
+  {
+  }
+
+  ~EventWrapper(void)
+  {
+    if(e_!=NULL)
+      gg_event_free(e_);
+  }
+
+  gg_event *get(void)
+  {
+    return e_;
+  }
+
+private:
+  gg_event *e_;
+}; // struct EventWrapper
+
+
+std::string getMessageFromAccount(const Trigger::GG::AccountConfig &account)
+{
+  Trigger::GG::Connection conn(account);
+  for(;;)
+  {
+    EventWrapper e( gg_watch_fd( conn.get() ) );    // read event
+    // skip errors
+    if( e.get()==NULL )
+      continue;
+    // we wait for message
+    if( e.get()->type!=GG_EVENT_MSG )
+      continue;
+    // get reference to message
+    gg_event_msg &m=e.get()->event.msg;
+    // only messages from given UID are interesting for us
+    if( m.sender!=account.getUserID() )
+      continue;
+
+    // ok - this is our message!
+    const char *msg=reinterpret_cast<const char*>(m.message);
+    return msg;
+  } // for(events)
+} // getMessageFromAccount()
 
 } // unnamed namespace
 
