af5b19cce280d14e99344ba0699c84f5a9fbe345
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-05-25 11:35:39 +0200

    StrError class has been implemented to wrap gettting errro string from system.
    added:
      daemon/base/Base/StrError.cpp
      daemon/base/Base/StrError.hpp
      daemon/base/Base/StrError.t.cpp

---
+++ daemon/base/Base/StrError.hpp
@@ -0,0 +1,36 @@
+/*
+ * StrError.hpp
+ *
+ */
+#ifndef INCLUDE_BASE_STRERROR_HPP_FILE
+#define INCLUDE_BASE_STRERROR_HPP_FILE
+
+#include <string>
+
+namespace Base
+{
+
+/** \brief create error string from curren errno's value.
+ */
+class StrError
+{
+public:
+  /** \brief read error message from errno.
+   */
+  StrError(void);
+
+  /** \brief get error message.
+   *  \return error reported bu errno as a string.
+   */
+  const std::string &get(void) const
+  {
+    return msg_;
+  }
+
+private:
+  std::string msg_;
+}; // class StrError
+
+} // namespace Base
+
+#endif
