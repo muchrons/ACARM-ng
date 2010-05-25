33fe6beed8b9681b7281a1b2b440efff6ff0537f
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-05-25 17:52:38 +0200

    MailSender now sends messages as expected.
    modified:
      daemon/triggermail/Trigger/Mail/MailSender.cpp
      doc/CONFIGURATION.txt

--- daemon/triggermail/Trigger/Mail/MailSender.cpp
+++ daemon/triggermail/Trigger/Mail/MailSender.cpp
@@ -16,13 +16,30 @@ MailSender::MailSender(const Config &cfg):
 {
 }
 
+
+namespace
+{
+inline std::string addHeaders(const std::string &content,
+                              const std::string &subject,
+                              const std::string &from,
+                              const std::string &to)
+{
+  const char *eol="\r\n";
+  std::stringstream ss;
+  ss<<"Subject: [acarmng] "<<subject<<eol;
+  ss<<"From: "<<from<<eol;
+  ss<<"To: "  <<to  <<eol;
+  ss<<content;
+  return ss.str();
+} // addHeaders()
+} // unnamed namespace
+
+
 void MailSender::send(const std::string &subject, const std::string &content)
 {
   const Config::Server        &srv =cfg_.getServerConfig();
   const Config::Authorization *auth=cfg_.getAuthorizationConfig();
   MailSmtp                     ms;
-
-  // TODO: add switching between ssl/raw connection
 
   // connect to server
   switch( srv.sec_.toInt() )
@@ -49,15 +66,20 @@ void MailSender::send(const std::string &subject, const std::string &content)
   if(srv.sec_==Config::Server::Security::STARTTLS)              // STARTTLS?
     errorHandler( mailsmtp_socket_starttls( ms.get() ), "mailsmtp_socket_starttls");
   if(auth!=NULL)                                                // require authorization?
-    errorHandler( mailsmtp_auth( ms.get(), auth->user_.c_str(), auth->pass_.c_str() ), "mailsmtp_auth" );
-  errorHandler( mailesmtp_mail( ms.get(), srv.from_.c_str(), 1, "ACARM-ng" ), "mailesmtp_mail" );
+    errorHandler( mailsmtp_auth( ms.get(), auth->user_.c_str(), auth->pass_.c_str() ),
+                  "mailsmtp_auth" );                            // AUTH
+  errorHandler( mailesmtp_mail( ms.get(), srv.from_.c_str(), 0, NULL ),
+                "mailesmtp_mail" );                             // FROM
   errorHandler( mailesmtp_rcpt( ms.get(), cfg_.getRecipientAddress().c_str(),
                                 MAILSMTP_DSN_NOTIFY_FAILURE|MAILSMTP_DSN_NOTIFY_DELAY, NULL ),
-                "mailesmtp_rcpt" );
-  errorHandler( mailsmtp_data( ms.get() ), "mailsmtp_data" );
-  errorHandler( mailsmtp_data_message( ms.get(), content.c_str(), content.length() ),
-                "mailsmtp_data_message" );
-  // TODO: setup subject too
+                "mailesmtp_rcpt" );                             // TO
+  errorHandler( mailsmtp_data( ms.get() ), "mailsmtp_data" );   // DATA
+  // data-part headers and stuff...
+  const std::string &whole=addHeaders(content, subject, srv.from_, cfg_.getRecipientAddress() );
+  errorHandler( mailsmtp_data_message( ms.get(), whole.c_str(), whole.length() ),
+                "mailsmtp_data_message" );                      // message body goes here
+
+  errorHandler( mailsmtp_quit( ms.get() ), "mailsmtp_quit" );   // QUIT
 }
 
 void MailSender::connectionErrorHandler(int ret, const char *call) const
