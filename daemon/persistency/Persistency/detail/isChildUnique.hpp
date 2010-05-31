9abd7281f340e67b7214dfd71de9f08888d64fe2
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-05-31 13:12:26 +0200

    doxygen addded.
    modified:
      daemon/persistency/Persistency/detail/isChildUnique.hpp

--- daemon/persistency/Persistency/detail/isChildUnique.hpp
+++ daemon/persistency/Persistency/detail/isChildUnique.hpp
@@ -5,8 +5,6 @@
 #ifndef INCLUDE_PERSISTENCY_DETAIL_ISCHILDUNIQUE_HPP_FILE
 #define INCLUDE_PERSISTENCY_DETAIL_ISCHILDUNIQUE_HPP_FILE
 
-/* public header */
-
 #include "Persistency/GraphNodePtr.hpp"
 
 namespace Persistency
@@ -14,6 +12,11 @@ namespace Persistency
 namespace detail
 {
 
+/** \brief checks if given child is not present as a direct child of given parent.
+ *  \param parent parent to look child for in.
+ *  \param child  child to be checked, if is in parent or not.
+ *  \return true if child is not present in parent, false otherwise.
+ */
 bool isChildUnique(const GraphNode &parent, GraphNodePtrNN child);
 
 } // namespace detail
