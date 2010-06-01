dc352ea7e430b03814ef1af0419aa38dc9cfca62
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-06-01 12:00:21 +0200

    added baisc logging for CUrlInit.
    modified:
      daemon/filteripblacklist/Filter/IPBlackList/CUrlInit.cpp
      daemon/filteripblacklist/Filter/IPBlackList/CUrlInit.hpp

--- daemon/filteripblacklist/Filter/IPBlackList/CUrlInit.cpp
+++ daemon/filteripblacklist/Filter/IPBlackList/CUrlInit.cpp
@@ -5,6 +5,7 @@
 #include <cassert>
 
 #include "curlpp/cURLpp.hpp"
+#include "Logger/Logger.hpp"
 #include "System/Threads/SafeInitLocking.hpp"
 #include "System/AtExit.hpp"
 #include "Filter/IPBlackList/CUrlInit.hpp"
@@ -28,7 +29,9 @@ struct CUrlUninit: public System::AtExitResourceDeallocator
 {
   virtual void deallocate(void)
   {
+    Logger::Node log("filter.ipblacklist.curluninit");
     System::Threads::SafeInitLock lock(mutex);
+    LOGMSG_INFO(log, "termination called from AtExit");
     assert(g_terminated==false && "termination called before deallocate()");
     g_exitIsDone=true;
     // terminate ONLY if there are no more instances of CUrlInit present
@@ -40,6 +43,8 @@ struct CUrlUninit: public System::AtExitResourceDeallocator
 
   static void terminate(void)
   {
+    Logger::Node log("filter.ipblacklist.curluninit");
+    LOGMSG_INFO(log, "terminating curl library");
     assert(g_terminated==false && "oops - curl has been already terminated");
     g_terminated=true;
     curlpp::terminate();
@@ -48,20 +53,30 @@ struct CUrlUninit: public System::AtExitResourceDeallocator
 } // unnamed namespace
 
 
-CUrlInit::CUrlInit(void)
+CUrlInit::CUrlInit(void):
+  log_("filter.ipblacklist.curlinit")
 {
   System::Threads::SafeInitLock lock(mutex);
   if(g_count==0)
   {
+    LOGMSG_INFO(log_, "initializing curl library");
     // initialize cURL++
     curlpp::initialize();
     // create deinitializer to AtExit, if it has not been reviously created.
     if(g_exitIsDone==false)
     {
+      LOGMSG_INFO(log_, "registering automatic deallocator uppon exit");
       System::AtExit::TDeallocPtr ptr(new CUrlUninit);
       System::AtExit::registerDeallocator(ptr);
     }
-  }
+    else
+    {
+      LOGMSG_INFO(log_, "re-registration of curl - skipping AtExit registration");
+      g_terminated=false;
+    }
+  } // if(intialize)
+
+  assert(g_terminated==false && "something's wrong here...");
   ++g_count;
 }
 
@@ -72,7 +87,10 @@ CUrlInit::~CUrlInit(void)
   // if we're the last instance and AtExit() has already finished,
   // we have to do clean-up ourselfs
   if(g_count==0 && g_exitIsDone==true)
+  {
+    LOGMSG_INFO(log_, "we're the last instance - calling termination");
     CUrlUninit::terminate();
+  }
   else
     assert(g_terminated==false && "oops - curl terminated too soon");
 }
