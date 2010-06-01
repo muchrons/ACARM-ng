65c63d26e2dd80d62cb1a358dd14070eb2b5eb65
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-06-01 08:44:49 +0200

    added extra check when multiple threads are present.
    modified:
      daemon/logger/Logger/ThreadIDMap.t.cpp

--- daemon/logger/Logger/ThreadIDMap.t.cpp
+++ daemon/logger/Logger/ThreadIDMap.t.cpp
@@ -3,6 +3,7 @@
  *
  */
 #include <tut.h>
+#include <unistd.h>
 
 #include "Base/Threads/ThreadJoiner.hpp"
 #include "Logger/ThreadIDMap.hpp"
@@ -59,8 +60,10 @@ namespace
 // helper class for checking IDs in test.
 struct IDGetter
 {
-  explicit IDGetter(ThreadIDMap *tim):
-    tim_(tim)
+  IDGetter(ThreadIDMap *tim, unsigned int id, volatile int *state=NULL):
+    state_(state),
+    tim_(tim),
+    id_(id)
   {
     ensure("pointer is NULL", tim_!=NULL);
   }
@@ -68,13 +71,29 @@ struct IDGetter
   void operator()(void)
   {
     assert(tim_!=NULL);
-    ensure_equals("invalid ID returned for thread / 1", tim_->getThreadID(), 1u);   // add
-    ensure_equals("invalid ID returned for thread / 2", tim_->getThreadID(), 1u);   // get
+    ensure_equals("invalid ID returned for thread / 1", tim_->getThreadID(), id_);  // add
+    // extra wait requested?
+    if(state_!=NULL)
+    {
+      *state_=1;
+      while(*state_!=2)
+        usleep(10*1000);
+    }
+    ensure_equals("invalid ID returned for thread / 2", tim_->getThreadID(), id_);  // get
   }
 
+  volatile int *state_;
 private:
-  ThreadIDMap *tim_;
+  ThreadIDMap  *tim_;
+  unsigned int  id_;
 }; // struct IDGetter
+
+void waitState(volatile int *state)
+{
+  ensure("NULL state", state!=NULL);
+  while(*state!=1)
+    usleep(11*1000);
+} // waitState()
 } // unnamed namespace
 
 // test "get" from different thread
@@ -86,8 +105,36 @@ void testObj::test<4>(void)
   ensure_equals("invalid ID returned / 1", tim.getThreadID(), 0u);  // add
   ensure_equals("invalid ID returned / 2", tim.getThreadID(), 0u);  // get
   // now do the same within a thread
-  Base::Threads::ThreadJoiner tj( (IDGetter(&tim)) );
+  Base::Threads::ThreadJoiner tj( (IDGetter(&tim, 1u)) );
   tj->join();
+}
+
+// test "get" from many threads, after some time
+template<>
+template<>
+void testObj::test<5>(void)
+{
+  volatile int state[4]={0};
+  ThreadIDMap  tim;
+  // run multiple threads
+  Base::Threads::ThreadJoiner tj1( (IDGetter(&tim, 0u, &state[0])) );
+  waitState(&state[0]);
+  Base::Threads::ThreadJoiner tj2( (IDGetter(&tim, 1u, &state[1])) );
+  waitState(&state[1]);
+  Base::Threads::ThreadJoiner tj3( (IDGetter(&tim, 2u, &state[2])) );
+  waitState(&state[2]);
+  Base::Threads::ThreadJoiner tj4( (IDGetter(&tim, 3u, &state[3])) );
+  waitState(&state[3]);
+  // proceed with next checks
+  state[0]=2;
+  state[1]=2;
+  state[2]=2;
+  state[3]=2;
+  // wait for them
+  tj1->join();
+  tj2->join();
+  tj3->join();
+  tj4->join();
 }
 
 } // namespace tut
