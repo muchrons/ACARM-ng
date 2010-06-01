2ebc618ba0d3dfa8f927609b9355b6761d41b643
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-06-01 09:40:33 +0200

    searching for ThreadID optimiezed to use lower_bound.
    modified:
      daemon/logger/Logger/ThreadIDMap.cpp

--- daemon/logger/Logger/ThreadIDMap.cpp
+++ daemon/logger/Logger/ThreadIDMap.cpp
@@ -2,6 +2,9 @@
  * ThreadIDMap.cpp
  *
  */
+#include <algorithm>
+#include <cassert>
+
 #include "Base/Threads/Lock.hpp"
 #include "Logger/ThreadIDMap.hpp"
 
@@ -13,18 +16,35 @@ ThreadIDMap::ThreadIDMap(void):
 {
 }
 
+namespace
+{
+// strict-weak ordering
+template<typename T>
+struct SWO
+{
+  inline bool operator()(const T &left, const T &right) const
+  {
+    return left.first < right.first;
+  }
+}; // SWO
+} // unnamed namespace
+
 unsigned int ThreadIDMap::getThreadID(void)
 {
   const boost::thread::id id=boost::this_thread::get_id();
 
   Base::Threads::Lock lock(mutex_);
-  // has entry in "map"?
-  for(IDMap::const_iterator it=m_.begin(); it!=m_.end(); ++it)
-    if(it->first==id)
-      return it->second;
-  // if not - make new one!
+
+  // has entry in "map"? (NOTE: lower_boundis used here, since it has O(logn) on sorted data.
+  IDMap::iterator it=std::lower_bound( m_.begin(), m_.end(), Mapping(id, 0u) );
+  if( it!=m_.end() && it->first==id)
+  {
+    assert( std::find( m_.begin(), m_.end(), Mapping(id, it->second) )==it );
+    return it->second;
+  }
+  // else - add new entry, using lower_bound's result.
   const Mapping tmp(id, nextFreeID_++);
-  m_.push_back(tmp);
+  m_.insert(it, tmp);
   return tmp.second;
 }
 
