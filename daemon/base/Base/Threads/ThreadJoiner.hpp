939da49a299a56abfb1a917a1a3ba9c765cfd4ba
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-06-15 09:41:36 +0200

    d-tor update.
    modified:
      daemon/base/Base/Threads/ThreadJoiner.hpp

--- daemon/base/Base/Threads/ThreadJoiner.hpp
+++ daemon/base/Base/Threads/ThreadJoiner.hpp
@@ -47,11 +47,9 @@ public:
   ~ThreadJoiner(void)
   {
     assert( boost::this_thread::get_id()!=th_.get_id() );   // sanity check
+    th_.interrupt();
     if( th_.joinable() )
-    {
-      th_.interrupt();
       th_.join();
-    }
   }
 
   /** \brief arrow operator to access underlying thread directly.
