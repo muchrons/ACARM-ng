d5f402af190685e0b830413744976e1e35eae1c2
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-06-16 16:49:28 +0200

    mtests updated (typos and comments).
    modified:
      daemon/triggerfile/Trigger/File/Strategy.mt.cpp
      daemon/triggergg/Trigger/GG/Strategy.mt.cpp

--- daemon/triggerfile/Trigger/File/Strategy.mt.cpp
+++ daemon/triggerfile/Trigger/File/Strategy.mt.cpp
@@ -1,28 +1,18 @@
 /*
- * MessageSender.t.cpp
+ * Strategy.t.cpp
  *
  */
 #include <iostream>
 
 #include "Trigger/File/Strategy.hpp"
-#include "Trigger/File/TestAccount.t.hpp"
 #include "TestHelpers/Persistency/TestHelpers.hpp"
 
 using namespace std;
 using namespace Trigger::File;
 
-int main(int argc, char **argv)
+int main(void)
 {
-  if(argc!=1+1)
-  {
-    cerr<<argv[0]<<" <receiver>"<<endl;
-    return 1;
-  }
-
-  const UserID           receiver=atoi(argv[1]);
-  const Config           cfg( getTestConfig1(),
-                              receiver,
-                              Trigger::Simple::ThresholdConfig("0.0", "0") );
+  const Config           cfg( ".", Trigger::Simple::ThresholdConfig("0.0", "0") );
   Strategy               s(cfg);
   Strategy::ChangedNodes nc;
   s.process( TestHelpers::Persistency::makeNewNode(), nc ); // trigger and send
