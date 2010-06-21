c2c4a3739748e725120a0520dd5b9396baf1e9dc
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-06-21 11:31:15 +0200

    implemented signal ignoring mechanism.
    added:
      daemon/core/Core/IgnoreSignal.cpp
      daemon/core/Core/IgnoreSignal.hpp

---
+++ daemon/core/Core/IgnoreSignal.hpp
@@ -0,0 +1,35 @@
+/*
+ * IgnoreSignal.hpp
+ *
+ */
+#ifndef INCLUDE_CORE_IGNORESIGNAL_HPP_FILE
+#define INCLUDE_CORE_IGNORESIGNAL_HPP_FILE
+
+/* public header */
+
+#include "System/SignalRegistrator.hpp"
+#include "Logger/Node.hpp"
+
+namespace Core
+{
+/** \brief ignores given signal.
+ */
+class IgnoreSignal: public System::SignalRegistrator
+{
+public:
+  /** \brief registers given signal as ignored.
+   *  \param signum signal number to be sent.
+   *  \param wt     main system threads. if NULL, signal is ignored.
+   */
+  explicit IgnoreSignal(int signum);
+  /** \brief unregisters signal handler.
+   */
+  ~IgnoreSignal(void);
+
+private:
+  int          signum_;
+  Logger::Node log_;
+}; // class IgnoreSignal
+} // namespace Core
+
+#endif
