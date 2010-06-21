86c118d829ed23042fe0110637e32f4693bd26bf
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-06-21 10:18:13 +0200

    mail trigger is now more verbose.
    modified:
      daemon/triggermail/Trigger/Mail/Strategy.cpp

--- daemon/triggermail/Trigger/Mail/Strategy.cpp
+++ daemon/triggermail/Trigger/Mail/Strategy.cpp
@@ -4,7 +4,7 @@
  */
 #include <cassert>
 
-#include "Trigger/Compose/Summary.hpp"
+#include "Trigger/Compose/Full.hpp"
 #include "Trigger/Mail/Strategy.hpp"
 #include "Trigger/Mail/MailSender.hpp"
 
@@ -30,7 +30,7 @@ void Strategy::trigger(const Node &n)
   // prepare message's content
   stringstream ss;
   // TODO: use more verbose Compose variant here - mails can be longger.
-  Compose::Summary::append(ss, n);
+  Compose::Full::append(ss, n);
 
   // send message
   MailSender ms(cfg_);
