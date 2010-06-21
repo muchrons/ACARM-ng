d28c9042b281c2b3fc49991bbf10a8e071353b74
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-06-21 14:46:39 +0200

    test account's data for GG ported to be placed in testhelpers.
    added:
      daemon/testhelpers/TestHelpers/Data/
      daemon/testhelpers/TestHelpers/Data/gg1.hpp
      daemon/testhelpers/TestHelpers/Data/gg2.hpp
      daemon/triggergg/features/deps/mtest@
      daemon/triggergg/features/deps/test
    modified:
      daemon/testhelpers/features/modes/default
      daemon/triggergg/Trigger/GG/TestAccount.t.hpp

---
+++ daemon/testhelpers/TestHelpers/Data/gg2.hpp
@@ -0,0 +1,13 @@
+/*
+ * gg2.hpp
+ *
+ */
+#ifndef INCLUDE_TESTHELPERS_DATA_GG2_HPP_FILE
+#define INCLUDE_TESTHELPERS_DATA_GG2_HPP_FILE
+
+/* public header */
+
+#define GG2_TEST_ACCOUNT_UIN    23461426
+#define GG2_TEST_ACCOUNT_PASS   "testowekonto"
+
+#endif
