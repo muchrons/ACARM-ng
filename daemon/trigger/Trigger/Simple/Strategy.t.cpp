41191d844fd68ff053381733608cf86cd98f5402
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-06-22 11:49:41 +0200

    added tests for messages buffering.
    modified:
      daemon/trigger/Trigger/Simple/Strategy.t.cpp

--- daemon/trigger/Trigger/Simple/Strategy.t.cpp
+++ daemon/trigger/Trigger/Simple/Strategy.t.cpp
@@ -39,9 +39,9 @@ struct TestClass: private TestHelpers::Persistency::TestStubs
     tut::ensure_equals("pre-condition failed", tt.callsTrigger_, 0);
     TestTrigger::ChangedNodes cn;
     tt.process(n, cn);
+    tut::ensure_equals("ChangedNodes changed in trigger", cn.size(), 0u);
     tut::ensure_equals("trigger called invalid number of times", tt.callsTrigger_, cnt);
   }
-
 };
 
 typedef tut::test_group<TestClass> factory;
@@ -115,6 +115,110 @@ void testObj::test<7>(void)
 {
   TestTrigger tt(NULL, NULL);
   check(tt, makeNewLeaf(), 0);
+}
+
+namespace
+{
+struct TestBufferTrigger: public Strategy
+{
+  explicit TestBufferTrigger(int skip):
+    Strategy("testbuffertrigger", ThresholdConfig("0", "0") ),
+    skip_(skip),
+    callsTrigger_(0),
+    counter_(0)
+  {
+  }
+
+  virtual void triggerImpl(const Persistency::GraphNodePtrNN &/*n*/)
+  {
+    ++counter_;
+    if(counter_<=skip_)
+      throw std::runtime_error("error simulation - skip this run");
+    // simulate success
+    ++callsTrigger_;
+  }
+
+  int skip_;
+  int callsTrigger_;
+  int counter_;
+};
+} // unnamed namespace
+
+// test if messages buffering works
+template<>
+template<>
+void testObj::test<8>(void)
+{
+  TestBufferTrigger tt(1);
+
+  // first run
+  {
+    TestBufferTrigger::ChangedNodes cn;
+    try
+    {
+      tt.process( makeNewLeaf(), cn );
+      // fail not needed here - it doesn't matter it this call throws or not...
+    }
+    catch(const std::runtime_error &)
+    {
+      // this is expected
+    }
+  }
+  // test first call
+  tut::ensure_equals("trigger not called at all", tt.counter_, 1);
+  tut::ensure_equals("trigger called after exception", tt.callsTrigger_, 0);
+
+  // second run
+  {
+    TestBufferTrigger::ChangedNodes cn;
+    tt.process( makeNewLeaf(), cn );
+  }
+  // test second call
+  tut::ensure_equals("trigger not called (2nd run)", tt.counter_, 1+2);
+  tut::ensure_equals("not all messages have been sent", tt.callsTrigger_, 0+2);
+}
+
+// test if messages are removed from buffer when sent
+template<>
+template<>
+void testObj::test<9>(void)
+{
+  TestBufferTrigger tt(1);
+
+  // first run
+  {
+    TestBufferTrigger::ChangedNodes cn;
+    try
+    {
+      tt.process( makeNewLeaf(), cn );
+      // fail not needed here - it doesn't matter it this call throws or not...
+    }
+    catch(const std::runtime_error &)
+    {
+      // this is expected
+    }
+  }
+  // test first call
+  tut::ensure_equals("trigger not called at all", tt.counter_, 1);
+  tut::ensure_equals("trigger called after exception", tt.callsTrigger_, 0);
+
+  // second run
+  {
+    TestBufferTrigger::ChangedNodes cn;
+    tt.process( makeNewLeaf(), cn );
+  }
+  // test second call
+  tut::ensure_equals("trigger not called (2nd run)", tt.counter_, 1+2);
+  tut::ensure_equals("not all messages have been sent", tt.callsTrigger_, 0+2);
+
+  // third call
+  {
+    TestBufferTrigger::ChangedNodes cn;
+    tt.process( makeNewLeaf(), cn );
+  }
+  // test third call
+  tut::ensure_equals("trigger not called (3rd run)", tt.counter_, 1+2+1);
+  tut::ensure_equals("messages are not removed?", tt.callsTrigger_, 0+2+1);
 }
 
 } // namespace tut
