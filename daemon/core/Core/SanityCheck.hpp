533ac5cdace5b5f09ba58f3032386fc91d92994e
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-06-28 11:47:10 +0200

    added logging of system data when system is starting.
    modified:
      daemon/core/Core/Main.hpp
      daemon/core/Core/SanityCheck.cpp
      daemon/core/Core/SanityCheck.hpp

--- daemon/core/Core/SanityCheck.hpp
+++ daemon/core/Core/SanityCheck.hpp
@@ -9,6 +9,7 @@
 
 #include <boost/noncopyable.hpp>
 
+#include "Logger/Node.hpp"
 #include "Core/Exception.hpp"
 #include "Core/ExceptionRunningAsRoot.hpp"
 
@@ -18,6 +19,8 @@ namespace Core
 /** \brief tests environment's sanity.
  *
  *  checks application's pre-conditions and throws in case of error.
+ *
+ *  also performs basic loging of system type.
  */
 class SanityCheck: private boost::noncopyable
 {
@@ -28,6 +31,9 @@ public:
 
 private:
   void ensureNotRoot(void);
+  void logSystemInfo(void);
+
+  Logger::Node log_;
 }; // class SanityCheck
 
 } // namespace Core
