b94b244ab265cd8b555f672d51c1ba0d1a8262c0
Mariusz Uchroński <mariusz.uchronski@pwr.wroc.pl>
2010-06-30 09:05:06 +0200

    Temporary commit.
    removed:
      daemon/filtersamename/Filter/SameName/tmp.cpp
    added:
      daemon/filtersamename/Filter/SameName/FactoryBuilder.cpp
      daemon/filtersamename/Filter/SameName/Strategy.cpp
      daemon/filtersamename/Filter/SameName/Strategy.hpp
      daemon/filtersamename/Filter/SameName/Strategy.t.cpp

---
+++ daemon/filtersamename/Filter/SameName/Strategy.cpp
@@ -0,0 +1,57 @@
+/*
+ * Strategy.cpp
+ *
+ */
+#include <vector>
+#include <algorithm>
+#include <cassert>
+
+#include "Filter/SameName/Strategy.hpp"
+#include "Algo/forEachUniqueLeaf.hpp"
+#include "Algo/GatherHosts.hpp"
+
+using namespace std;
+using namespace Persistency;
+
+namespace Filter
+{
+namespace SameName
+{
+
+Strategy::Strategy(unsigned int timeout):
+  Filter::Simple::Strategy<Data>("samename", timeout)
+{
+}
+
+Strategy::NodeEntry Strategy::makeThisEntry(const Node n) const
+{
+  return NodeEntry( n, Data() );
+}
+
+bool Strategy::isEntryInteresting(const NodeEntry /*thisEntry*/) const
+{
+  return true;
+}
+
+Persistency::MetaAlert::Name Strategy::getMetaAlertName(
+                                              const NodeEntry /*thisEntry*/,
+                                              const NodeEntry /*otherEntry*/) const
+{
+  return "[samename] atacks from ...";
+}
+
+bool Strategy::canCorrelate(const NodeEntry thisEntry,
+                            const NodeEntry otherEntry) const
+{
+  // sanityt check
+  assert( isEntryInteresting(thisEntry)  );
+  assert( isEntryInteresting(otherEntry) );
+  // ok - both names are the same
+  if(thisEntry.node_->getAlert().getName() == otherEntry.node_->getAlert().getName() )
+    return true;
+
+  return false;
+}
+
+} // namespace SameName
+} // namespace Filter
---
+++ daemon/filtersamename/Filter/SameName/Strategy.t.cpp
@@ -0,0 +1,69 @@
+/*
+ * Strategy.t.cpp
+ *
+ */
+#include <tut.h>
+#include <string>
+#include <cassert>
+
+#include "Filter/SameName/Strategy.hpp"
+#include "TestHelpers/Persistency/TestStubs.hpp"
+#include "TestHelpers/Persistency/TestHelpers.hpp"
+
+using namespace std;
+using namespace Persistency;
+using namespace Filter::SameName;
+using namespace TestHelpers::Persistency;
+
+namespace
+{
+
+struct TestClass: public TestStubs
+{
+  TestClass(void):
+    sampleLeaf_( makeNewLeaf( makeNewAlert("some alert") ) ),
+    s_(997)
+  {
+  }
+  GraphNodePtrNN         sampleLeaf_;
+  Strategy::ChangedNodes changed_;
+  Strategy               s_;
+};
+
+typedef tut::test_group<TestClass> factory;
+typedef factory::object testObj;
+
+factory tf("Filter/SameName/Strategy");
+} // unnamed namespace
+
+namespace tut
+{
+
+
+// TODO: test canCorelate method
+template<>
+template<>
+void testObj::test<1>(void)
+{
+  GraphNodePtrNN tmp( makeNewLeaf( makeNewAlert("some name") ) );
+  s_.process(tmp, changed_);
+  ensure_equals("some nodes have been changed", changed_.size(), 0u);
+
+  s_.process(sampleLeaf_, changed_);
+  ensure_equals("no nodes changed", changed_.size(), 1u);
+}
+
+// TODO: test if can correltate will return false for non-overlaping entries
+template<>
+template<>
+void testObj::test<2>(void)
+{
+}
+
+// TODO: test getting name of meta alert
+template<>
+template<>
+void testObj::test<5>(void)
+{
+}
+} // namespace tut
