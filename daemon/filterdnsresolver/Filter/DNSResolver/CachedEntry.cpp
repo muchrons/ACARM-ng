da328d3c8397cc79ebbb23fbcc2caedcece18624
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-07-06 17:06:31 +0200

    fixed
    added:
      daemon/filterdnsresolver/Filter/DNSResolver/Exception.hpp
      daemon/filterdnsresolver/Filter/DNSResolver/ExceptionUnableToResolve.hpp
    modified:
      daemon/filterdnsresolver/Filter/DNSResolver/CachedEntry.cpp
      daemon/filterdnsresolver/Filter/DNSResolver/CachedEntry.hpp
      daemon/filterdnsresolver/Filter/DNSResolver/EntryProcessor.cpp
      daemon/filterdnsresolver/Filter/DNSResolver/EntryProcessor.hpp

--- daemon/filterdnsresolver/Filter/DNSResolver/CachedEntry.cpp
+++ daemon/filterdnsresolver/Filter/DNSResolver/CachedEntry.cpp
@@ -16,22 +16,30 @@ namespace DNSResolver
 
 void CachedEntry::resolve(const IP &ip)
 {
-  // TODO: N5boost6system12system_errorE is thrown here on error - rethrow it as
-  //       internal DNS-resolv-ailure error, to be more specific about the problem
-  //       by type (N5boost6system12system_errorE is too generic to catch later on).
-  io_service                  ios;
-  ip::tcp::endpoint           ep(ip, 0);
-  ip::tcp::resolver           resolver(ios);
-  ip::tcp::resolver::iterator it=resolver.resolve(ep);
-  ip::tcp::resolver::iterator end;
-  if( it==end || it->host_name()==ip.to_string() )
+  try
   {
-    hasName_=false;
+    io_service                  ios;
+    ip::tcp::endpoint           ep(ip, 0);
+    ip::tcp::resolver           resolver(ios);
+    ip::tcp::resolver::iterator it=resolver.resolve(ep);
+    ip::tcp::resolver::iterator end;
+    if( it==end || it->host_name()==ip.to_string() )
+    {
+      hasName_=false;
+    }
+    else
+    {
+      name_   =it->host_name();
+      hasName_=true;
+    }
   }
-  else
+  catch(const boost::system::system_error &ex)
   {
-    hasName_=true;
-    name_   =it->host_name();
+    throw ExceptionUnableToResolve(SYSTEM_SAVE_LOCATION, ip.to_string(), ex.what() );
+  }
+  catch(const std::exception &ex)
+  {
+    throw ExceptionUnableToResolve(SYSTEM_SAVE_LOCATION, ip.to_string(), ex.what() );
   }
 }
 
---
+++ daemon/filterdnsresolver/Filter/DNSResolver/Exception.hpp
@@ -0,0 +1,35 @@
+/*
+ * Exception.hpp
+ *
+ */
+#ifndef INCLUDE_FILTER_DNSRESOLVER_EXCEPTION_HPP_FILE
+#define INCLUDE_FILTER_DNSRESOLVER_EXCEPTION_HPP_FILE
+
+/* public header */
+
+#include "Filter/Exception.hpp"
+
+namespace Filter
+{
+namespace DNSResolver
+{
+
+/** \brief base exception for DNS-resolver filter.
+ */
+class Exception: public Filter::Exception
+{
+public:
+  /** \brief create execption with given message.
+   *  \param where place where exception has been thrown.
+   *  \param msg   message to represent.
+   */
+  Exception(const Location &where, const std::string &msg):
+    Filter::Exception(where, "dnsresolver", msg)
+  {
+  }
+}; // class Exception
+
+} // namespace DNSResolver
+} // namespace Filter
+
+#endif
