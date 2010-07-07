c702e287e195f044df3b17e0d00465a252b1dc52
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-07-07 16:19:24 +0200

    EntryControlList implemented and tested.
    added:
      daemon/coretypes/Core/Types/Proc/EntryControlList.cpp
      daemon/coretypes/Core/Types/Proc/EntryControlList.hpp
      daemon/coretypes/Core/Types/Proc/EntryControlList.t.cpp

---
+++ daemon/coretypes/Core/Types/Proc/EntryControlList.t.cpp
@@ -0,0 +1,90 @@
+/*
+ * EntryControlList.t.cpp
+ *
+ */
+#include <tut.h>
+
+#include "Core/Types/Proc/EntryControlList.hpp"
+
+using namespace Core::Types::Proc;
+
+namespace
+{
+
+struct TestClass
+{
+};
+
+typedef tut::test_group<TestClass> factory;
+typedef factory::object testObj;
+
+factory tf("Core/Types/Proc/EntryControlList");
+} // unnamed namespace
+
+
+namespace tut
+{
+
+// test adding new entry to default-accept object - smoke test
+template<>
+template<>
+void testObj::test<1>(void)
+{
+  EntryControlList ecl=EntryControlList::createDefaultAccept();
+  ecl.add("fran");
+}
+
+// test adding new entry to default-reject object - somoke test
+template<>
+template<>
+void testObj::test<2>(void)
+{
+  EntryControlList ecl=EntryControlList::createDefaultReject();
+  ecl.add("narf");
+}
+
+// test default accepting - positive test
+template<>
+template<>
+void testObj::test<3>(void)
+{
+  EntryControlList ecl=EntryControlList::createDefaultAccept();
+  ecl.add("fran");
+  ecl.add("42");
+  ensure("entry dropped but not listed", ecl.isAcceptable("DooM") );
+}
+
+// test default accepting - negative test
+template<>
+template<>
+void testObj::test<4>(void)
+{
+  EntryControlList ecl=EntryControlList::createDefaultAccept();
+  ecl.add("fran");
+  ecl.add("42");
+  ensure("entry not dropped but listed", ecl.isAcceptable("42")==false );
+}
+
+// test default rejecting - positive test
+template<>
+template<>
+void testObj::test<5>(void)
+{
+  EntryControlList ecl=EntryControlList::createDefaultReject();
+  ecl.add("fran");
+  ecl.add("42");
+  ensure("entry not dropped but not listed", ecl.isAcceptable("DooM")==false );
+}
+
+// test default rejecting - negative test
+template<>
+template<>
+void testObj::test<6>(void)
+{
+  EntryControlList ecl=EntryControlList::createDefaultReject();
+  ecl.add("fran");
+  ecl.add("42");
+  ensure("entry dropped but not listed", ecl.isAcceptable("42") );
+}
+
+} // namespace tut
