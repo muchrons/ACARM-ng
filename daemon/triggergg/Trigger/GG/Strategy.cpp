8becf0483c3e497dba96dfa3d9ff159fd6dcc0b4
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-07-13 12:35:33 +0200

    pinging mechanism has been initially implemented.
    modified:
      daemon/triggergg/Trigger/GG/Strategy.cpp
      daemon/triggergg/Trigger/GG/Strategy.hpp

--- daemon/triggergg/Trigger/GG/Strategy.cpp
+++ daemon/triggergg/Trigger/GG/Strategy.cpp
@@ -4,26 +4,74 @@
  */
 #include <cassert>
 
+#include "Base/Threads/Lock.hpp"
 #include "Trigger/Compose/Summary.hpp"
 #include "Trigger/GG/Strategy.hpp"
 #include "Trigger/GG/MessageSender.hpp"
 
 using namespace std;
+using namespace Base::Threads;
 
 namespace Trigger
 {
 namespace GG
 {
 
+namespace
+{
+struct PingThread
+{
+  explicit PingThread(Strategy &s):
+    s_(&s)
+  {
+  }
+
+  void operator()(void)
+  {
+    // ping server every 60[s]
+    while(true)
+    {
+      sleep(60);
+      boost::this_thread::interruption_point();
+      assert(s_!=NULL);
+      s_->ping();
+    } // while(true)
+  }
+
+private:
+  Strategy *s_;
+}; // struct PingThread
+} // unnamed namespace
+
+
 Strategy::Strategy(const Config &cfg):
   Trigger::Simple::Strategy("gg", cfg.getThresholdConfig() ),
   ggCfg_( cfg.getAccountConfig() ),
-  receiver_( cfg.getReceiver() )
+  receiver_( cfg.getReceiver() ),
+  pingThread_( PingThread(*this) )   // TODO: passing 'this' of object not yet created...
 {
+}
+
+void Strategy::ping(void)
+{
+  Lock lock(mutex_);
+  LOGMSG_DEBUG(log_, "sending ping to server");
+  if( conn_.get()==NULL )
+  {
+    LOGMSG_DEBUG(log_, "not connected - ping not send");
+    return;
+  }
+  if( gg_ping( conn_.get()->get() )!=0 )
+  {
+    LOGMSG_DEBUG(log_, "unable to send ping - unknown error while sending");
+    return;
+  }
+  LOGMSG_DEBUG(log_, "ping sent");
 }
 
 void Strategy::triggerImpl(const Node &n)
 {
+  Lock lock(mutex_);
   // prepare connection
   ConnectionAutoPtr conn=conn_; // take ownership
   assert( conn_.get()==NULL );
--- daemon/triggergg/Trigger/GG/Strategy.hpp
+++ daemon/triggergg/Trigger/GG/Strategy.hpp
@@ -7,6 +7,8 @@
 
 #include <memory>
 
+#include "Base/Threads/Mutex.hpp"
+#include "Commons/Threads/Thread.hpp"
 #include "Trigger/Simple/Strategy.hpp"
 #include "Trigger/GG/Config.hpp"
 #include "Trigger/GG/Connection.hpp"
@@ -26,14 +28,20 @@ public:
    */
   explicit Strategy(const Config &cfg);
 
+  /** \brief ping gg server.
+   */
+  void ping(void);
+
 private:
   virtual void triggerImpl(const Node &n);
 
   typedef std::auto_ptr<Connection> ConnectionAutoPtr;
 
-  const AccountConfig  ggCfg_;
-  const UserID         receiver_;
-  ConnectionAutoPtr    conn_;
+  const AccountConfig          ggCfg_;
+  const UserID                 receiver_;
+  ConnectionAutoPtr            conn_;
+  mutable Base::Threads::Mutex mutex_;
+  Commons::Threads::Thread     pingThread_; // keep this element as a last one!
 }; // class Strategy
 
 } // namespace GG
