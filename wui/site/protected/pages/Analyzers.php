a5bdcc7aef8ed6654ae0d1fe267a00dad09c2d43
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-07-20 18:07:44 +0200

    analyzers have been implemented.
    modified:
      wui/site/protected/pages/Analyzers.page
      wui/site/protected/pages/Analyzers.php

--- wui/site/protected/pages/Analyzers.php
+++ wui/site/protected/pages/Analyzers.php
@@ -2,6 +2,35 @@
 
 class Analyzers extends TPage
 {
+  public function __construct()
+  {
+    parent::__construct();
+    $this->data_=CSQLMap::get()->queryForList('SelectAnalyzers');
+  }
+
+  public function onLoad()
+  {
+    // initialization of GridData
+    if(!$this->IsPostBack)
+    {
+      $this->DataGrid->DataSource=$this->data_;
+      $this->DataGrid->dataBind();
+    }
+  }
+
+  public function changePage($sender, $param)
+  {
+    $this->DataGrid->CurrentPageIndex=$param->NewPageIndex;
+    $this->DataGrid->DataSource      =$this->data_;
+    $this->DataGrid->dataBind();
+  }
+
+  public function pagerCreated($sender, $param)
+  {
+    $param->Pager->Controls->insertAt(0, 'Page: ');
+  }
+
+  private $data_;
 }
 
 ?>
