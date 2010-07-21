666353dafe99e1c44bda235b12737a141a6dec19
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-07-21 16:34:33 +0200

    analyzers and source/destination hosts are now available as links in alert.
    added:
      wui/site/protected/main/DB/CLinkPair.php
    modified:
      wui/site/protected/mp-sqlmap-postgres.xml
      wui/site/protected/pages/Alert.page
      wui/site/protected/pages/Alert.php

--- wui/site/protected/pages/Alert.php
+++ wui/site/protected/pages/Alert.php
@@ -23,19 +23,42 @@ class Alert extends TPage
       $this->AlertCreateTime->Text =$this->alert_->create;
       $this->AlertSeverity->Text   =$this->alert_->severity;
       $this->AlertCertainty->Text  =$this->alert_->certainty;
-      // TODO: analyzers
-      // TODO: sources
-      // TODO: destinations
+      $this->AlertAnalyzers->Text  =$this->makeAnalyzersLinkList($this->alert_->id);
+      $this->AlertSource->Text     =$this->makeHostsLinkList($this->alert_->id, 'SelectAlertSourceHosts');
+      $this->AlertDestination->Text=$this->makeHostsLinkList($this->alert_->id, 'SelectAlertDestinationHosts');
       $this->AlertDescription->Text=$this->alert_->description;
     }
   }
 
-  /*
-  private function 
-  $url = $this->Service->constructUrl('PageName',
-                                      array('id' => 42) );
-      $this->Response->redirect($url);
-  */
+
+  private function makeAnalyzersLinkList($idAlert)
+  {
+    $pairs=CSQLMap::get()->queryForList('SelectAlertAnalyzers', $idAlert);
+    $out  ="";
+    foreach($pairs as $e)
+    {
+      $out.=$this->makeLinkTo('Analyzer', $e->id, $e->name) . ' ';
+    }
+    return $out;
+  }
+
+  private function makeHostsLinkList($idAlert, $query)
+  {
+    $pairs=CSQLMap::get()->queryForList($query, $idAlert);
+    $out  ="";
+    foreach($pairs as $e)
+    {
+      $out.=$this->makeLinkTo('ReportedHost', $e->id, $e->name) . ' ';
+    }
+    return $out;
+  }
+
+  private function makeLinkTo($page, $id, $name)
+  {
+    $url =$this->Service->constructUrl( $page, array('id' => $id) );
+    $href="<a href=\"$url\">$name</a>";
+    return $href;
+  }
 
   private $alert_;
 }
