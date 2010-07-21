730c882356dc6632a49bd86ad85e7227cec78501
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-07-21 17:11:38 +0200

    ReportedHost now has services and processes as well.
    modified:
      wui/site/protected/mp-sqlmap-postgres.xml
      wui/site/protected/pages/ReportedHost.page
      wui/site/protected/pages/ReportedHost.php

--- wui/site/protected/pages/ReportedHost.php
+++ wui/site/protected/pages/ReportedHost.php
@@ -18,14 +18,44 @@ class ReportedHost extends TPage
     if(!$this->IsPostBack)
     {
       // fill fileds with avlues
-      $this->ReportedHostRole->Text   =($this->repHost_->role==='src')?'source':'destination';
-      $this->ReportedHostIP->Text     =$this->repHost_->ip;
-      $this->ReportedHostMask->Text   =$this->repHost_->mask;
-      $this->ReportedHostOS->Text     =$this->repHost_->os;
-      $this->ReportedHostName->Text   =$this->repHost_->name;
-      $this->ReportedHostRefName->Text=$this->repHost_->ref_name;
-      $this->ReportedHostRefURL->Text =$this->repHost_->ref_url;
+      $this->ReportedHostRole->Text     =($this->repHost_->role==='src')?'source':'destination';
+      $this->ReportedHostIP->Text       =$this->repHost_->ip;
+      $this->ReportedHostMask->Text     =$this->repHost_->mask;
+      $this->ReportedHostOS->Text       =$this->repHost_->os;
+      $this->ReportedHostName->Text     =$this->repHost_->name;
+      $this->ReportedHostProcesses->Text=$this->makeProcessesLinkList($this->repHost_->id);
+      $this->ReportedHostServices->Text =$this->makeServicesLinkList($this->repHost_->id);
+      $this->ReportedHostRefName->Text  =$this->repHost_->ref_name;
+      $this->ReportedHostRefURL->Text   =$this->repHost_->ref_url;
     }
+  }
+
+  private function makeProcessesLinkList($idRepHost)
+  {
+    return $this->makeLinkList($idRepHost, 'SelectReportedHostsProcesses', 'ReportedProcess');
+  }
+
+  private function makeServicesLinkList($idRepHost)
+  {
+    return $this->makeLinkList($idRepHost, 'SelectReportedHostsServices', 'ReportedService');
+  }
+
+  private function makeLinkList($idRepHost, $query, $page)
+  {
+    $pairs=CSQLMap::get()->queryForList($query, $idRepHost);
+    $out  ="";
+    foreach($pairs as $e)
+    {
+      $out.=$this->makeLinkTo($page, $e->id, $e->name) . ' ';
+    }
+    return $out;
+  }
+
+  private function makeLinkTo($page, $id, $name)
+  {
+    $url =$this->Service->constructUrl( $page, array('id' => $id) );
+    $href="<a href=\"$url\">$name</a>";
+    return $href;
   }
 
   private $repHost_;
