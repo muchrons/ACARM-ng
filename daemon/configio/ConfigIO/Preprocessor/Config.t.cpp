df9aa6ba3956b6af516ffcf553d9f3855f8dc111
Mariusz Uchroński <mariusz.uchronski@pwr.wroc.pl>
2010-07-23 09:49:59 +0200

    Fix misspells.
    modified:
      daemon/configio/ConfigIO/Preprocessor/Config.t.cpp

--- daemon/configio/ConfigIO/Preprocessor/Config.t.cpp
+++ daemon/configio/ConfigIO/Preprocessor/Config.t.cpp
@@ -39,8 +39,7 @@ template<>
 void testObj::test<1>(void)
 {
   const Config c( makeSections() );
-  // TODO: misspell 'elements'
-  ensure_equals("invalid elemsnts count", c.getSections().size(), 0u);
+  ensure_equals("invalid elements count", c.getSections().size(), 0u);
 }
 
 // test non-empty section
@@ -49,8 +48,7 @@ template<>
 void testObj::test<2>(void)
 {
   const Config c( makeSections(42) );
-  // TODO: misspell 'elements'
-  ensure_equals("invalid elemsnts count", c.getSections().size(), 42u);
+  ensure_equals("invalid elements count", c.getSections().size(), 42u);
 }
 
 } // namespace tut
