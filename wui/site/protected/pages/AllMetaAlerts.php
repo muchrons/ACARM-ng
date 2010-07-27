c3259fb747d8a1d5e0b9ac69f6409226dd830317
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-07-27 13:55:21 +0200

    AllMetaAlerts page created.
    added:
      wui/site/protected/main/DB/CMetaAlertSummaryEntry.php
    modified:
      wui/site/protected/mp-sqlmap-postgres.xml
      wui/site/protected/pages/AllMetaAlerts.page
      wui/site/protected/pages/AllMetaAlerts.php

--- wui/site/protected/pages/AllMetaAlerts.php
+++ wui/site/protected/pages/AllMetaAlerts.php
@@ -2,6 +2,42 @@
 
 class AllMetaAlerts extends TPage
 {
+  public function __construct()
+  {
+    parent::__construct();
+    $this->data_=CSQLMap::get()->queryForList('SelectMetaAlertsSummary');
+    // add link field
+    foreach($this->data_ as $e)
+    {
+      $url    =$this->Service->constructUrl( 'MetaAlert', array('id' => $e->id) );
+      $href   ="<a href=\"$url\">see</a>";
+      $e->link=$href;
+    }
+  }
+
+  public function onLoad()
+  {
+    // initialization of GridData
+    if(!$this->IsPostBack)
+    {
+      $this->DataGrid->DataSource=$this->data_;
+      $this->DataGrid->dataBind();
+    }
+  }
+
+  public function changePage($sender, $param)
+  {
+    $this->DataGrid->CurrentPageIndex=$param->NewPageIndex;
+    $this->DataGrid->DataSource      =$this->data_;
+    $this->DataGrid->dataBind();
+  }
+
+  public function pagerCreated($sender, $param)
+  {
+    $param->Pager->Controls->insertAt(0, 'Page: ');
+  }
+
+  private $data_;
 }
 
 ?>
