801515b738bd3bde58a3b2d68c3b0c0d01d47daa
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-07-28 11:53:20 +0200

    fixes from filteripblacklist review.
    modified:
      daemon/filteripblacklist/Filter/IPBlackList/EntryProcessor.cpp
      daemon/filteripblacklist/Filter/IPBlackList/Strategy.hpp

--- daemon/filteripblacklist/Filter/IPBlackList/EntryProcessor.cpp
+++ daemon/filteripblacklist/Filter/IPBlackList/EntryProcessor.cpp
@@ -32,8 +32,7 @@ void EntryProcessor::operator()(Persistency::GraphNodePtrNN leaf)
 {
   assert( leaf->isLeaf() && "wrong graph-passing algorithm choosen" );
   processHosts(leaf, leaf->getAlert()->getReportedSourceHosts() );
-  //TODO: dead code
-  //processHosts(leaf, leaf->getAlert()->getReportedTargetHosts() );
+  processHosts(leaf, leaf->getAlert()->getReportedTargetHosts() );
 }
 
 void EntryProcessor::processHosts(Persistency::GraphNodePtrNN              leaf,
