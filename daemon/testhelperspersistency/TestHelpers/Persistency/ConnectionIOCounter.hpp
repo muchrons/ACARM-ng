d28fa42004e49a703408e7d774002382277d2066
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-08-24 10:40:31 +0200

    added generic counter implementaiton for DynamicConfig along with connection that creates such object nad persists it's state.
    added:
      daemon/testhelperspersistency/TestHelpers/Persistency/ConnectionIOCounter.hpp
      daemon/testhelperspersistency/TestHelpers/Persistency/ConnectionIOCounter.t.cpp
      daemon/testhelperspersistency/TestHelpers/Persistency/IODynamicConfigCounter.cpp
      daemon/testhelperspersistency/TestHelpers/Persistency/IODynamicConfigCounter.hpp

---
+++ daemon/testhelperspersistency/TestHelpers/Persistency/ConnectionIOCounter.hpp
@@ -0,0 +1,81 @@
+/*
+ * ConnectionIOCounter.hpp
+ *
+ */
+#ifndef INCLUDE_TESTHELPERS_PERSISTENCY_CONNECTIONIOCOUNTER_HPP_FILE
+#define INCLUDE_TESTHELPERS_PERSISTENCY_CONNECTIONIOCOUNTER_HPP_FILE
+
+/* public header */
+
+#include <boost/shared_ptr.hpp>
+#include <cassert>
+
+#include "TestHelpers/Persistency/ConnectionUserStubBase.hpp"
+#include "TestHelpers/Persistency/IODynamicConfigCounter.hpp"
+
+namespace TestHelpers
+{
+namespace Persistency
+{
+
+// helper typedef
+typedef boost::shared_ptr<IODynamicConfigCounter> IODynamicConfigCounterPtr;
+
+// counter that forwards all calls to other counter, that does not change
+struct DynamicConfigCounter: public ::Persistency::IO::DynamicConfig
+{
+  DynamicConfigCounter(IODynamicConfigCounterPtr counter, const Owner &owner, ::Persistency::IO::Transaction &t):
+    ::Persistency::IO::DynamicConfig(owner, t),
+    counter_(counter)
+  {
+    assert( counter_.get()!=NULL );
+  }
+
+  virtual void writeImpl(::Persistency::IO::Transaction &/*t*/, const Key &key, const Value &value)
+  {
+    counter_->write(key, value);
+  }
+
+  virtual ValueNULL readImpl(::Persistency::IO::Transaction &/*t*/, const Key &key)
+  {
+    return counter_->read(key);
+  }
+
+  virtual Value readConstImpl(::Persistency::IO::Transaction &/*t*/, const Key &key)
+  {
+    return counter_->readConst(key);
+  }
+
+  IODynamicConfigCounterPtr counter_;
+}; // class DynamicConfigCounter
+
+
+// connection that returns different implementaitons of the same counter
+struct ConnectionIOCounter: public ConnectionUserStubBase
+{
+  // key - key name that will be used for the counter
+  ConnectionIOCounter(const std::string &owner, const std::string &key):
+    owner_(owner),
+    key_(key)
+  {
+  }
+
+  virtual ::Persistency::IO::DynamicConfigAutoPtr dynamicConfigImpl(const ::Persistency::IO::DynamicConfig::Owner &owner,
+                                                                    ::Persistency::IO::Transaction                &t)
+  {
+    tut::ensure_equals("invalid owner", owner.get(), owner_);
+    if( ioCounter_.get()==NULL )
+      ioCounter_.reset( new IODynamicConfigCounter(t, key_) );
+    tut::ensure("NULL ioCounter_ - logic error", ioCounter_.get()!=NULL );
+    return ::Persistency::IO::DynamicConfigAutoPtr( new DynamicConfigCounter(ioCounter_, owner, t) );
+  }
+
+  const std::string         owner_;
+  const std::string         key_;
+  IODynamicConfigCounterPtr ioCounter_;
+}; // struct ConnectionIOCounter
+
+} // namespace Persistency
+} // namespace TestHelpers
+
+#endif
---
+++ daemon/testhelperspersistency/TestHelpers/Persistency/IODynamicConfigCounter.hpp
@@ -0,0 +1,37 @@
+/*
+ * IODynamicConfigCounter.hpp
+ *
+ */
+#ifndef INCLUDE_TESTHELPERS_PERSISTENCY_IODYNAMICCONFIGCOUNTER_HPP
+#define INCLUDE_TESTHELPERS_PERSISTENCY_IODYNAMICCONFIGCOUNTER_HPP
+
+#include <string>
+
+#include "Persistency/MetaAlert.hpp"
+#include "Persistency/IO/DynamicConfig.hpp"
+#include "Persistency/IO/Transaction.hpp"
+
+namespace TestHelpers
+{
+namespace Persistency
+{
+
+struct IODynamicConfigCounter: public ::Persistency::IO::DynamicConfig
+{
+  IODynamicConfigCounter(::Persistency::IO::Transaction &t, const std::string &keyValue);
+
+  virtual void writeImpl(::Persistency::IO::Transaction &t, const Key &key, const Value &value);
+
+  virtual ValueNULL readImpl(::Persistency::IO::Transaction &t, const Key &key);
+
+  virtual Value readConstImpl(::Persistency::IO::Transaction &t, const Key &key);
+
+  const std::string                     keyValue_;
+  bool                                  isNull_;
+  ::Persistency::MetaAlert::ID::Numeric id_;
+}; // struct IODynamicConfigCounter
+
+} // namespace Persistency
+} // namespace TestHelpers
+
+#endif
