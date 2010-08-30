aed7db8ddf09671168c9a0494288d2bbdf75dd21
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-08-30 17:30:36 +0200

    Parser for general config has been implemented, but not tested yet.
    added:
      daemon/configio/ConfigIO/ParseGeneralConfig.cpp
      daemon/configio/ConfigIO/ParseGeneralConfig.hpp

---
+++ daemon/configio/ConfigIO/ParseGeneralConfig.cpp
@@ -0,0 +1,54 @@
+/*
+ * ParseGeneralConfig.cpp
+ *
+ */
+#include <boost/lexical_cast.hpp>
+#include <boost/numeric/conversion/cast.hpp>
+#include <cassert>
+
+#include "ConfigIO/ParseGeneralConfig.hpp"
+
+namespace ConfigIO
+{
+
+namespace
+{
+
+int toInt(const XML::Node &node)
+{
+  return boost::lexical_cast<int>( node.getValuesString() );
+} // toInt
+
+unsigned int toUnsignedInt(const XML::Node &node)
+{
+  return boost::numeric_cast<unsigned int>( toInt(node) );
+} // toUnsignedInt()
+
+GeneralConfig parseConfig(const XML::Node &node)
+{
+  try
+  {
+    const std::string  url    =node.getChild("url").getValuesString();
+    const unsigned int cleanup=toUnsignedInt( node.getChild("cleanupInterval") );
+    // return final object
+    return GeneralConfig(url, cleanup);
+  }
+  catch(const std::exception &ex)
+  {
+    // rethrown covering implementation details
+    throw ExceptionParseError(SYSTEM_SAVE_LOCATION, ex.what() );
+  }
+
+  assert(!"code never reaches here");
+} // parseConfig()
+} // unnamed namespace
+
+
+ParseGeneralConfig::ParseGeneralConfig(const XML::Node &node):
+  gc_( parseConfig(node) )
+{
+  // this should be already checked higher
+  assert( node.getName()=="generic" );
+}
+
+} // namespace ConfigIO
