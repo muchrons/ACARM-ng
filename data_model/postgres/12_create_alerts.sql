e7236c07178fbebf9085289213b08297145f44e0
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-09-01 13:39:44 +0200

    comment fixed.
    modified:
      data_model/postgres/12_create_alerts.sql

--- data_model/postgres/12_create_alerts.sql
+++ data_model/postgres/12_create_alerts.sql
@@ -106,7 +106,7 @@ CREATE TABLE    analyzers
   ip      inet         NULL,
 
   UNIQUE(sys_id),
-  UNIQUE(sys_id, name, version, os, ip)  -- this is test for uniqueness in fact
+  UNIQUE(sys_id, name, version, os, ip)  -- this will make analyzers' scanning faster
 );
 
 
