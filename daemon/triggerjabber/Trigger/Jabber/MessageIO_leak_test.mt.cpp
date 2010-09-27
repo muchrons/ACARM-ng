ff853343adbb5638292391842321a6499feef80c
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-09-27 16:34:40 +0200

    added manual leak test for message IO.
    added:
      daemon/triggerjabber/Trigger/Jabber/MessageIO_leak_test.mt.cpp

---
+++ daemon/triggerjabber/Trigger/Jabber/MessageIO_leak_test.mt.cpp
@@ -0,0 +1,24 @@
+/*
+ * MessageIO_leak_test.mt.cpp
+ *
+ */
+#include <iostream>
+
+#include "Trigger/Jabber/MessageIO.hpp"
+#include "Trigger/Jabber/TestAccount.t.hpp"
+
+using namespace std;
+using namespace Trigger::Jabber;
+
+int main(void)
+{
+  cout<<"*** run this test under valgrind and see if memory is leaking ***"<<endl;
+  Connection conn( getTestConfig1() );
+  MessageIO  mio(conn);
+  for(int i=0; i<2; ++i)
+    mio.discardIncommingMessages();
+  for(int i=0; i<2; ++i)
+    mio.send( getTestConfig2().getLogin(), "alice has a cat" );
+
+  return 0;
+}
