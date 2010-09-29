698106cb4a7ff80baf234bca086489ab9c8d31f7
Mariusz Uchroński <mariusz.uchronski@pwr.wroc.pl>
2010-09-29 14:07:30 +0200

    Comments added.
    modified:
      daemon/triggerjabber/Trigger/Jabber/ExceptionCreatingError.hpp

--- daemon/triggerjabber/Trigger/Jabber/ExceptionCreatingError.hpp
+++ daemon/triggerjabber/Trigger/Jabber/ExceptionCreatingError.hpp
@@ -11,7 +11,7 @@ namespace Trigger
 {
 namespace Jabber
 {
-/** \brief exception thrown when sending message failed.
+/** \brief exception thrown when creating message failed.
  */
 class ExceptionCreatingError: public Exception
 {
@@ -19,7 +19,6 @@ public:
   /** \brief create execption with given message.
    *  \param where    place where exception has been thrown.
    *  \param receiver user that was suposed to receive message.
-   *  \param ret      error code returned by library
    */
   ExceptionCreatingError(const Location &where, const std::string &receiver):
     Exception(where, cc("unable to create message to user ", receiver) )
