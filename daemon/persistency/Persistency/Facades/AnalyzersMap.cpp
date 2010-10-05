fc9388188836147dc24dd4755c1ba755ece25f10
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-10-05 07:53:18 +0200

    files names fixed.
    added:
      daemon/persistency/Persistency/Facades/AnalyzersMap.cpp
    renamed:
      daemon/persistency/Persistency/Facades/AnalyzersMap.cpp => daemon/persistency/Persistency/Facades/AnalyzersLocalCache.cpp
    modified:
      daemon/persistency/Persistency/Facades/AnalyzersLocalCache.cpp

--- daemon/persistency/Persistency/Facades/AnalyzersMap.cpp
+++ daemon/persistency/Persistency/Facades/AnalyzersMap.cpp
@@ -2,30 +2,12 @@
  * AnalyzersMap.cpp
  *
  */
-#include "Input/detail/AnalyzersMap.hpp"
+#include "Persistency/Facades/AnalyzersMap.hpp"
 
-namespace Input
+namespace Persistency
 {
-namespace detail
+namespace Facades
 {
 
-Persistency::AnalyzerPtrNN::SharedPtr AnalyzersMap::get(const std::string &id) const
-{
-  InputIDMap::const_iterator it=map_.find(id);
-  if( it==map_.end() )
-    return Persistency::AnalyzerPtrNN::SharedPtr(); // NULL
-  return it->second;
-}
-
-void AnalyzersMap::add(const std::string &id, Persistency::AnalyzerPtrNN analyzer)
-{
-  // do not allow owerwrites (it means some sort of error)
-  if( get(id).get()!=NULL )
-    throw ExceptionAddingAlreadyExistingKey(SYSTEM_SAVE_LOCATION, id, analyzer);
-  // add to collection
-  map_.insert( InputIDMap::value_type(id, analyzer) );
-  assert( get(id)==analyzer && "entry not added" );
-}
-
-} // namespace detail
-} // namespace Input
+} // namespace Facades
+} // namespace Persistency
