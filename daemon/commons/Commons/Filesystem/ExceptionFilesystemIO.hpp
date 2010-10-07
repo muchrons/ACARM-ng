73b51f68e3bdb96c7a0dcd4dd5b6f3ac401b3856
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-10-07 15:17:13 +0200

    exceptions' hierarchy has been applied.
    added:
      daemon/commons/Commons/Filesystem/ExceptionSecurityIssue.hpp
    renamed:
      daemon/commons/Commons/Filesystem/ExceptionFileIO.hpp => daemon/commons/Commons/Filesystem/ExceptionFilesystemIO.hpp
    modified:
      daemon/commons/Commons/Filesystem/ExceptionFilesystemIO.hpp

---
+++ daemon/commons/Commons/Filesystem/ExceptionFilesystemIO.hpp
@@ -0,0 +1,40 @@
+/*
+ * ExceptionFilesystemIO.hpp
+ *
+ */
+#ifndef INCLUDE_COMMONS_FILESYSTEM_EXCEPTIONFILESYSTEMIO_HPP_FILE
+#define INCLUDE_COMMONS_FILESYSTEM_EXCEPTIONFILESYSTEMIO_HPP_FILE
+
+#include <boost/filesystem.hpp>
+
+#include "Commons/Filesystem/Exception.hpp"
+
+namespace Commons
+{
+namespace Filesystem
+{
+/** \brief exception thrown on I/O related problems (read/write error, syscalls failure, etc...)
+ */
+class ExceptionFilesystemIO: public Exception
+{
+public:
+  /** \brief create execption with given message.
+   *  \param where   place where exception has been thrown.
+   *  \param path    path to file, that has been used.
+   *  \param op      operation that failed (ex.: open, close, create, etc...).
+   *  \param details some details on error conditions.
+   */
+  ExceptionFilesystemIO(const Location                &where,
+                        const boost::filesystem::path &path,
+                        const std::string             &op,
+                        const std::string             &details):
+    Exception(where, cc("error while performing '", op, "' operation on file '", path, "' (cannonical: '",
+                        cc(boost::filesystem::system_complete(path), "'): ", details)) )
+  {
+  }
+}; // class ExceptionFilesystemIO
+
+} // namespace Filesystem
+} // namespace Commons
+
+#endif
