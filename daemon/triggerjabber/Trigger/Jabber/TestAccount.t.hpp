652764c8c24563d57645472c0c43d548e2b32d0e
Mariusz Uchroński <mariusz.uchronski@pwr.wroc.pl>
2010-10-19 14:11:35 +0200

    Fixed link errors for mtests.
    modified:
      daemon/triggerjabber/Trigger/Jabber/TestAccount.t.cpp
      daemon/triggerjabber/Trigger/Jabber/TestAccount.t.hpp

--- daemon/triggerjabber/Trigger/Jabber/TestAccount.t.hpp
+++ daemon/triggerjabber/Trigger/Jabber/TestAccount.t.hpp
@@ -5,7 +5,9 @@
 #ifndef INCLUDE_TRIGGER_JABBER_TESTACCOUNT_T_HPP_FILE
 #define INCLUDE_TRIGGER_JABBER_TESTACCOUNT_T_HPP_FILE
 
+#include <tut.h>
 #include <loudmouth/loudmouth.h>
+#include <boost/algorithm/string.hpp>
 
 #include "System/ScopedPtrCustom.hpp"
 #include "System/Timer.hpp"
@@ -13,30 +15,88 @@
 #include "TestHelpers/Data/jabber1.hpp"
 #include "TestHelpers/Data/jabber2.hpp"
 
-namespace Trigger
-{
-namespace Jabber
+namespace
 {
 
-extern "C" struct MessageHandler;
+Trigger::Jabber::AccountConfig getTestConfig1(void)
+{
+  return Trigger::Jabber::AccountConfig(JABBER1_TEST_ACCOUNT_SERVER,
+                                        JABBER1_TEST_ACCOUNT_LOGIN,
+                                        JABBER1_TEST_ACCOUNT_PASS);
+}
 
-AccountConfig getTestConfig1(void);
-
-AccountConfig getTestConfig2(void);
+Trigger::Jabber::AccountConfig getTestConfig2(void)
+{
+  return Trigger::Jabber::AccountConfig(JABBER2_TEST_ACCOUNT_SERVER,
+                                        JABBER2_TEST_ACCOUNT_LOGIN,
+                                        JABBER2_TEST_ACCOUNT_PASS);
+}
 
 // helper for backward compatibility
-AccountConfig getTestConfig(void);
+Trigger::Jabber::AccountConfig getTestConfig(void)
+{
+  return getTestConfig1();
+}
 
-extern "C" LmHandlerResult
+struct MessageHandler
+{
+  std::string msg_;
+  std::string sender_;
+};
+
+LmHandlerResult
 handleMessages (LmMessageHandler * /*handler*/,
                 LmConnection     * /*connection*/,
                 LmMessage        *m,
-                MessageHandler   *mh);
+                MessageHandler   *mh)
+{
+  // TODO: throwing exceptions to C code (here - caller) is undefined behaviour
+  tut::ensure("message is NULL", m!=NULL);
+  if ((lm_message_get_sub_type (m) != LM_MESSAGE_SUB_TYPE_CHAT &&
+       lm_message_get_sub_type (m) != LM_MESSAGE_SUB_TYPE_NORMAL)) {
 
-std::string getMessageFromAccount(const Trigger::Jabber::AccountConfig &account, const std::string &sender);
+    return LM_HANDLER_RESULT_ALLOW_MORE_HANDLERS;
+  }
+  tut::ensure("message handler is NULL", mh!=NULL);
+  mh->sender_ = std::string(lm_message_node_get_attribute (m->node, "from"));
+  mh->msg_ = std::string(lm_message_node_get_value( lm_message_node_get_child(m->node, "body") ));
+  return LM_HANDLER_RESULT_REMOVE_MESSAGE;
+}
 
-}  // Jabber
+std::string getMessageFromAccount(const Trigger::Jabber::AccountConfig &account, const std::string &sender)
+{
+  Trigger::Jabber::Connection conn(account);
+  MessageHandler              mh;
+  LmMessage                   *m = lm_message_new(NULL, LM_MESSAGE_TYPE_PRESENCE);
+  System::ScopedPtrCustom<LmMessage, lm_message_unref> mPtr(m);
+  tut::ensure("message is NULL", mPtr.get()!=NULL);
+  // TODO: casting to function pointer to use arg void* with LmHandleMessageFunction is slippy (what if code changes?) - better
+  //       use explicit void* as argument to handleMessages() and cast inside the call and be on the safe side. :)
+  LmMessageHandler            *handler = lm_message_handler_new((LmHandleMessageFunction)handleMessages, &mh, NULL);
+  tut::ensure("message handler is NULL", handler!=NULL);
+  lm_connection_register_message_handler(conn.get(), handler,
+                                         LM_MESSAGE_TYPE_MESSAGE,
+                                         LM_HANDLER_PRIORITY_NORMAL);
 
-}  // Trigger
+  lm_message_handler_unref (handler);
+  lm_connection_send(conn.get(), mPtr.get(), NULL);
+  const double timeout = 20.0f;      // timeout is 20[s]
+  System::Timer t;
+  for(;;)
+  {
+    // short sleep here to avoid waste CPU too much
+    usleep(5000);
+    // wait for something
+    if(t.elapsed() > timeout)
+      tut::fail("waiting for messages timeouted");
+    g_main_context_iteration(NULL, FALSE);
+    // convert sender string to format "login@server"
+    boost::replace_last(mh.sender_, "/acarm-ng", "");
+    if(mh.sender_ != sender)
+      continue;
+    return mh.msg_;
+  }
+}
+}  // unnamed namespace
 
 #endif
