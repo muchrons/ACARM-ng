ac01920d1797fcff9f209a1a40a7442130b4fa78
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2010-10-26 09:43:46 +0200

    small refactoring of test case; added loop devices to list as well.
    modified:
      daemon/commons/Commons/Filesystem/isElementSane.t.cpp

--- daemon/commons/Commons/Filesystem/isElementSane.t.cpp
+++ daemon/commons/Commons/Filesystem/isElementSane.t.cpp
@@ -99,17 +99,18 @@ void testObj::test<8>(void)
   blockDevs.push_back("/dev/hda");
   blockDevs.push_back("/dev/hda1");
   blockDevs.push_back("/dev/hda5");
+  blockDevs.push_back("/dev/loop");
+  blockDevs.push_back("/dev/loop0");
 
-  bool checked=false;
   for(vector<string>::const_iterator it=blockDevs.begin(); it!=blockDevs.end(); ++it)
     if( exists(*it) )
     {
-      checked=true;
+      // ok - device exists, so check it
       ensure("block device marked sane", isElementSane(*it)==false );
-      break;
+      return;
     }
 
-  ensure("no block device found - check the list in test case", checked);
+  fail("no known block device found - check the list in test case");
 }
 
 } // namespace tut
