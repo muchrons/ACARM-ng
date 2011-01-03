bb46a7b59bb9326ea6b80419330d631a669752eb
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2011-01-03 15:02:37 +0100

    reverted to previous version of ConnectionBuilder for stubs.
    modified:
      daemon/persistencystubs/Persistency/IO/Stubs/ConnectionBuilder.cpp

--- daemon/persistencystubs/Persistency/IO/Stubs/ConnectionBuilder.cpp
+++ daemon/persistencystubs/Persistency/IO/Stubs/ConnectionBuilder.cpp
@@ -2,8 +2,14 @@
  * ConnectionBuilder.cpp
  *
  */
+#include <cassert>
+
+#include "Commons/Factory/RegistratorHelper.hpp"
 #include "Persistency/IO/Stubs/Connection.hpp"
 #include "Persistency/IO/Stubs/ConnectionBuilder.hpp"
+
+using namespace std;
+
 
 namespace Persistency
 {
@@ -11,6 +17,16 @@ namespace IO
 {
 namespace Stubs
 {
+
+namespace
+{
+// this code registers builder to readers factory, making it available for
+// further usage, by generic interface.
+const Commons::Factory::RegistratorHelper<BackendFactory, ConnectionBuilder> g_rh;
+} // unnamed namespace
+
+FORCE_LINK_THIS_OBJECT(Persistency_IO_Stubs_ConnectionBuilder)
+
 
 ConnectionBuilder::ConnectionBuilder(void):
   name_("stubs"),
@@ -21,6 +37,8 @@ ConnectionBuilder::ConnectionBuilder(void):
 ConnectionBuilder::FactoryPtr ConnectionBuilder::buildImpl(const Options &/*options*/) const
 {
   LOGMSG_INFO(log_, "building Persistency::IO::Stubs");
+  assert(g_rh.isRegistered() && "oops - registration failed");
+
   return ConnectionBuilder::FactoryPtr(new Connection);
 }
 
