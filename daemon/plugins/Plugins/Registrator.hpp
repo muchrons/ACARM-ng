a4fdfdc41c0152df74e1bc46e424a1dbecf2c8e5
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2011-01-04 15:03:42 +0100

    initial concept for deallocation after the singletons (does not work yet).
    modified:
      daemon/plugins/Plugins/Registrator.hpp
      daemon/plugins/Plugins/Registrator.t.cpp
      daemon/plugins/Plugins/c_helper.t.cpp

--- daemon/plugins/Plugins/Registrator.hpp
+++ daemon/plugins/Plugins/Registrator.hpp
@@ -7,9 +7,11 @@
 
 /* public header */
 
+#include <cassert>
 #include <boost/static_assert.hpp>
 #include <boost/type_traits/is_polymorphic.hpp>
 
+#include "System/AtExit.hpp"
 #include "System/Plugins/DynamicObject.hpp"
 #include "Plugins/ExceptionRegistrationError.hpp"
 
@@ -37,22 +39,43 @@ private:
    */
   struct DerivedBuilder: TBuilder
   {
+    typedef System::Plugins::DynamicObject              DynamicObj;
+    typedef System::AtExitMemoryDeallocator<DynamicObj> DynamicObjDeallocator;
+
     /** \brief constructor saving handle.
      *  \param handle handle to shared object required for given plugin.
      */
-    explicit DerivedBuilder(const System::Plugins::DynamicObject &dynObj):
-      dynObj_(dynObj)
+    explicit DerivedBuilder(const DynamicObj &dynObj):
+      deallocator_( new DynamicObjDeallocator( new DynamicObj(dynObj) ) )
     {
+      assert( deallocator_.get()!=NULL );
       // in order to safely derive from TBuilder it has to be polymorphic already.
       // it shuld be always true, since this should be interface derived object,
       // though we'd better be safe than sorry.
       BOOST_STATIC_ASSERT( boost::is_polymorphic<TBuilder>::type::value );
     }
 
+    ~DerivedBuilder(void)
+    {
+      assert( deallocator_.get()!=NULL );
+      try
+      {
+        // pass ownershit of the handle to the AtExit - it should be destroyed
+        // after all the singletons are done.
+        System::AtExit::registerDeallocator(deallocator_);
+      }
+      catch(...)
+      {
+        assert(!"exception thrown in d-tor, when registering deallocator for "
+                "dynamic library - this usually crashes application...");
+      }
+    }
+
   private:
     // holding handle ensures that library is opened as long as factory exists.
     // this is required to ensure that all symbols in use are there.
-    System::Plugins::DynamicObject dynObj_;
+    // deallocation object is to be passed to the AtExit when deallocation will be called.
+    System::AtExit::TDeallocPtr deallocator_;
   }; // struct DerivedBuilder
 
 
