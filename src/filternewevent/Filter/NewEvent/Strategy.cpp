68c9a5adadc5017e0af0e630deffb3229ef32baf
Mariusz Uchroński <mariusz.uchronski@pwr.wroc.pl>
2011-02-07 20:17:28 +0100

    Temporary commit.
    added:
      src/filternewevent/Filter/NewEvent/FactoryBuilder.cpp
      src/filternewevent/Filter/NewEvent/FactoryBuilder.hpp
      src/filternewevent/Filter/NewEvent/FactoryBuilder.t.cpp
    modified:
      src/filternewevent/Filter/NewEvent/Strategy.cpp
      src/filternewevent/Filter/NewEvent/Strategy.hpp

--- src/filternewevent/Filter/NewEvent/Strategy.cpp
+++ src/filternewevent/Filter/NewEvent/Strategy.cpp
@@ -3,11 +3,32 @@
  *
  */
 
+#include "Filter/NewEvent/Strategy.hpp"
 
 namespace Filter
 {
 namespace NewEvent
 {
+
+Strategy::Strategy(const std::string &name, const Parameters &params):
+  Filter::Strategy<Data>("newevent", name),
+  params_(params)
+{
+}
+
+Core::Types::Proc::EntryControlList Strategy::createEntryControlList(void)
+{
+    Core::Types::Proc::EntryControlList ecl=Core::Types::Proc::EntryControlList::createDefaultReject();
+      ecl.add("*input*");   // TODO: magic value
+        return ecl;
+}
+
+void Strategy::processImpl(Node               n,
+                           NodesTimeoutQueue &ntq,
+                           BackendFacade     &bf)
+{
+  //TODO
+}
 
 } // namespace NewEvent
 } // namespace Filter
--- src/filternewevent/Filter/NewEvent/Strategy.hpp
+++ src/filternewevent/Filter/NewEvent/Strategy.hpp
@@ -7,12 +7,62 @@
 
 /* public header */
 
+#include "Filter/Strategy.hpp"
 
 namespace Filter
 {
 namespace NewEvent
 {
 
+/** \brief this filter does not need to handle any data.
+ *  */
+struct Data
+{
+}; // struct Data
+
+/** \brief filter updating priority of an meta alert, that has name, not found before.
+ */
+class Strategy: public Filter::Strategy<Data>
+{
+public:
+  /** \brief paramters for strategy.
+   */
+  struct Parameters
+  {
+    /** \brief create paramters object.
+     *  \param timeout   (in seconds). //TODO add description
+     *  \param priDelta increment of priority when host is found.
+     */
+    Parameters(const unsigned int timeout,
+               const double       priDelta):
+      timeout_(timeout),
+      priDelta_(priDelta)
+    {
+    }
+
+    const unsigned int timeout_;        ///< timeout
+    const double       priDelta_;       ///< priority increase
+  }; // struct Parameters
+
+  /** \brief create instance.
+   *  \param name  name for dns resolver.
+   *  \param params paramters for dns resolver.
+   */
+  Strategy(const std::string &name, const Parameters &params);
+
+  /** \brief create ECL for this filter.
+   *  \return ECL for filter.
+   */
+  static Core::Types::Proc::EntryControlList createEntryControlList(void);
+
+private:
+
+  virtual void processImpl(Node               n,
+                           NodesTimeoutQueue &ntq,
+                           BackendFacade     &bf);
+
+  const Parameters params_;
+}; // class Strategy
 } // namespace NewEvent
 } // namespace Filter
 
