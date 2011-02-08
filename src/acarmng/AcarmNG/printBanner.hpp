4f2407020f35c55bf1baed290ab6ed0acf439994
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2011-02-08 11:09:45 +0100

    banner printing function has been moved to separate file.
    added:
      src/acarmng/AcarmNG/printBanner.cpp
      src/acarmng/AcarmNG/printBanner.hpp
    modified:
      src/acarmng/AcarmNG/main.cpp

---
+++ src/acarmng/AcarmNG/printBanner.hpp
@@ -0,0 +1,13 @@
+/*
+ * printBanner.hpp
+ *
+ */
+#ifndef INCLUDE_ACARMNG_PRINTBANNER_HPP_FILE
+#define INCLUDE_ACARMNG_PRINTBANNER_HPP_FILE
+
+/** \brief priend greeting banner for application.
+ *  \param app application/binary name.
+ */
+void printBanner(const char *app);
+
+#endif
