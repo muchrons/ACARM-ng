29afd78cc22c3595edc3e44279f3d904611badce
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2011-02-09 16:57:20 +0100

    doxygen has been updated.
    modified:
      src/filterusersmonitor/Filter/UsersMonitor/Data.hpp

--- src/filterusersmonitor/Filter/UsersMonitor/Data.hpp
+++ src/filterusersmonitor/Filter/UsersMonitor/Data.hpp
@@ -19,22 +19,46 @@ namespace UsersMonitor
 
 /** \brief data of the filters' observed/correlated entry.
  *
- *  holds entry or a set of user name entries.
+ *  holds entry or a set of user name entries from a given alert.
  */
 class Data
 {
 public:
+  /** \brief user name type. */
   typedef std::string       Name;
+  /** \brief colection of users' names. */
   typedef std::vector<Name> Names;
 
+  /** \brief create empty collection.
+   */
   Data(void);
+  /** \brief create collection with one name.
+   *  \param name name to assign.
+   *  \param skip skip list to apply before assigning names.
+   *
+   *  this call is mostly usefull when createing meta-alert, corelated by a given name.
+   */
   Data(const Name &name, const Names &skip);
+  /** \brief create colleciton with users from a given alert.
+   *  \param a    alert to get user names from.
+   *  \param skip skip list to apply before assigning names.
+   */
   Data(const Persistency::ConstAlertPtrNN &a, const Names &skip);
 
+  /** \brief gives read-only access to collection of (non-skipped) user names.
+   */
   const Names &get(void) const;
 
+  /** \brief returns user name that is common with other data entry.
+   *  \param other element co compare with.
+   *  \return iterator to the element of collection from this object (see: get())
+   *          when common user name is found, or end iterator if not.
+   */
   Names::const_iterator commonWith(const Data &other) const;
 
+  /** \brief performs swap operation between two objects.
+   *  \param other element to swap contents with.
+   */
   void swap(Data &other);
 
 private:
