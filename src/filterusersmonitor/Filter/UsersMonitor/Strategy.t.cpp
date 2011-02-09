4b3cb2ca295959f479f66d4f082205a54aec2f0c
Bartek 'BaSz' Szurgot <bartosz.szurgot@pwr.wroc.pl>
2011-02-09 15:34:17 +0100

    strategy has been tested.
    modified:
      src/filterusersmonitor/Filter/UsersMonitor/Strategy.t.cpp

--- src/filterusersmonitor/Filter/UsersMonitor/Strategy.t.cpp
+++ src/filterusersmonitor/Filter/UsersMonitor/Strategy.t.cpp
@@ -43,40 +43,56 @@ template<>
 template<>
 void testObj::test<1>(void)
 {
-  GraphNodePtrNN leaf=makeNewLeaf( mkAlert() );
-  s_.process(leaf, changed_);
+  for(int i=0; i<2; ++i)
+  {
+    GraphNodePtrNN leaf=makeNewLeaf( mkAlert() );
+    s_.process(leaf, changed_);
+    ensure_equals("something has been changed", changed_.size(), 0u);
+  }
 }
 
-// test if entry is not interesting when no user is set
+// test correlation
 template<>
 template<>
 void testObj::test<2>(void)
 {
-  fail("TODO");
-}
+  GraphNodePtrNN leaf1=makeNewLeaf( mkAlert("alice", "cat") );
+  s_.process(leaf1, changed_);
+  ensure_equals("something has been changed", changed_.size(), 0u);
 
-// test canCorrelate method
-template<>
-template<>
-void testObj::test<3>(void)
-{
-  fail("TODO");
+  GraphNodePtrNN leaf2=makeNewLeaf( mkAlert("cat", "kszy") );
+  s_.process(leaf2, changed_);
+  ensure_equals("correlation failed", changed_.size(), 1u);
 }
 
 // test if can correltate will return false for non-overlaping entries
 template<>
 template<>
-void testObj::test<4>(void)
+void testObj::test<3>(void)
 {
-  fail("TODO");
+  GraphNodePtrNN leaf1=makeNewLeaf( mkAlert("alice", "cat") );
+  s_.process(leaf1, changed_);
+  ensure_equals("something has been changed", changed_.size(), 0u);
+
+  GraphNodePtrNN leaf2=makeNewLeaf( mkAlert("other-cat", "kszy") );
+  s_.process(leaf2, changed_);
+  ensure_equals("correlation didn't failed", changed_.size(), 0u);
 }
 
 // test getting name of meta alert
 template<>
 template<>
-void testObj::test<5>(void)
+void testObj::test<4>(void)
 {
-  fail("TODO");
+  GraphNodePtrNN leaf1=makeNewLeaf( mkAlert("alice", "cat") );
+  s_.process(leaf1, changed_);
+  ensure_equals("something has been changed", changed_.size(), 0u);
+
+  GraphNodePtrNN leaf2=makeNewLeaf( mkAlert("cat", "kszy") );
+  s_.process(leaf2, changed_);
+  ensure_equals("correlation failed", changed_.size(), 1u);
+
+  ensure_equals("invalid name", changed_[0]->getMetaAlert()->getName().get(), string("[usersmonitor] actions of user 'cat'") );
 }
 
 } // namespace tut
