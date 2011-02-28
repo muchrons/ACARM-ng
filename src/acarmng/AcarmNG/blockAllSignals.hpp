3deaf7ed00a9de1217da272fb98de2e5e5cc9515
Bartosz Szurgot <bartosz.szurgot@pwr.wroc.pl>
2011-02-28 11:11:43 +0100

    added blocking signals; all application's elements are now in AcarmNG namespace.
    added:
      src/acarmng/AcarmNG/blockAllSignals.cpp
      src/acarmng/AcarmNG/blockAllSignals.hpp
    modified:
      src/acarmng/AcarmNG/main.cpp
      src/acarmng/AcarmNG/printBanner.cpp
      src/acarmng/AcarmNG/printBanner.hpp

---
+++ src/acarmng/AcarmNG/blockAllSignals.hpp
@@ -0,0 +1,17 @@
+/*
+ * blockAllSignals.cpp
+ *
+ */
+#ifndef INCLUDE_ACARMNG_BLOCKALLSIGNALS_HPP_FILE
+#define INCLUDE_ACARMNG_BLOCKALLSIGNALS_HPP_FILE
+
+namespace AcarmNG
+{
+
+/** \brief blocks all incomming signals permanently.
+ */
+void blockAllSignals(void);
+
+} // namespace AcarmNG
+
+#endif
