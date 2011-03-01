4ea4493eea827524abe6afb8a73a46db8058812e
Bartosz Szurgot <bartosz.szurgot@pwr.wroc.pl>
2011-03-01 12:46:26 +0100

    created header gathering all other differs.
    added:
      src/algo/Algo/Diff/all.hpp

---
+++ src/algo/Algo/Diff/all.hpp
@@ -0,0 +1,16 @@
+/*
+ * all.hpp
+ *
+ */
+#ifndef INCLUDE_ALGO_DIFF_ALL_HPP_FILE
+#define INCLUDE_ALGO_DIFF_ALL_HPP_FILE
+
+/* public header */
+
+#include "Algo/Diff/analyzer.hpp"
+#include "Algo/Diff/ip.hpp"
+#include "Algo/Diff/objectID.hpp"
+#include "Algo/Diff/string.hpp"
+#include "Algo/Diff/viaPointer.hpp"
+
+#endif
