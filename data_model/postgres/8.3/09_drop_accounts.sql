ab6e68ba12c56db65db830ccde6de9c35568de5a
Bartosz Szurgot <bartosz.szurgot@pwr.wroc.pl>
2011-04-12 17:23:31 +0200

    fixed bug with invalid parameters order.
    modified:
      data_model/postgres/8.3/09_drop_accounts.sql
      data_model/postgres/8.4/09_drop_accounts.sql
      data_model/postgres/init_db

--- data_model/postgres/8.3/09_drop_accounts.sql
+++ data_model/postgres/8.3/09_drop_accounts.sql
@@ -1,3 +1,4 @@
+-- TODO: move this to scripts - data base name is a paramter
 REVOKE CONNECT ON DATABASE "acarm_ng" FROM "acarm-ng-ui";
 REVOKE CONNECT ON DATABASE "acarm_ng" FROM "acarm-ng-daemon";
 
