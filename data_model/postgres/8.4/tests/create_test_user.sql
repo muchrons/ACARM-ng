10917c9969e5aefa1f2d7331778d1caf61c2e43e
Bartosz Szurgot <bartosz.szurgot@pwr.wroc.pl>
2011-04-13 17:27:01 +0200

    temporary checkin.
    modified:
      data_model/postgres/8.4/tests/create_test_user.sql
      data_model/postgres/init_db
      data_model/postgres/init_test_db

--- data_model/postgres/8.4/tests/create_test_user.sql
+++ data_model/postgres/8.4/tests/create_test_user.sql
@@ -1,5 +1,5 @@
 CREATE USER "acarm-ng-tests" PASSWORD 'test.password';
-GRANT ALL ON DATABASE acarm_ng_test TO "acarm-ng-tests";
+
 GRANT ALL ON TABLE
     config_rdonly,
     config,
@@ -30,3 +30,34 @@ GRANT ALL ON SEQUENCE
     alerts_id_seq,
     meta_alerts_id_seq
     TO "acarm-ng-tests";
+
+GRANT ALL ON TABLE
+    config_rdonly,
+    config,
+    wui_users,
+    logs,
+    procs,
+    services,
+    reference_urls,
+    hosts,
+    analyzers,
+    alerts,
+    alert_analyzers,
+    meta_alerts,
+    alert_to_meta_alert_map,
+    meta_alerts_tree,
+    meta_alerts_roots,
+    meta_alerts_in_use,
+    meta_alerts_already_triggered
+    TO _USER_;
+
+GRANT ALL ON SEQUENCE
+    wui_users_id_seq,
+    procs_id_seq,
+    services_id_seq,
+    reference_urls_id_seq,
+    hosts_id_seq,
+    analyzers_id_seq,
+    alerts_id_seq,
+    meta_alerts_id_seq
+    TO _USER_;
