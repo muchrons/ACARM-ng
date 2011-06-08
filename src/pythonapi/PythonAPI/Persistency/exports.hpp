bfea2db78da84d4b4ebdd700f77cb5da157f9ce4
Bartosz Szurgot <bartosz.szurgot@pwr.wroc.pl>
2011-06-08 16:21:12 +0200

    exports module added. created hello-world test script that could be run.
    added:
      src/pythonapi/PythonAPI/Persistency/exports.hpp
      src/pythonapi/PythonAPI/Persistency/exports.mt.cpp
      src/pythonapi/features/testdata/hello_world.py

---
+++ src/pythonapi/PythonAPI/Persistency/exports.hpp
@@ -0,0 +1,18 @@
+/*
+ * exports.hpp
+ *
+ */
+#ifndef INCLUDE_PYTHONAPI_PERSISTENCY_EXPORTS_HPP_FILE
+#define INCLUDE_PYTHONAPI_PERSISTENCY_EXPORTS_HPP_FILE
+
+#include <boost/python/module.hpp>
+
+//#include "PythonAPI/Persistency/.hpp"
+
+BOOST_PYTHON_MODULE(persistency)
+{
+  //initMyModMyClassPython();
+  //initMyModHelloGoodbye();
+}
+
+#endif
