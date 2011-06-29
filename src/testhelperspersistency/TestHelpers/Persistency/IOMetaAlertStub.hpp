9bb81b2d796772e3d5b2eb30ec9efca3f393c86d
Bartosz Szurgot <bartosz.szurgot@pwr.wroc.pl>
2011-06-29 09:32:01 +0200

    added stub for meta alert IO.
    added:
      src/testhelperspersistency/TestHelpers/Persistency/IOMetaAlertStub.hpp
      src/testhelperspersistency/TestHelpers/Persistency/IOMetaAlertStub.t.cpp

---
+++ src/testhelperspersistency/TestHelpers/Persistency/IOMetaAlertStub.hpp
@@ -0,0 +1,67 @@
+/*
+ * IOMetaAlertStub.hpp
+ *
+ */
+#ifndef INCLUDE_TESTHELPERS_PERSISTENCY_IOMETAALERTSTUB_HPP_FILE
+#define INCLUDE_TESTHELPERS_PERSISTENCY_IOMETAALERTSTUB_HPP_FILE
+
+#include "Persistency/IO/MetaAlert.hpp"
+
+namespace TestHelpers
+{
+namespace Persistency
+{
+
+/** \brief stub for operations on meta-alert
+ */
+class IOMetaAlertStub: public ::Persistency::IO::MetaAlert
+{
+public:
+  /** \brief meta alert persistency IO stub.
+   *  \param ma     meta alert to work on.
+   *  \param t      active transaction.
+   */
+  IOMetaAlertStub(::Persistency::MetaAlertPtrNN   ma,
+                  ::Persistency::IO::Transaction &t):
+    MetaAlert(ma, t)
+  {
+  }
+
+private:
+  virtual void saveImpl(::Persistency::IO::Transaction &/*t*/)
+  {
+  }
+
+  virtual void markAsTriggeredImpl(::Persistency::IO::Transaction &/*t*/, const std::string &/*name*/)
+  {
+  }
+
+  virtual void markAsUsedImpl(::Persistency::IO::Transaction &/*t*/)
+  {
+  }
+
+  virtual void markAsUnusedImpl(::Persistency::IO::Transaction &/*t*/)
+  {
+  }
+
+  virtual void updateSeverityDeltaImpl(::Persistency::IO::Transaction &/*t*/, double /*delta*/)
+  {
+  }
+
+  virtual void updateCertaintyDeltaImpl(::Persistency::IO::Transaction &/*t*/, double /*delta*/)
+  {
+  }
+
+  virtual void addChildImpl(::Persistency::IO::Transaction &/*t*/, ::Persistency::MetaAlertPtrNN /*child*/)
+  {
+  }
+
+  virtual void associateWithAlertImpl(::Persistency::IO::Transaction &/*t*/, ::Persistency::AlertPtrNN /*alert*/)
+  {
+  }
+}; // class IOMetaAlertStub
+
+} // namespace Persistency
+} // namespace TestHelpers
+
+#endif
