8fb1553e0f9b080d3af523ad8585d25bbfa4a022
Mariusz Uchroński <mariusz.uchronski@pwr.wroc.pl>
2011-06-30 12:32:17 +0200

    tests for ProcessedSet class updated.
    modified:
      src/filternewevent/Filter/NewEvent/ProcessedSet.t.cpp

--- src/filternewevent/Filter/NewEvent/ProcessedSet.t.cpp
+++ src/filternewevent/Filter/NewEvent/ProcessedSet.t.cpp
@@ -22,12 +22,12 @@ namespace
 
 struct TestClass: private TestStubs
 {
-/*
+
   TestClass(void):
     conn_( Persistency::IO::create() ),
     bf_(conn_, changed_, TypeName("testnewevent"), InstanceName("myname")),
     h_("entryname"),
-    ePtr_(new Entry(hPtr_, bf_, ts_))
+    ePtr_(new Entry(h_, bf_, ts_))
   {
   }
 
@@ -38,7 +38,7 @@ struct TestClass: private TestStubs
   ProcessedSet                     ps_;
   Hash                             h_;
   EntrySharedPtr                   ePtr_;
-*/
+
 };
 
 typedef tut::test_group<TestClass> factory;
@@ -51,7 +51,7 @@ factory tf("Filter/NewEvent/ProcessedSet");
 
 namespace tut
 {
-/*
+
 // check if non-existing entry is reported as such
 template<>
 template<>
@@ -65,26 +65,26 @@ template<>
 template<>
 void testObj::test<2>(void)
 {
-  HashSharedPtr h1(new Hash("some entry"));
-  Entry         e1(h1, bf_, ts_);
-  HashSharedPtr h2(new Hash("some other entry"));
-  Entry         e2(h2, bf_, ts_);
+  Hash           h1("some entry");
+  EntrySharedPtr e1(new Entry(h1, bf_, ts_));
+  Hash           h2("some other entry");
+  EntrySharedPtr e2(new Entry(h2, bf_, ts_));
   ps_.markAsProcessed(e1, 1);
   ps_.markAsProcessed(e2, 2);
   ps_.prune();
-  ensure("element with timeout 1 s has been pruned before timeout", ps_.update(*h1.get(), 1) );
-  ensure("element with timeout 2 s has been pruned before timeout", ps_.update(*h2.get(), 2) );
+  ensure("element with timeout 1 s has been pruned before timeout", ps_.update(h1, 1) );
+  ensure("element with timeout 2 s has been pruned before timeout", ps_.update(h2, 2) );
   sleep(2);
   ps_.prune();
-  ensure("element with timeout 1 s has not been pruned after 2 seconds", ps_.update(*h1.get(), 1)==false );
-  ensure("element with timeout 2 s has been pruned after 2 seconds", ps_.update(*h2.get(), 2) );
+  ensure("element with timeout 1 s has not been pruned after 2 seconds", ps_.update(h1, 1)==false );
+  ensure("element with timeout 2 s has been pruned after 2 seconds", ps_.update(h2, 2) );
   sleep(1);
   ps_.prune();
-  ensure("element with timeout 2 s has been pruned after timeout update", ps_.update(*h2.get(), 2) );
+  ensure("element with timeout 2 s has been pruned after timeout update", ps_.update(h2, 2) );
   sleep(3);
   ps_.prune();
-  ensure("element with timeout 1 s has not been pruned", ps_.update(*h1.get(), 1)==false );
-  ensure("element with timeout 2 s has not been pruned", ps_.update(*h2.get(), 2)==false );
+  ensure("element with timeout 1 s has not been pruned", ps_.update(h1, 1)==false );
+  ensure("element with timeout 2 s has not been pruned", ps_.update(h2, 2)==false );
 }
 
 
@@ -93,14 +93,14 @@ template<>
 template<>
 void testObj::test<3>(void)
 {
-  HashSharedPtr h1(new Hash("some entry"));
-  Entry         e1(h1, bf_, ts_);
-  HashSharedPtr h2(new Hash("some other entry"));
-  Entry         e2(h2, bf_, ts_);
+  Hash            h1("some entry");
+  EntrySharedPtr  e1(new Entry(h1, bf_, ts_));
+  Hash            h2("some other entry");
+  EntrySharedPtr  e2(new Entry(h2, bf_, ts_));
   ps_.markAsProcessed(e1, 1);
   ps_.markAsProcessed(e2, 2);
-  ensure("element 1 not present", ps_.update(*h1.get(), 1) );
-  ensure("element 2 not present", ps_.update(*h2.get(), 2) );
+  ensure("element 1 not present", ps_.update(h1, 1) );
+  ensure("element 2 not present", ps_.update(h2, 2) );
 }
 
 // test if after prune proper element is present in timeouted set
@@ -108,14 +108,12 @@ template<>
 template<>
 void testObj::test<4>(void)
 {
-  HashSharedPtr hash(new Hash("some name"));
-  //{
-    Entry e(hash, bf_, ts_);
+  Hash hash("some name");
+  {
+    EntrySharedPtr e(new Entry(hash, bf_, ts_));
     ps_.markAsProcessed(e, 1);
-  //}
-    std::cout << "sleep\n";
+  }
   sleep(2);
-  //TODO: this test fail, implementation shouldanged to fix this issue
   ensure("element timeouted before prune", ts_.isTimeouted(hash) == false );
   ps_.prune();
   ensure("element not-timeouted after prune", ts_.isTimeouted(hash));
@@ -126,18 +124,18 @@ template<>
 template<>
 void testObj::test<5>(void)
 {
-  HashSharedPtr hash(new Hash("some name"));
-  Entry e(hash, bf_, ts_);
-  ensure("element present in processed set", ps_.update(*hash.get(), 1) == false);
+  Hash hash("some name");
+  EntrySharedPtr e(new Entry(hash, bf_, ts_));
+  ensure("element present in processed set", ps_.update(hash, 1) == false);
   // add element to the processed set
   ps_.markAsProcessed(e, 1);
   // update added element with timeout 3[s]
-  ps_.update(*hash.get(), 3);
+  ps_.update(hash, 3);
   // wait two seconds
   sleep(2);
   ps_.prune();
   // check if element is present after prune
-  ensure("element is not timeouted", ps_.update(*hash.get(), 1));
+  ensure("element is not timeouted", ps_.update(hash, 1));
 }
-*/
+
 } // namespace tut
