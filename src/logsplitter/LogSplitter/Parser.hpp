c96d57953c98d7bb9ce66aa82762d3448eb93d21
Bartosz Szurgot <bartosz.szurgot@pwr.wroc.pl>
2011-07-04 16:54:35 +0200

    parser added and tested.
    added:
      src/logsplitter/LogSplitter/Parser.cpp
      src/logsplitter/LogSplitter/Parser.hpp
      src/logsplitter/LogSplitter/Parser.t.cpp

---
+++ src/logsplitter/LogSplitter/Parser.hpp
@@ -0,0 +1,31 @@
+/*
+ * Parser.hpp
+ *
+ */
+#ifndef INCLUDE_LOGSPLITTER_PARSER_HPP_FILE
+#define INCLUDE_LOGSPLITTER_PARSER_HPP_FILE
+
+#include <string>
+#include <stdexcept>
+
+#include <boost/noncopyable.hpp>
+
+namespace LogSplitter
+{
+
+/** \brief parser looking for " [<digits>] " sequence.
+ */
+class Parser: private boost::noncopyable
+{
+public:
+  explicit Parser(const std::string &str);
+
+  unsigned int get(void) const;
+
+private:
+  const unsigned int num_;
+}; // class Parser
+
+} // namespace LogSplitter
+
+#endif
