fb8c5f13aa1ec1a2b5053e6b2db4daab788d3aab
Bartosz Szurgot <bartosz.szurgot@pwr.wroc.pl>
2011-07-06 09:40:16 +0200

    formatting updated.
    modified:
      src/pythonapi/PythonAPI/ExceptionFromScript.cpp

--- src/pythonapi/PythonAPI/ExceptionFromScript.cpp
+++ src/pythonapi/PythonAPI/ExceptionFromScript.cpp
@@ -11,7 +11,7 @@ ExceptionFromScript::ExceptionFromScript(const Location    &where,
                                          const std::string &type,
                                          const std::string &msg,
                                          const std::string &backtrace):
-  Exception(where, cc("exception type '", type, "' risen: ", msg, "; backtrace is: ", backtrace) )
+  Exception(where, cc("exception type \"", type, "\" risen: ", msg, "; backtrace is: ", backtrace) )
 {
 }
 
