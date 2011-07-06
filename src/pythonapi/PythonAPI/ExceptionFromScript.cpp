748f8e0b2bb2122d291b65402ca47598530e6746
Bartosz Szurgot <bartosz.szurgot@pwr.wroc.pl>
2011-07-06 08:47:53 +0200

    created exception class when thrown from user script.
    added:
      src/pythonapi/PythonAPI/ExceptionFromScript.cpp
      src/pythonapi/PythonAPI/ExceptionFromScript.hpp

---
+++ src/pythonapi/PythonAPI/ExceptionFromScript.cpp
@@ -0,0 +1,18 @@
+/*
+ * ExceptionFromScript.cpp
+ *
+ */
+#include "PythonAPI/ExceptionFromScript.hpp"
+
+namespace PythonAPI
+{
+
+ExceptionFromScript::ExceptionFromScript(const Location    &where,
+                                         const std::string &type,
+                                         const std::string &msg,
+                                         const std::string &backtrace):
+  Exception(where, cc("exception type '", type, "' risen: ", msg, "; backtrace is: ", backtrace) )
+{
+}
+
+} // namespace PythonAPI
