06d6b4ce227757c64f56f95904c9e35a672426e1
Bartosz Szurgot <bartosz.szurgot@pwr.wroc.pl>
2011-07-19 10:02:19 +0200

    limitd null string exporting has been tested. added it to build.
    added:
      src/pythonapi/PythonAPI/Persistency/exportLimitedNULLString.hpp
      src/pythonapi/PythonAPI/Persistency/exportLimitedNULLString.t.cpp
      src/pythonapi/PythonAPI/Persistency/exportLimitedString.hpp
      src/pythonapi/PythonAPI/Persistency/exportLimitedString.t.cpp
    modified:
      src/pythonapi/PythonAPI/Persistency/detail/exportGenericLimitedString.hpp
      src/pythonapi/features/modes/default

--- src/pythonapi/PythonAPI/Persistency/detail/exportGenericLimitedString.hpp
+++ src/pythonapi/PythonAPI/Persistency/detail/exportGenericLimitedString.hpp
@@ -6,6 +6,7 @@
 #define INCLUDE_PYTHONAPI_PERSISTENCY_EXPORTGENERICLIMITEDSTRING_HPP_FILE
 
 #include <string>
+#include <cassert>
 
 #include "PythonAPI/Python.hpp"
 
@@ -17,10 +18,13 @@ namespace detail
 {
 
 /** \brief exports generic string class.
+ *  \param name name of the class, as it should be visible from python script;
+ *         must be a non-NULL, compile-time string.
  */
 template<typename TLimStr>
 void exportGenericLimitedString(const char *name)
 {
+  assert(name!=NULL && "name must be compile-time, non-NULL string");
   using namespace boost::python;
   class_<TLimStr>(name, init<>())
     .def(init<const char*>())
---
+++ src/pythonapi/PythonAPI/Persistency/exportLimitedNULLString.hpp
@@ -0,0 +1,30 @@
+/*
+ * exportLimitedNULLString.cpp
+ *
+ */
+#ifndef INCLUDE_PYTHONAPI_PERSISTENCY_EXPORTLIMITEDNULLSTRING_HPP_FILE
+#define INCLUDE_PYTHONAPI_PERSISTENCY_EXPORTLIMITEDNULLSTRING_HPP_FILE
+
+#include "Commons/LimitedNULLString.hpp"
+#include "PythonAPI/Persistency/detail/exportGenericLimitedString.hpp"
+
+
+namespace PythonAPI
+{
+namespace Persistency
+{
+
+/** \brief exports LimitedNULLString class.
+ */
+template<uint16_t N>
+void exportLimitedNULLString(const char *name)
+{
+  using namespace boost::python;
+  using namespace Commons;
+  detail::exportGenericLimitedString< Commons::LimitedNULLString<N> >(name);
+} // exportLimitedNULLString()
+
+} // namespace Persistency
+} // namespace PythonAPI
+
+#endif
