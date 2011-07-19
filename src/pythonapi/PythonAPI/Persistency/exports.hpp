dbd996eb287715503d285e0e2670df8f0b2b4ca6
Bartosz Szurgot <bartosz.szurgot@pwr.wroc.pl>
2011-07-19 11:17:09 +0200

    Severity class has been exported.
    added:
      src/pythonapi/PythonAPI/Persistency/exportSeverity.cpp
      src/pythonapi/PythonAPI/Persistency/exportSeverity.hpp
      src/pythonapi/PythonAPI/Persistency/exportSeverity.t.cpp
    modified:
      src/pythonapi/PythonAPI/Persistency/exports.hpp

--- src/pythonapi/PythonAPI/Persistency/exports.hpp
+++ src/pythonapi/PythonAPI/Persistency/exports.hpp
@@ -11,6 +11,7 @@
 #include "PythonAPI/Persistency/exportMD5Sum.hpp"
 #include "PythonAPI/Persistency/exportPortNumber.hpp"
 #include "PythonAPI/Persistency/exportSeverityLevel.hpp"
+#include "PythonAPI/Persistency/exportSeverity.hpp"
 #include "PythonAPI/Persistency/exportReferenceURL.hpp"
 
 BOOST_PYTHON_MODULE(persistency)
@@ -20,6 +21,7 @@ BOOST_PYTHON_MODULE(persistency)
   PythonAPI::Persistency::exportMD5Sum();
   PythonAPI::Persistency::exportPortNumber();
   PythonAPI::Persistency::exportSeverityLevel();
+  PythonAPI::Persistency::exportSeverity();
   PythonAPI::Persistency::exportReferenceURL();
 }
 
