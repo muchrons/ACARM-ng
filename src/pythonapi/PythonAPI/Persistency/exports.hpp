f00c3b28ebbbd855cade8a7a4c1f92453468eb8d
Bartosz Szurgot <bartosz.szurgot@pwr.wroc.pl>
2011-07-19 09:12:51 +0200

    SeverityLevel has been exported.
    added:
      src/pythonapi/PythonAPI/Persistency/exportSeverityLevel.cpp
      src/pythonapi/PythonAPI/Persistency/exportSeverityLevel.hpp
      src/pythonapi/PythonAPI/Persistency/exportSeverityLevel.t.cpp
    modified:
      src/pythonapi/PythonAPI/Persistency/exports.hpp

--- src/pythonapi/PythonAPI/Persistency/exports.hpp
+++ src/pythonapi/PythonAPI/Persistency/exports.hpp
@@ -10,6 +10,7 @@
 #include "PythonAPI/Persistency/exportTimestamp.hpp"
 #include "PythonAPI/Persistency/exportMD5Sum.hpp"
 #include "PythonAPI/Persistency/exportPortNumber.hpp"
+#include "PythonAPI/Persistency/exportSeverityLevel.hpp"
 
 BOOST_PYTHON_MODULE(persistency)
 {
@@ -17,6 +18,7 @@ BOOST_PYTHON_MODULE(persistency)
   PythonAPI::Persistency::exportTimestamp();
   PythonAPI::Persistency::exportMD5Sum();
   PythonAPI::Persistency::exportPortNumber();
+  PythonAPI::Persistency::exportSeverityLevel();
 }
 
 #endif
