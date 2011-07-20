1fc2e7127d555c0178b1f0151dc6aedc7933d716
Bartosz Szurgot <bartosz.szurgot@pwr.wroc.pl>
2011-07-20 17:43:41 +0200

    test for HandleIndirection havebeen implemted.
    modified:
      src/persistency/Persistency/Facades/StrAccess/HandleIndirection.t.cpp

--- src/persistency/Persistency/Facades/StrAccess/HandleIndirection.t.cpp
+++ src/persistency/Persistency/Facades/StrAccess/HandleIndirection.t.cpp
@@ -13,12 +13,14 @@ using namespace Persistency::Facades::StrAccess;
 
 namespace
 {
+unsigned int g_MyTestProcessFuncObj_runCnt;
 
 struct MyTestProcessFuncObj
 {
   template<typename T, typename TParams>
   static bool process(const T &e, TParams &p)
   {
+    ++g_MyTestProcessFuncObj_runCnt;
     BOOST_STATIC_ASSERT( (boost::is_same<T, int>::value) );
     tut::ensure_equals("invalid path", p.path().get(), "a.b");
     tut::ensure_equals("invalid value", e, 42);
@@ -29,10 +31,13 @@ struct MyTestProcessFuncObj
 struct TestClass
 {
   TestClass(void):
+    v_(42),
     p_(Path("a.b"), cb_)
   {
+    g_MyTestProcessFuncObj_runCnt=0;
   }
 
+  const int                  v_;
   TestParams::ResultCallback cb_;
   TestParams                 p_;
 };
@@ -52,8 +57,8 @@ template<>
 template<>
 void testObj::test<1>(void)
 {
-  int v=42;
-  HandleIndirection<MyTestProcessFuncObj>::process(v, p_);
+  HandleIndirection<MyTestProcessFuncObj>::process(v_, p_);
+  ensure_equals("invalid run count", g_MyTestProcessFuncObj_runCnt, 1);
 }
 
 // test pointer
@@ -61,6 +66,9 @@ template<>
 template<>
 void testObj::test<2>(void)
 {
+  const int *ptr=&v_;
+  HandleIndirection<MyTestProcessFuncObj>::process(ptr, p_);
+  ensure_equals("invalid run count", g_MyTestProcessFuncObj_runCnt, 1);
 }
 
 // test smart pointer
@@ -68,6 +76,9 @@ template<>
 template<>
 void testObj::test<3>(void)
 {
+  boost::shared_ptr<int> ptr(new int(v_));
+  HandleIndirection<MyTestProcessFuncObj>::process(ptr, p_);
+  ensure_equals("invalid run count", g_MyTestProcessFuncObj_runCnt, 1);
 }
 
 // test pointer to pointer
@@ -75,6 +86,10 @@ template<>
 template<>
 void testObj::test<4>(void)
 {
+  const int  *ptr =&v_;
+  const int **ptr2=&ptr;
+  HandleIndirection<MyTestProcessFuncObj>::process(ptr2, p_);
+  ensure_equals("invalid run count", g_MyTestProcessFuncObj_runCnt, 1);
 }
 
 // test pointer to smart pointer to pointer
@@ -82,6 +97,32 @@ template<>
 template<>
 void testObj::test<5>(void)
 {
+  boost::shared_ptr<const int*>  ptr(new const int*(&v_));
+  boost::shared_ptr<const int*> *ptr2=&ptr;
+  HandleIndirection<MyTestProcessFuncObj>::process(ptr2, p_);
+  ensure_equals("invalid run count", g_MyTestProcessFuncObj_runCnt, 1);
+}
+
+// test NULL pointer
+template<>
+template<>
+void testObj::test<6>(void)
+{
+  const int *ptr=NULL;
+  HandleIndirection<MyTestProcessFuncObj>::process(ptr, p_);
+  ensure_equals("invalid run count", g_MyTestProcessFuncObj_runCnt, 0);
+  ensure_equals("null callback not called", cb_.lastNullFound_, "a");
+}
+
+// test NULL in smart pointer
+template<>
+template<>
+void testObj::test<7>(void)
+{
+  boost::shared_ptr<int>  ptr;
+  HandleIndirection<MyTestProcessFuncObj>::process(ptr, p_);
+  ensure_equals("invalid run count", g_MyTestProcessFuncObj_runCnt, 0);
+  ensure_equals("null callback not called", cb_.lastNullFound_, "a");
 }
 
 } // namespace tut
