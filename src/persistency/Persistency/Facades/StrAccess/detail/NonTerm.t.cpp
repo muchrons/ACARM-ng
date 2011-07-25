4821025b62305b93b7bf64251af16446fa91dee0
Bartosz Szurgot <bartosz.szurgot@pwr.wroc.pl>
2011-07-25 17:00:26 +0200

    work in progress.
    modified:
      src/persistency/Persistency/Facades/StrAccess/detail/NonTerm.t.cpp

--- src/persistency/Persistency/Facades/StrAccess/detail/NonTerm.t.cpp
+++ src/persistency/Persistency/Facades/StrAccess/detail/NonTerm.t.cpp
@@ -17,19 +17,29 @@ using namespace Persistency::Facades::StrAccess::detail;
 namespace
 {
 
+struct TestString
+{
+  TestString(const std::string &str):
+    str_(str)
+  {
+  }
+
+  std::string str_;
+}; // struct TestString
+
 struct OnStringTest
 {
   template<typename T, typename TParams>
   static bool process(const T &e, TParams &p)
   {
-    tut::ensure_equals("invalid parameter", e, string("narf"));
-    tut::ensure("element is end (?!)", p.isEnd()==false);
-    return true;
+    tut::ensure("terminal element found too soon", p.hasNext());
+    typedef typename TParams::template handle<string>::type Handle;
+    return Handle::process(e.str_, ++p);
   }
 }; // struct OnStringTest
 
 // add std::string support
-typedef boost::mpl::insert<DefaultHandleMap, boost::mpl::pair<string, OnStringTest>::type >::type StringHandleMap;
+typedef boost::mpl::insert<DefaultHandleMap, boost::mpl::pair<TestString, OnStringTest>::type >::type StringHandleMap;
 // add this to paramter
 typedef Params<StringHandleMap,TestParams::ResultCallback> LocalTestParams;
 
@@ -38,13 +48,15 @@ struct TestClass
 {
   TestClass(void):
     p_(Path("a.b"), cb_),
-    e_("narf")
+    str_("narf"),
+    e_(str_)
   {
   }
 
   LocalTestParams::ResultCallback cb_;
   LocalTestParams                 p_;
-  const string                    e_;
+  const string                    str_;
+  const TestString                e_;
 };
 
 typedef tut::test_group<TestClass> factory;
@@ -67,7 +79,7 @@ void testObj::test<1>(void)
   assert(p_.isEnd()==false);
   try
   {
-    NonTerm::process(e_, p_);
+    NonTerm::process(str_, p_);
     fail("call didn't throw for last element");
   }
   catch(const ExceptionInvalidPath &)
@@ -82,6 +94,7 @@ template<>
 void testObj::test<2>(void)
 {
   NonTerm::process(&e_, p_);
+  ensure_equals("invalid response", cb_.lastValue_, str_);
 }
 
 // test collection
@@ -89,12 +102,13 @@ template<>
 template<>
 void testObj::test<3>(void)
 {
-  vector<string> vec;
-  vec.push_back("first");
+  vector<TestString> vec;
+  vec.push_back( TestString("first") );
   vec.push_back(e_);
-  vec.push_back("third");
+  vec.push_back( TestString("third") );
   LocalTestParams p(Path("array.1.sth"), cb_);
   NonTerm::process(vec, p);
+  ensure_equals("invalid response", cb_.lastValue_, str_);
 }
 
 // test collection indirect call
@@ -102,20 +116,22 @@ template<>
 template<>
 void testObj::test<4>(void)
 {
-  vector<string> vec;
-  vec.push_back("first");
+  vector<TestString> vec;
+  vec.push_back( TestString("first") );
   vec.push_back(e_);
-  vec.push_back("third");
+  vec.push_back( TestString("third") );
   LocalTestParams p(Path("array.1.sth"), cb_);
   NonTerm::process(&vec, p);
+  ensure_equals("invalid response", cb_.lastValue_, str_);
 }
 
-// test if reuired code is run
+// test if required code is run
 template<>
 template<>
 void testObj::test<5>(void)
 {
   NonTerm::process(e_, p_);
+  ensure_equals("invalid response", cb_.lastValue_, str_);
 }
 
 } // namespace tut
