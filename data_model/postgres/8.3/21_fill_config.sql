c525e9e3e6f020fecf9efbc7b82efe271987b06c
Bartosz Szurgot <bartosz.szurgot@pwr.wroc.pl>
2011-08-31 13:35:43 +0200

    release procedure.
    removed:
      TODO
      doc/devel/
      doc/devel/build_system.txt
      doc/devel/coding_style.txt
      doc/devel/creating_filters.txt
      doc/devel/creating_inputs.txt
      doc/devel/creating_triggers.txt
      doc/devel/main_components.txt
      doc/terminology.txt
      misc/bzr_plugins/
      misc/bzr_plugins/bzr_buildbot.py
      src/TODO
      wui/TODO
    modified:
      bzr_team.conf
      data_model/postgres/8.3/21_fill_config.sql
      data_model/postgres/8.4/21_fill_config.sql
      doc/RELEASE_NOTES.txt
      misc/bzr_build_all.conf
      misc/bzr_build_some.conf
      src/acarmng/AcarmNG/printBanner.cpp

--- data_model/postgres/8.3/21_fill_config.sql
+++ data_model/postgres/8.3/21_fill_config.sql
@@ -3,7 +3,7 @@
 --
 
 INSERT INTO config_rdonly VALUES
-  ('acarm-ng', 'version',     'v0.2.1'),
+  ('acarm-ng', 'version',     'v0.3.0'),
   ('acarm-ng', 'db-version',  '5'),
   ('acarm-ng', 'create_time', now() )
 ;
