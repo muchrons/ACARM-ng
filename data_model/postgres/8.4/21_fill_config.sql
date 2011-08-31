3db3c1de4ec1d1eb445590e51000c14605e753d0
Bartosz Szurgot <bartosz.szurgot@pwr.wroc.pl>
2011-08-31 13:29:00 +0200

    system version is no longer in the data basem since it does not depend on it.
    modified:
      data_model/postgres/8.3/21_fill_config.sql
      data_model/postgres/8.4/21_fill_config.sql

--- data_model/postgres/8.4/21_fill_config.sql
+++ data_model/postgres/8.4/21_fill_config.sql
@@ -3,8 +3,6 @@
 --
 
 INSERT INTO config_rdonly VALUES
-  ('acarm-ng', 'version',     'v0.2.1'),
--- acarm version to be removed
   ('acarm-ng', 'db-version',  '5'),
   ('acarm-ng', 'create_time', now() )
 ;
