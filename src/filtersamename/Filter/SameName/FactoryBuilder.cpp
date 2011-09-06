b716fc52e50e4b6ebbc79b384b682aa5813e2d6f
Bartosz Szurgot <bartosz.szurgot@pwr.wroc.pl>
2011-09-06 15:24:42 +0200

    minor style fixes.
    modified:
      src/coretypes/Core/Types/Proc/InterfaceImpl.hpp
      src/filtersamename/Filter/SameName/FactoryBuilder.cpp

--- src/filtersamename/Filter/SameName/FactoryBuilder.cpp
+++ src/filtersamename/Filter/SameName/FactoryBuilder.cpp
@@ -43,7 +43,7 @@ FactoryBuilder::FactoryPtr FactoryBuilder::buildImpl(const Options &options) con
 
   const FilterConfig fc(type_.str(), options);
   // filter samename name
-  const std::string    &name=fc["name"];
+  const std::string &name=fc["name"];
   LOGMSG_INFO_S(log_)<<"setting filter \""<<getTypeName()<<"\" name to \""<<name<<"\"";
   const unsigned int timeout=Commons::Convert::to<unsigned int>(fc["timeout"]);
   LOGMSG_INFO_S(log_)<<"setting timeout to "<<timeout<<"[s]";
