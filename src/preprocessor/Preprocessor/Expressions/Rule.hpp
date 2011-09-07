67f3d96f1a95eac83b18f0c852ab3a1bd74bed71
Bartosz Szurgot <bartosz.szurgot@pwr.wroc.pl>
2011-09-07 17:21:14 +0200

    fixed header - dead code and fixed TODO removed.
    modified:
      src/preprocessor/Preprocessor/Expressions/Rule.hpp

--- src/preprocessor/Preprocessor/Expressions/Rule.hpp
+++ src/preprocessor/Preprocessor/Expressions/Rule.hpp
@@ -14,7 +14,6 @@
 #include "Preprocessor/Expressions/Base.hpp"
 #include "Preprocessor/ExceptionInvalidPath.hpp"
 
-// TODO: to be reimplemented with Persistency::Facades::StrAccess
 
 namespace Preprocessor
 {
@@ -47,42 +46,6 @@ public:
   virtual bool compute(const Persistency::Alert &alert) const;
 
 private:
-#if 0
-  typedef Path::const_iterator PathCit;
-
-  template<typename T>
-  bool processCollection(PathCit t, const T &col) const;
-
-  template<typename Tit>
-  bool matchEach(PathCit t, const Tit begin, const Tit end) const;
-
-  template<typename Tit>
-  bool matchAny(PathCit t, const Tit begin, const Tit end) const;
-
-  template<typename T>
-  bool check(const T *e, PathCit t) const;
-  template<typename T>
-  bool check(const boost::shared_ptr<T> e, PathCit t) const;
-
-  bool check(const Persistency::Alert            &e, PathCit t) const;
-  bool check(const Persistency::Alert::Analyzers &e, PathCit t) const;
-  bool check(const Persistency::Analyzer         &e, PathCit t) const;
-  bool check(const Persistency::Alert::Hosts     &e, PathCit t) const;
-  bool check(const Persistency::ReferenceURL     &e, PathCit t) const;
-  bool check(const Persistency::Host             &e, PathCit t) const;
-  bool check(const Persistency::Host::Services   &e, PathCit t) const;
-  bool check(const Persistency::Service          &e, PathCit t) const;
-  bool check(const Persistency::Host::Processes  &e, PathCit t) const;
-  bool check(const Persistency::Process          &e, PathCit t) const;
-  bool check(const Persistency::MD5Sum           &e, PathCit t) const;
-  // final point
-  bool check(const std::string                   &e, PathCit t) const;
-
-  void throwIfEnd(const Exception::Location &where, PathCit t) const;
-  void throwIfNotEnd(const Exception::Location &where, PathCit t) const;
-  void throwInvalid(const Exception::Location &where, PathCit t) const;
-#endif
-
   const Path                        path_;
   boost::scoped_ptr<Checkers::Mode> checker_;
 }; // class Rule
