13a571a66d966d8da767b53f70978fe6821e2f52
Bartosz Szurgot <bartosz.szurgot@pwr.wroc.pl>
2011-09-26 15:01:01 +0200

    fixed invalid elements in persistency tests.
    modified:
      src/persistencypostgres/Persistency/IO/Postgres/TestDBAccess.t.cpp
      src/persistencypostgres/features/testdata/test_data_001.sql

--- src/persistencypostgres/features/testdata/test_data_001.sql
+++ src/persistencypostgres/features/testdata/test_data_001.sql
@@ -19,8 +19,7 @@ DELETE FROM reference_urls;
 DELETE FROM alerts;
 DELETE FROM config;
 DELETE FROM config_rdonly;
-DELETE FROM logs;
-DELETE FROM wui_users;
+DELETE FROM pradolog;
 DELETE FROM heartbeats;
 
 
