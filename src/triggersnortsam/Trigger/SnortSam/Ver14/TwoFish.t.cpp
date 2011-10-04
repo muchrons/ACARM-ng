73efbe157f66ea5206674076d9b8aed0391f5f65
Bartosz Szurgot <bartosz.szurgot@pwr.wroc.pl>
2011-10-04 09:21:59 +0200

    added one more stress test.
    modified:
      src/triggersnortsam/Trigger/SnortSam/Ver14/TwoFish.t.cpp

--- src/triggersnortsam/Trigger/SnortSam/Ver14/TwoFish.t.cpp
+++ src/triggersnortsam/Trigger/SnortSam/Ver14/TwoFish.t.cpp
@@ -3,6 +3,7 @@
  *
  */
 #include <tut.h>
+#include <utility>
 
 #include "Trigger/SnortSam/Ver14/TwoFish.hpp"
 
@@ -14,11 +15,13 @@ namespace
 struct TestClass
 {
   TestClass(void):
-    tf_("abc")
+    e_("abc"),
+    d_("abc")
   {
   }
 
-  TwoFish tf_;
+  TwoFish e_;
+  TwoFish d_;
 };
 
 typedef tut::test_group<TestClass> factory;
@@ -37,7 +40,7 @@ template<>
 void testObj::test<1>(void)
 {
   const uint8_t    buf[]={1,2,3,4,5,6};
-  TwoFish::DataRef out  =tf_.encrypt(buf, sizeof(buf));
+  TwoFish::DataRef out  =e_.encrypt(buf, sizeof(buf));
   ensure("output buffer is too small", out.len_>=sizeof(buf));
 }
 
@@ -48,11 +51,10 @@ void testObj::test<2>(void)
 {
   const uint8_t    buf[]={1,2,3,4,5,6};
   // encryption
-  TwoFish::DataRef enc=tf_.encrypt(buf, sizeof(buf));
+  TwoFish::DataRef enc=e_.encrypt(buf, sizeof(buf));
   ensure("output buffer is too small", enc.len_>=sizeof(buf));
   // decryption
-  TwoFish          tmp("abc");
-  TwoFish::DataRef dec=tmp.decrypt(enc.data_, enc.len_);
+  TwoFish::DataRef dec=d_.decrypt(enc.data_, enc.len_);
   // check
   ensure_equals("invalid data length", dec.len_, sizeof(buf));
   ensure("got NULL pointer", dec.data_!=NULL);
@@ -68,12 +70,40 @@ void testObj::test<3>(void)
   const uint8_t    buf[]={1,2,3,4,5,6};
   try
   {
-    tf_.decrypt(buf, sizeof(buf));
+    d_.decrypt(buf, sizeof(buf));
     fail("decrypt() didn't throw on invalid data");
   }
   catch(const Trigger::SnortSam::ExceptionCryptoFailed&)
   {
     // this is expected
+  }
+}
+
+// try enc/dec few times
+template<>
+template<>
+void testObj::test<4>(void)
+{
+  uint8_t buf[]={1,2,3,4,5,6};
+  TwoFish *a=&e_;
+  TwoFish *b=&d_;
+  for(int i=0; i<5; ++i)
+  {
+    // make some random changes
+    buf[4]=42-i;
+    buf[2]=42+i;
+    // encryption
+    TwoFish::DataRef enc=e_.encrypt(buf, sizeof(buf));
+    ensure("output buffer is too small", enc.len_>=sizeof(buf));
+    // decryption
+    TwoFish::DataRef dec=d_.decrypt(enc.data_, enc.len_);
+    // check
+    ensure_equals("invalid data length", dec.len_, sizeof(buf));
+    ensure("got NULL pointer", dec.data_!=NULL);
+    for(size_t i=0; i<dec.len_; ++i)
+      ensure_equals("invalid byte decoded", dec.data_[i], buf[i]);
+    // use encryption part for decryption and vice versa
+    std::swap(a, b);
   }
 }
 
