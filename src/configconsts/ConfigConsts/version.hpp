0284eabcd8914a763a00b3e3a2e98e2002bd2bec
Bartosz Szurgot <bartosz.szurgot@pwr.wroc.pl>
2011-11-09 17:22:29 +0100

    added configconsts component along with first element - version strings.
    added:
      src/configconsts/
      src/configconsts/ConfigConsts/
      src/configconsts/ConfigConsts/main.t.cpp@
      src/configconsts/ConfigConsts/version.cpp
      src/configconsts/ConfigConsts/version.hpp
      src/configconsts/ConfigConsts/version.t.cpp
      src/configconsts/Makefile@
      src/configconsts/features/
      src/configconsts/features/deps/
      src/configconsts/features/modes/
      src/configconsts/features/modes/default
      src/configconsts/features/testdata/
    modified:
      src/build_config/components.lst
      src/configio/features/deps/common

---
+++ src/configconsts/ConfigConsts/version.hpp
@@ -0,0 +1,25 @@
+/*
+ * version.hpp
+ *
+ */
+#ifndef INCLUDE_CONFIGCONSTS_VERSION_HPP_FILE
+#define INCLUDE_CONFIGCONSTS_VERSION_HPP_FILE
+
+/* public header */
+
+namespace ConfigConsts
+{
+
+/** \brief compile-time knonw software version string. */
+extern const char * const versionString;
+
+/** \brief main version number vX.y.z. */
+extern const unsigned int versionMain;
+/** \brief smller version number vx.Y.z. */
+extern const unsigned int versionMajor;
+/** \brief bugfix version number vx.y.Z. */
+extern const unsigned int versionMinor;
+
+} // namespace ConfigCOnsts
+
+#endif
