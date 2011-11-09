9f05ecd85c9169d5b4ed3048e88410584b21654a
Bartosz Szurgot <bartosz.szurgot@pwr.wroc.pl>
2011-11-09 08:49:40 +0100

    more tests added, for single-file xml config.
    added:
      src/configio/features/testdata/cfg_file_expand/non-xml-file.xml
    modified:
      src/configio/ConfigIO/ConfigFileReader.t.cpp

--- src/configio/ConfigIO/ConfigFileReader.t.cpp
+++ src/configio/ConfigIO/ConfigFileReader.t.cpp
@@ -52,7 +52,23 @@ template<>
 void testObj::test<2>(void)
 {
   const ConfigFileReader cfr("testdata/cfg_file_expand/basic_file.xml");
-  // TODO
+  const XML::Node r=cfr.getTree().getRoot();
+  ensure_equals("invalid root's name", r.getName(), "some_root");
+
+  const XML::Node::TNodesList &children1=r.getChildrenList();
+  ensure_equals("invalid root's children count", children1.size(), 1);
+
+  const XML::Node &child11=(*children1.begin());
+  ensure_equals("invalid name of child 1.1", child11.getName(), "some_node");
+  const XML::Node::TNodesList &children2=child11.getChildrenList();
+
+  ensure_equals("invalid child's name", children2.size(), 2);
+  XML::Node::TNodesList::const_iterator it=children2.begin();
+  ensure_equals("invalid child 2.1 name", it->getName(), "key");
+  ensure_equals("invalid child 2.1 value", it->getValuesString(), "question");
+  ++it;
+  ensure_equals("invalid child 2.2 name", it->getName(), "value");
+  ensure_equals("invalid child 2.2 value", it->getValuesString(), "42");
 }
 
 // open non-xml file
@@ -60,7 +76,8 @@ template<>
 template<>
 void testObj::test<3>(void)
 {
-  // TODO
+  const char *msg="opening non-xml file didn't failed";
+  ensureThrow<Exception>(msg, "testdata/cfg_file_expand/non-xml-file.xml");
 }
 
 // check for exception when file cannot be read
@@ -68,9 +85,8 @@ template<>
 template<>
 void testObj::test<4>(void)
 {
-  typedef Exception Excp;
   const char *msg="opening file without permissions didn't caused an error";
-  ensureThrow<Excp>(msg, "/etc/shadow");
+  ensureThrow<Exception>(msg, "/etc/shadow");
 }
 
 // check for exception when file does not exist
@@ -78,9 +94,8 @@ template<>
 template<>
 void testObj::test<5>(void)
 {
-  typedef Exception Excp;
   const char *msg="opening non-exisitng file didn't caused an error";
-  ensureThrow<Excp>(msg, "/file/that/does/not/exist.txt");
+  ensureThrow<Exception>(msg, "/file/that/does/not/exist.txt");
 }
 
 // check for exception when file does not exist, but can be created
@@ -88,9 +103,8 @@ template<>
 template<>
 void testObj::test<6>(void)
 {
-  typedef Exception Excp;
   const char *msg="opening non-exisitng file didn't caused an error";
-  ensureThrow<Excp>(msg, "_file_that_does_not_exist_but_can_be_created.txt");
+  ensureThrow<Exception>(msg, "_file_that_does_not_exist_but_can_be_created.txt");
 }
 
 } // namespace tut
