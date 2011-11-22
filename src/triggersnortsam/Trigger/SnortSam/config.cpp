1badb5bf2f2614dd0ce7ce466a4774f636f49c37
Bartosz Szurgot <bartosz.szurgot@pwr.wroc.pl>
2011-11-22 13:42:58 +0100

    moved pre-defined value to config file of the component.
    added:
      src/triggersnortsam/Trigger/SnortSam/config.cpp
      src/triggersnortsam/Trigger/SnortSam/config.hpp
    modified:
      src/triggersnortsam/Trigger/SnortSam/Ver14/Strategy.cpp

---
+++ src/triggersnortsam/Trigger/SnortSam/config.cpp
@@ -0,0 +1,15 @@
+/*
+ * config.cpp
+ *
+ */
+#include "Trigger/SnortSam/config.hpp"
+
+namespace Trigger
+{
+namespace SnortSam
+{
+
+const unsigned int networkTimeout=30;   // [s]
+
+} // namespace SnortSam
+} // namespace Trigger
---
+++ src/triggersnortsam/Trigger/SnortSam/config.hpp
@@ -0,0 +1,19 @@
+/*
+ * config.hpp
+ *
+ */
+#ifndef INCLUDE_TRIGGER_SNORTSAM_CONFIG_HPP_FILE
+#define INCLUDE_TRIGGER_SNORTSAM_CONFIG_HPP_FILE
+
+namespace Trigger
+{
+namespace SnortSam
+{
+
+/** \brief timeout for network operations. */
+extern const unsigned int networkTimeout;
+
+} // namespace SnortSam
+} // namespace Trigger
+
+#endif
