c82c71dedf0ff9dd6f68cd291a8795a64c3519d0
Mariusz Uchronski <mariusz.uchronski@pwr.wroc.pl>
2012-01-03 10:11:44 +0100

    config class for whois trigger added.
    removed:
      src/triggerwhois/Trigger/Whois/tmp.cpp
    added:
      src/triggerwhois/Trigger/Whois/Config.hpp

---
+++ src/triggerwhois/Trigger/Whois/Config.hpp
@@ -0,0 +1,53 @@
+/*
+ * Config.hpp
+ *
+ */
+#ifndef INCLUDE_TRIGGER_WHOIS_CONFIG_HPP
+#define INCLUDE_TRIGGER_WHOIS_CONFIG_HPP
+
+#include <boost/filesystem.hpp>
+
+#include "Trigger/Simple/ThresholdConfig.hpp"
+
+namespace Trigger
+{
+namespace Whois
+{
+
+class Config
+{
+public:
+  /** \brief create configration description.
+   *  \param templateFile path to file containing message template.
+   *  \param th           threshold configuration - informs when run trigger.
+   */
+  Config(const boost::filesystem::path &templateFile, const Simple::ThresholdConfig &th):
+    templateFile_(templateFile),
+    th_(th)
+  {
+  }
+
+  /** \brief get path to file containing message template.
+   *  \return path to template file.
+   */
+  const boost::filesystem::path &getTemplateFilePath(void) const
+  {
+    return templateFile_;
+  }
+
+  /** \brief get thresholds configuration.
+   *  \return threshold's config.
+   */
+  const Simple::ThresholdConfig &getThresholdConfig(void) const
+  {
+    return th_;
+  }
+
+private:
+  boost::filesystem::path templateFile_;
+  Simple::ThresholdConfig th_;
+}; // class Config
+
+} // namespace Whois
+} // namespace Trigger
+#endif
