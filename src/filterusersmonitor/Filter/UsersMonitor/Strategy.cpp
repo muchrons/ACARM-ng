bad6a7ee564a7ab3ea34e4dd6f5a11d2674f9388
Bartosz Szurgot <bartosz.szurgot@pwr.wroc.pl>
2012-01-23 13:06:27 +0100

    consts and format changes.
    modified:
      src/filterusersmonitor/Filter/UsersMonitor/Strategy.cpp

--- src/filterusersmonitor/Filter/UsersMonitor/Strategy.cpp
+++ src/filterusersmonitor/Filter/UsersMonitor/Strategy.cpp
@@ -40,18 +40,17 @@ Data Strategy::makeThisEntryUserData(const Node n) const
 bool Strategy::isEntryInteresting(const NodeEntry thisEntry) const
 {
   assert( thisEntry.node_->isLeaf() && "ECL accepted non-leaf" );
-  Data d( thisEntry.node_->getAlert(), params_.skip_ );
+  const Data d( thisEntry.node_->getAlert(), params_.skip_ );
   if(d.get().size()==0u)
     return false;
   assert( d.get().size()>0u );
   return true;
 }
 
-Persistency::MetaAlert::Name Strategy::getMetaAlertName(
-                                              const NodeEntry thisEntry,
-                                              const NodeEntry otherEntry) const
+Persistency::MetaAlert::Name Strategy::getMetaAlertName(const NodeEntry thisEntry,
+                                                        const NodeEntry otherEntry) const
 {
-  Data::Names::const_iterator it=thisEntry.t_.commonWith(otherEntry.t_);
+  const Data::Names::const_iterator it=thisEntry.t_.commonWith(otherEntry.t_);
   assert( it!=thisEntry.t_.get().end() );
   stringstream ss;
   ss << "[usersmonitor] actions of user '"<<*it<<"'";
@@ -59,17 +58,16 @@ Persistency::MetaAlert::Name Strategy::getMetaAlertName(
 }
 
 
-bool Strategy::canCorrelate(const NodeEntry thisEntry,
-                            const NodeEntry otherEntry) const
+bool Strategy::canCorrelate(const NodeEntry thisEntry, const NodeEntry otherEntry) const
 {
-  return thisEntry.t_.commonWith(otherEntry.t_)!=thisEntry.t_.get().end();
+  return thisEntry.t_.commonWith(otherEntry.t_) != thisEntry.t_.get().end();
 }
 
 Data Strategy::makeUserDataForNewNode(const NodeEntry &thisEntry,
                                       const NodeEntry &otherEntry,
                                       const Node       /*newNode*/) const
 {
-  Data::Names::const_iterator it=thisEntry.t_.commonWith(otherEntry.t_);
+  const Data::Names::const_iterator it=thisEntry.t_.commonWith(otherEntry.t_);
   assert( it!=thisEntry.t_.get().end() );
   const Data d(*it, params_.skip_);
   assert( d.get().size()==1u ); // skip list should not apply here
